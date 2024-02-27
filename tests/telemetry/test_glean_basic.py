5ed8d3c77d7dcbae3bde37b0e8851ace99cdfdf6
Ben Chatterton <bchatterton@mozilla.com>
2024-02-27 11:13:25 -0800

    finished basic glean test; now to method-ify, pom-ify, etc

--- tests/telemetry/test_glean_basic.py
+++ tests/telemetry/test_glean_basic.py
@@ -12,16 +12,41 @@ from pytest_httpserver import HTTPServer
 from werkzeug.wrappers import Request
 from werkzeug.wrappers import Response
 import re
+import gzip
+import json
+
+
+PINGS_WITH_ID = 0
+PING_ID = ""
+
+
+def confirm_glean_correctness(
+    ping_ground: str, ping_test: str, engine_ground: str, engine_test: str
+) -> bool:
+    assert ping_ground == ping_test
+    assert engine_ground.lower() == engine_test.lower()
 
 
 def glean_handler(rq: Request) -> Response:
-    print(list(rq.headers.keys()))
     if "X-Debug-Id" in rq.headers.keys():
-        print("URL:", rq.url)
-        print("HEAD:", rq.headers)
-        return Response({"id": rq.headers["X-Debug-Id"]}, status=200)
-    else:
-        return Response("", status=200)
+        ping_id = rq.headers["X-Debug-Id"]
+        if rq.data:
+            body = json.loads(gzip.decompress(rq.data).decode())
+            engine_name = body["metrics"]["string"][
+                "search.engine.default.display_name"
+            ]
+            if PINGS_WITH_ID == 0:
+                engine_ground = "Google"
+                PINGS_WITH_ID += 1
+            else:
+                engine_ground = "DuckDuckGo"
+            confirm_glean_correctness(
+                ping_ground=PING_ID,
+                ping_test=ping_id,
+                engine_ground=engine_ground,
+                engine_test=engine_name,
+            )
+    return Response("", status=200)
 
 
 @dataclass
@@ -44,6 +69,7 @@ def test_glean_ping(driver: Firefox, httpserver: HTTPServer):
 
     # Set ping name
     ping = u.random_string(8)
+    PING_ID = ping
     print(f"ping: {ping}")
     driver.get("about:glean")
     ping_input = driver.find_element(By.ID, c.glean_ping_id)
@@ -52,42 +78,38 @@ def test_glean_ping(driver: Firefox, httpserver: HTTPServer):
         (By.CSS_SELECTOR, f"label[for='{c.glean_submit_id}'"), ping
     )
     driver.find_element(By.ID, c.glean_submit_id).click()
-    # driver.execute_script(
-    #     f"Services.fog.setTagPings('{ping}'); Services.fog.sendPing('metrics');"
-    # )
+
     # Search 1 (Google)
-    #driver.switch_to.new_window("tab")
     sleep(1)
     ba.search("trombone")
-    # for rq in driver.requests:
-    #     if "quicksuggest" in rq.url or "firefox.settings" in rq.url:
-    #         continue
-    #     print(rq.url, rq.headers, rq.body)
-    #     print("----")
-    #     if rq.response is not None:
-    #         print(rq.response.status_code, rq.response.headers)
-    #     else:
-    #         print("null response")
-    #     print("=====\n")
     ba.wait_on_title("Search")
     wait.until(
-        EC.visibility_of_element_located(
-            (By.CSS_SELECTOR, "div[aria-label='About this result']")
-        )
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div[role='navigation']"))
     )
+
     # Change default search engine
-    # driver.switch_to.new_window("tab")
     driver.get("about:preferences")
     driver.find_element(By.ID, c.prefs_search_cat_id).click()
     engine_select = driver.find_element(By.ID, c.prefs_engine_dropdown_id)
     engine_select.click()
     list_item = driver.find_element(By.CSS_SELECTOR, "menuitem[label='Google']")
     list_item.click()
+    wait.until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "menuitem[_moz-menuactive='true']")
+        )
+    )
     list_item.send_keys(
         Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.RETURN
     )  # we hack because we care - clicking on these special elements doesn't always work
     sleep(1)
+
     # Search 2 (DDG)
     ba.search("trumpet")
     ba.wait_on_title("DuckDuckGo")
     wait.until(EC.visibility_of_element_located((By.ID, "more-results")))
+
+    # We could go back to about:glean, but this is faster
+    with driver.context(driver.CONTEXT_CHROME):
+        driver.execute_script('Services.fog.sendPing("metrics");')
+    sleep(1)
