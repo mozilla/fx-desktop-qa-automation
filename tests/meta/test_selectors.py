ea8c7daf4b35d0adee023dacb31db3b1b7878b0a
Ben Chatterton <bchatterton@mozilla.com>
2024-05-03 09:18:36 -0700

    early hamburger menu selectors

---
+++ tests/meta/test_selectors.py
@@ -0,0 +1,10 @@
+from modules.browser_object import PanelUi
+from selenium.webdriver import Firefox
+
+def test_a_selector(driver: Firefox):
+    panel = PanelUi(driver).open()
+    panel.open_panel_menu()
+    panel.get_element("zoom-reduce").click()
+    panel.get_element("zoom-reduce").click()
+    panel.get_element("zoom-enlarge").click()
+    panel.get_element("zoom-reset").click()
