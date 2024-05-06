06db06b95f24a598700ffec16c6769297edcdfef
Sarina Li <sarinajin.li@gmail.com>
2024-05-06 14:05:41 -0400

    Onboarding Changes

--- tests/address_bar_and_search/test_search_modes_for_sites.py
+++ tests/address_bar_and_search/test_search_modes_for_sites.py
@@ -9,9 +9,23 @@ sites = ["Google", "Amazon", "Bing", "DuckDuckGo", "eBay"]
 
 @pytest.mark.parametrize("site", sites)
 def test_search_modes_for_sites(driver: Firefox, site: str):
-    # C2234690
-    # C1365213 (potentially)
+    """
+    Test that the Awesome bar contains the site shard and search for arbitrary item
+
+    Parameters
+    ----------
+    driver: WebDriver
+        The instance of the webdriver (Firefox)
+
+    sites: str[]
+        The list of search options to try
+
+    """
+    # search in bar the site
     nav = Navigation(driver).open()
+    # mode is optional because it will try to enter it as the little shard if specified
     nav.search("soccer", mode=site)
+    # verify its in the search bar
     nav.expect_in_content(EC.url_contains(site.lower()))
+    # close
     nav.clear_awesome_bar()
