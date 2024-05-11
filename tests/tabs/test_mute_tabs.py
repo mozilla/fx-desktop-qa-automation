60612ba1e206f09a5ebc36d80f23f0707d28b2f1
Ben Chatterton <bchatterton@mozilla.com>
2024-05-10 20:05:49 -0700

    for real, figure out fx version

--- tests/tabs/test_mute_tabs.py
+++ tests/tabs/test_mute_tabs.py
@@ -1,11 +1,16 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 
 from modules.browser_object import TabBar
 
 
-def test_mute_unmute_tab(sys_platform: str, driver: Firefox, video_url: str):
+def test_mute_unmute_tab(screenshot, driver: Firefox, video_url: str):
     # C134719
+    driver.get("about:support")
+    ac = ActionChains(driver)
+    ac.move_to_element_with_offset(driver.find_element(By.ID, "useragent-box"))
+    screenshot("about-support")
     tabs = TabBar(driver).open()
     driver.get(video_url)
     play_button = driver.find_element(By.CSS_SELECTOR, ".ytp-play-button")
