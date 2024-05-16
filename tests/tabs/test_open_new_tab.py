d24cf0fcd116ede98bb569bce1bebb0c974ace26
Tracy <twalker@mozilla.com>
2024-05-16 13:57:47 -0500

    Created start of test for open new tab test

--- tests/tabs/test_open_new_tab.py
+++ tests/tabs/test_open_new_tab.py
@@ -1,18 +1,31 @@
+import time
+
 import pytest
-from selenium.webdriver import Firefox
+import logging
+from selenium.webdriver import Firefox, ActionChains, Keys
 from selenium.webdriver.common.by import By
 
 from modules.browser_object import TabBar
 
 
-def test_open_new_tab_plus(driver: Firefox):
+# def test_open_new_tab_plus(driver: Firefox):
     # C134453
-    tabs = TabBar(driver).open()
-    tabs.new_tab_by_button()
+    # tabs = TabBar(driver).open()
+    # tabs.new_tab_by_button()
     # add assert code
 
 def test_open_new_tab_via_keyboard(driver: Firefox, sys_platform: str):
     # C134442
-    tabs = TabBar(driver).open()
-    tabs.NEW_TAB_KEY_COMBO[sys_platform]
+    TabBar(driver).open()
+    with driver.context(driver.CONTEXT_CHROME):
+        ActionChains(driver)\
+            .key_down(Keys.COMMAND)\
+            .send_keys("t")\
+            .perform()
+        time.sleep(1)
+    with driver.context(driver.CONTEXT_CONTENT):
+        time.sleep(1)
+    # key_combo=tabs.NEW_TAB_KEY_COMBO[sys_platform]
+    # print(key_combo)
+    # logging.info(f"Key combo {key_combo}...")
     # add assert code
