c825573c04412e31546b3bef9d3992354ccd5af5
Tracy <twalker@mozilla.com>
2024-05-17 08:29:50 -0500

    Created start of test for open new tab test

--- tests/tabs/test_open_new_tab.py
+++ tests/tabs/test_open_new_tab.py
@@ -1,27 +1,25 @@
 import time
-from selenium.webdriver import Firefox, ActionChains, Keys
+
+from selenium.webdriver import ActionChains, Firefox, Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from modules.browser_object import TabBar
 
-
 # def test_open_new_tab_plus(driver: Firefox):
-    # C134453
-    # tabs = TabBar(driver).open()
-    # tabs.new_tab_by_button()
-    # add assert code
+# C134453
+# tabs = TabBar(driver).open()
+# tabs.new_tab_by_button()
+# add assert code
+
 
 def test_open_new_tab_via_keyboard(driver: Firefox, sys_platform: str):
     # C134442
     browser = TabBar(driver).open()
     driver.get("about:robots")
     browser.set_chrome_context()
-    ActionChains(driver)\
-            .key_down(Keys.COMMAND)\
-            .send_keys("t")\
-            .perform()
+    ActionChains(driver).key_down(Keys.COMMAND).send_keys("t").perform()
     WebDriverWait(driver, 10).until(EC.title_contains("Mozilla Firefox"))
     assert driver.title == "Mozilla Firefox"
     if driver.find_element(By.CLASS_NAME, "fake-editable"):
