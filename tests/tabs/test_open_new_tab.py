615813b41505f2f444211527dad1028be479fb27
Tracy <twalker@mozilla.com>
2024-05-21 09:13:41 -0500

    Created start of test for open new tab test

--- tests/tabs/test_open_new_tab.py
+++ tests/tabs/test_open_new_tab.py
@@ -1,10 +1,9 @@
-import time
-
 from selenium.webdriver import ActionChains, Firefox, Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from modules.browser_object import TabBar
+
 
 def test_open_new_tab_plus(driver: Firefox):
     # C134453
@@ -24,6 +23,8 @@ def test_open_new_tab_via_keyboard(driver: Firefox, sys_platform: str):
     browser.set_chrome_context()
     # This action chain must be made into an object which accounts for cross
     # platform shortcut differences. As it is, will only work on macOS.
-    ActionChains(driver).key_down(Keys.COMMAND).send_keys("t").key_up(Keys.CONTROL).perform()
+    ActionChains(driver).key_down(Keys.COMMAND).send_keys("t").key_up(
+        Keys.CONTROL
+    ).perform()
     WebDriverWait(driver, 10).until(EC.title_contains("Mozilla Firefox"))
     assert driver.title == "Mozilla Firefox"
