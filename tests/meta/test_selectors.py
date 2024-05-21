de6d18145e1af993f8d6fcc976174961882750fa
Sarina Li <sarinajin.li@gmail.com>
2024-05-21 11:57:23 -0400

    Fixing zoom butrtons

--- tests/meta/test_selectors.py
+++ tests/meta/test_selectors.py
@@ -6,7 +6,8 @@ from modules.browser_object import PanelUi
 def test_a_selector(driver: Firefox):
     panel = PanelUi(driver).open()
     panel.open_panel_menu()
-    panel.get_element("zoom-reduce").click()
-    panel.get_element("zoom-reduce").click()
-    panel.get_element("zoom-enlarge").click()
-    panel.get_element("zoom-reset").click()
+    with driver.context(driver.CONTEXT_CHROME):
+        panel.get_element("zoom-reduce").click()
+        panel.get_element("zoom-reduce").click()
+        panel.get_element("zoom-enlarge").click()
+        panel.get_element("zoom-reset").click()
