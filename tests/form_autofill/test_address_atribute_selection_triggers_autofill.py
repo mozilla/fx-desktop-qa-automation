2faed9aa5a7722a71e338a4f7dfb892256be12d6
soncuteanca <soncutean.anca@gmail.com>
2024-05-24 15:38:00 +0300

    - Verify that the autofill values, when the dropdown is triggered from street address match the expected values for each field. - Normalize phone numbers to a standard format based on the country, removing country code if specified.

--- tests/form_autofill/test_address_atribute_selection_triggers_autofill.py
+++ tests/form_autofill/test_address_atribute_selection_triggers_autofill.py
@@ -3,42 +3,68 @@ from selenium.webdriver import Firefox
 
 from modules.browser_object import Navigation
 from modules.browser_object_autofill_popup import AutofillPopup
-from modules.util import Utilities
 from modules.page_object import AddressFill
+from modules.util import Utilities
 
 countries = ["CA", "US"]
 
 
 @pytest.mark.parametrize("country_code", countries)
-def test_address_atribute_selection(driver: Firefox, country_code: str):
+def test_address_attribute_selection(driver: Firefox, country_code: str):
     """
     C122359 - This test verifies that after filling the autofill fields and saving the data, hovering over the first
     item in the dropdown ensures that the actual value matches the expected value.
     """
     # Instantiate objects and open the navigation page
     Navigation(driver).open()
-    af = AddressFill(driver).open()
-    afp = AutofillPopup(driver)
+    address_form_fields = AddressFill(driver).open()
+    autofill_popup_panel = AutofillPopup(driver)
     util = Utilities()
 
     # Create fake data, fill in the form, and press submit and save on the doorhanger
     autofill_sample_data = util.fake_autofill_data(country_code)
-    af.save_information_basic(autofill_sample_data)
-    afp.press_doorhanger_save()
+    address_form_fields.save_information_basic(autofill_sample_data)
+    autofill_popup_panel.press_doorhanger_save()
 
-    # Creating new object to prevent stale webelements
-    new_af = AddressFill(driver).open()
-    new_afp = AutofillPopup(driver)
+    # Double-click on the name field to trigger the autocomplete address dropdown
+    address_form_fields.double_click("form-field", "street-address")
 
-    # Double-click on the name field to trigger the autocomplete dropdown
-    new_af.double_click("form-field", "street-address")
+    # Get the first element from the autocomplete address dropdown
+    first_item = autofill_popup_panel.get_nth_element("1")
+    autofill_popup_panel.hover_over_element(first_item)
+    autofill_popup_panel.click_nth_element("1")
 
-    # Get the first element from the autocomplete dropdown
-    first_item = new_afp.get_nth_element("1")
-    new_afp.hover_over_element(first_item)
+    fields_to_verify = {
+        "name": autofill_sample_data.name,
+        "organization": autofill_sample_data.organization,
+        "street-address": autofill_sample_data.street_address,
+        "address-level2": autofill_sample_data.address_level_2,
+        "address-level1": autofill_sample_data.address_level_1,
+        "postal-code": autofill_sample_data.postal_code,
+        "country": autofill_sample_data.country,
+        "email": autofill_sample_data.email,
+        "tel": autofill_sample_data.telephone,
+    }
 
-    # Get the primary value (street address) from the first item in the dropdown and assert that the actual value
-    # matches the expected value
-    actual_value = new_afp.get_primary_value(first_item)
-    expected_street_address = autofill_sample_data.street_address
-    assert expected_street_address == actual_value, f"Expected {expected_street_address}, but got {actual_value}"
+    # Loop through each field and compare the expected value with the displayed value
+    for field_name, expected_value in fields_to_verify.items():
+        if not expected_value:
+            continue  # Skip comparison if the field value is missing, phone number is not always generated
+
+        input_field = address_form_fields.get_element("form-field", field_name)
+        displayed_value = input_field.get_attribute("value")
+
+        if not displayed_value:
+            continue  # Skip comparison if the displayed value is empty
+
+        if field_name == "tel":
+            expected_value = address_form_fields.normalize_phone_number(
+                expected_value, country_code
+            )
+            displayed_value = address_form_fields.normalize_phone_number(
+                displayed_value, country_code
+            )
+
+        assert (
+            displayed_value == expected_value
+        ), f"Expected {field_name} to be {expected_value}, but got {displayed_value}"
