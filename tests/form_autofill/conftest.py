f4674ae53e99def4da70e13083e4a6cdbdebc197
Sarina Li <sarinajin.li@gmail.com>
2024-05-24 16:38:36 -0400

    Added conftest keyring

--- tests/form_autofill/conftest.py
+++ tests/form_autofill/conftest.py
@@ -1,9 +1,24 @@
 import pytest
+from subprocess import CalledProcessError, check_call
+import logging
 
 
 @pytest.fixture()
 def suite_id():
     return ("2054", "Form Autofill")
+
+@pytest.fixture()
+def unlock_keyring(sys_platform: str):
+    # TODO: add linux and windows unlocks if relevant
+    # TODO: add secrets mgmt and insertion
+    if sys_platform != "Darwin":
+        return None
+    try:
+        check_call(["security", "unlock-keychain"])
+    except CalledProcessError:
+        logging.warning("Failed to unlock keyring: security has errors.")
+    except OSError:
+        logging.warning("Failed to unlock keyring: security executable not found.")
 
 
 @pytest.fixture()
