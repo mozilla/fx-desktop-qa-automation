8362d8ce93d900823ae7b192f19085df014da4f4
Ben Chatterton <bchatterton@mozilla.com>
2024-05-28 16:15:17 -0700

    fix precommit logic

---
+++ run_commit_tests.py
@@ -0,0 +1,13 @@
+from subprocess import check_output
+
+if __name__ == "__main__":
+    committed_files = (
+        check_output(["git", "--no-pager", "diff", "--name-only", "--cached"])
+        .decode()
+        .splitlines()
+    )
+
+    tests = [f for f in committed_files if f.startswith("test") and f.endswith(".py")]
+    print(f"testing {*tests}")
+
+    check_output(["pytest", "-vs", "--run-headless", *tests])
