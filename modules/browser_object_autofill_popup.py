b46a3b9df31b407a1046d05acc724ddc07ad428f
Sarina Li <sarinajin.li@gmail.com>
2024-06-04 15:37:35 -0400

    fix merge conflict

--- modules/browser_object_autofill_popup.py
+++ modules/browser_object_autofill_popup.py
@@ -1,7 +1,7 @@
 import json
 
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
 
 from modules.page_base import BasePage
 
