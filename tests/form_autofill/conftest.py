c66f32e00e787b2049afbe6e49cd21d2a763bd6c
Sarina Li <sarinajin.li@gmail.com>
2024-06-04 11:22:42 -0400

    started private browsing

--- tests/form_autofill/conftest.py
+++ tests/form_autofill/conftest.py
@@ -6,10 +6,12 @@ def suite_id():
     return ("2054", "Form Autofill")
 
 
+# TODO: fix the private browsing so it only triggers the setting for the private browsing test
 @pytest.fixture()
 def set_prefs():
     """Set prefs"""
     return [
         ("extensions.formautofill.creditCards.reauth.optout", False),
         ("extensions.formautofill.reauth.enabled", False),
+        # ("browser.privatebrowsing.autostart", True)
     ]
---
+++ tests/form_autofill/test_private_browsing_form_autofill.py
@@ -0,0 +1,31 @@
+from time import sleep
+import pytest
+
+from selenium.webdriver import Firefox
+
+from modules.browser_object import Navigation
+from modules.browser_object_autofill_popup import AutofillPopup
+from modules.page_object import AboutPrefs, AddressFill
+from modules.util import Utilities
+
+countries = ["CA", "US"]
+
+@pytest.mark.parametrize("country_code", countries)
+def test_private_browsing_form_autofill(driver: Firefox, country_code: str):
+    # instantiate objects
+    Navigation(driver).open()
+    address_fill_obj = AddressFill(driver).open()
+    autofill_popup_obj = AutofillPopup(driver)
+    util = Utilities()
+
+    # TODO: open the browser in incognito mode
+    autofill_sample_data = util.fake_autofill_data(country_code)
+    address_fill_obj.save_information_basic(autofill_sample_data)
+    autofill_popup_obj.press_doorhanger_save()
+
+    # double click the name
+    address_fill_obj.double_click("form-field", "name")
+    with driver.context(driver.CONTEXT_CHROME):
+        autofill_popup_obj.get_element("autofill-panel").click()
+
+    sleep(5)
