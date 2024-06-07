5ed699bad8438f4cc663dc7ad4c83c94fa72c868
Ben Chatterton <bchatterton@mozilla.com>
2024-06-07 16:17:07 -0400

    add basic tests

--- modules/page_object.py
+++ modules/page_object.py
@@ -1,4 +1,5 @@
 from modules.page_object_about_addons import *
+from modules.page_object_about_config import *
 from modules.page_object_about_glean import *
 from modules.page_object_about_logins import *
 from modules.page_object_about_prefs import *
