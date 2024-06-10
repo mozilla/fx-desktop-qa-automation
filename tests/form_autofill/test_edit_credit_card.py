1e72b0669f8bf0dfcf3d0f01bad56052527f4aad
Sarina Li <sarinajin.li@gmail.com>
2024-06-10 09:51:38 -0400

    added basic test (Doesnt work

---
+++ tests/form_autofill/test_edit_credit_card.py
@@ -0,0 +1,124 @@
+import json
+import logging
+from time import sleep
+
+import pytest
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import Firefox
+from selenium.webdriver.support import expected_conditions as EC
+
+from modules.browser_object import CreditCardPopup, Navigation
+from modules.browser_object_autofill_popup import AutofillPopup
+from modules.page_object import AboutPrefs
+from modules.page_object_autofill_credit_card import CreditCardFill
+from modules.util import BrowserActions, Utilities
+
+# tabs = [i for i in range(4)]
+tabs = [1]
+
+
+@pytest.mark.parametrize("num_tabs", tabs)
+def test_edit_credit_card_profile(driver: Firefox, num_tabs: int):
+    """
+    C122390, ensures that editing a credit card profile in the about:prefs
+    has the correct behaviour
+    """
+    # instantiate objects
+    nav = Navigation(driver)
+    about_prefs_obj = AboutPrefs(driver, category="privacy")
+    util = Utilities()
+    browser_action_obj = BrowserActions(driver)
+    credit_card_fill_obj = CreditCardFill(driver).open()
+    autofill_popup_obj = AutofillPopup(driver)
+
+    # fill in cc information
+    nav.open()
+    credit_card_fill_obj.open()
+    credit_card_sample_data_original = util.fake_credit_card_data()
+    credit_card_fill_obj.fill_credit_card_info(credit_card_sample_data_original)
+    autofill_popup_obj.press_doorhanger_save()
+
+    # navigate to about:prefs and select the saved payment methods
+    about_prefs_obj.open()
+    iframe = about_prefs_obj.press_button_get_popup_dialog_iframe(
+        "Saved payment methods"
+    )
+    browser_action_obj.switch_to_iframe_context(iframe)
+
+    # fetch the saved cc profile and select edit
+    saved_profile = about_prefs_obj.get_element("cc-saved-options")
+    info_string = saved_profile.get_attribute("data-l10n-args")
+    cc_info_json_original = json.loads(info_string)
+    logging.info(f"Extracted Original data: {cc_info_json_original}")
+    saved_profile.click()
+
+    # assert that the edit button is clickable
+    panel_edit_button = about_prefs_obj.get_element(
+        "panel-popup-button", labels=["autofill-manage-edit-button"]
+    )
+    about_prefs_obj.expect(EC.element_to_be_clickable(panel_edit_button))
+
+    # ensure the same year or month is not generated
+    credit_card_sample_data_new = util.fake_credit_card_data()
+    while (
+        credit_card_sample_data_new.card_number[-4:] in info_string
+        or str(int(credit_card_sample_data_new.expiration_month)) in info_string
+        or credit_card_sample_data_new.expiration_year in info_string
+    ):
+        credit_card_sample_data_new = util.fake_credit_card_data()
+
+    # create the dict
+    tab_num_to_data = {
+        0: credit_card_sample_data_new.card_number,
+        1: credit_card_sample_data_new.expiration_month,
+        2: f"20{credit_card_sample_data_new.expiration_year}",
+        3: credit_card_sample_data_new.name,
+    }
+
+    tab_num_to_original_data = {
+        0: credit_card_sample_data_original.card_number[-4:],
+        1: f"{str(int(credit_card_sample_data_original.expiration_month))}",
+        2: f"20{credit_card_sample_data_original.expiration_year}",
+        3: credit_card_sample_data_original.name,
+    }
+
+    # modify some values
+    panel_edit_button.click()
+    about_prefs_obj.update_cc_field_panel(num_tabs, tab_num_to_data[num_tabs])
+
+    logging.info(f"New data: {credit_card_sample_data_new.expiration_month}")
+    # fetch the edited profile, ensure that the attribute containing the data is new
+    about_prefs_obj.expect_not(
+        EC.text_to_be_present_in_element_attribute(
+            about_prefs_obj.get_selector("cc-saved-options"),
+            "data-l10n-args",
+            tab_num_to_original_data[num_tabs],
+        )
+    )
+    try:
+        edited_profile = about_prefs_obj.get_element("cc-saved-options")
+    except TimeoutException:
+        util.write_html_content("cur", driver, False)
+    cc_info_json = json.loads(edited_profile.get_attribute("data-l10n-args"))
+    logging.info(f"Extracted Edited data: {cc_info_json}")
+
+    # changing the cc number
+    logging.info(f"Original Data: {credit_card_sample_data_original.card_number}")
+    logging.info(f"New Data: {credit_card_sample_data_new.card_number}")
+
+    if num_tabs == 0:
+        credit_card_sample_data_original.card_number = (
+            credit_card_sample_data_new.card_number
+        )
+    elif num_tabs == 1:
+        credit_card_sample_data_original.expiration_month = (
+            credit_card_sample_data_new.expiration_month
+        )
+    elif num_tabs == 2:
+        credit_card_sample_data_original.expiration_year = (
+            credit_card_sample_data_new.expiration_year
+        )
+    else:
+        credit_card_sample_data_original.name = credit_card_sample_data_new.name
+
+    about_prefs_obj.verify_cc_json(cc_info_json, credit_card_sample_data_original)
