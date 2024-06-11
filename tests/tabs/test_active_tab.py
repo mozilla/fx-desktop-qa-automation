f296c747ab19d0b416b9fe6e87193fb95def8b1a
Sarina Li <sarinajin.li@gmail.com>
2024-06-11 10:56:14 -0400

    rm a comment

--- tests/tabs/test_active_tab.py
+++ tests/tabs/test_active_tab.py
@@ -23,16 +23,3 @@ def test_active_test(driver: Firefox):
             visibility = target_tab.get_attribute("visuallyselected")
             assert visibility == ""
             assert tab_color == "rgb(12, 12, 13)" and tab_color != "rgb(21, 20, 26)"
-
-    # tabs.new_tab_by_button()
-    # target_tab = tabs.get_tab(3)
-    # with driver.context(driver.CONTEXT_CHROME):
-    #     css_properties = driver.execute_script("""
-    #         var styles = window.getComputedStyle(arguments[0]);
-    #         var styleObject = {};
-    #         for (var i = 0; i < styles.length; i++) {
-    #             styleObject[styles[i]] = styles.getPropertyValue(styles[i]);
-    #         }
-    #             return styleObject;
-    #         """, target_tab)
-    #     print(css_properties)
