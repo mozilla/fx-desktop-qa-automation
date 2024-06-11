73ca679a3a1cc51a0376d140f4e9d297018db1cc
Sarina Li <sarinajin.li@gmail.com>
2024-06-11 13:57:21 -0400

    edit a line

--- tests/tabs/test_active_tab.py
+++ tests/tabs/test_active_tab.py
@@ -15,8 +15,7 @@ def test_active_tab(driver: Firefox):
         tabs.new_tab_by_button()
 
     # go through all the tabs and ensure highlighted one is correct, +2 since 1 indexed and additional tab for the beginning
-    # for i in range(1, num_tabs + 2):
-    for i in range(1, 2):
+    for i in range(1, num_tabs + 2):
         target_tab = tabs.get_tab(i)
         with driver.context(driver.CONTEXT_CHROME):
             target_tab.click()
