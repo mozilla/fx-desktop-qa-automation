4f8bd897dac4f554d854619f6d50d7118d6a0aba
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-06-11 11:44:24 -0700

    Bc/pocket setup (#48)
    * add basic tests
    
    * add test 'starters' for layout
    
    * add support methods for about blank
    
    * get basic layout tests passing
    
    * Finish basic tests; update conftest
    
    * add basic flake testing tool
    
    * documentation, other chores
    
    * PR revisions

---
+++ modules/page_object_about_config.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.keys import Keys
+
+from modules.page_base import BasePage
+
+
+class AboutConfig(BasePage):
+    URL_TEMPLATE = "about:config"
+
+    def click_warning_button(self) -> BasePage:
+        self.get_element("warning-button").click()
+        return self
+
+    def search_pref(self, term: str) -> BasePage:
+        searchbar = self.get_element("about-config-search-input")
+        searchbar.clear()
+        searchbar.send_keys(term + Keys.ENTER)
+        return self
---
+++ tests/pocket/test_basic_fr.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium.webdriver import Firefox
+
+from modules.page_object import AboutNewtab
+
+
+@pytest.fixture()
+def add_prefs():
+    return [
+        ("browser.search.region", "FR"),
+        ("browser.ping-centre.log", True),
+    ]
+
+
+@pytest.mark.locale_fr
+def test_localized_pocket_layout_FR(driver: Firefox):
+    """
+    C408038: Test about:blank Pocket layout (ROW / FR)
+    """
+    about_newtab = AboutNewtab(driver).open()
+    about_newtab.set_language_code("frFR")
+    about_newtab.check_layout()
