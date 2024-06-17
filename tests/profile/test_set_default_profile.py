d744e6cbe48a3c7e455baa74da5057af9419461d
Sarina Li <sarinajin.li@gmail.com>
2024-06-17 11:13:16 -0400

    changes for now

--- tests/profile/test_set_default_profile.py
+++ tests/profile/test_set_default_profile.py
@@ -1,6 +1,8 @@
+import logging
 from time import sleep
 
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
 
 from modules.page_object import AboutProfiles
 
@@ -10,4 +12,11 @@ def test_set_default_profile(driver: Firefox):
     C130792, set the default profile through the firefox browser
     """
     about_profiles = AboutProfiles(driver).open()
-    sleep(10)
+
+    profiles_div = about_profiles.get_element("profile-container")
+
+    child_elements = profiles_div.find_elements(By.XPATH, "./child::*")
+    default_profile_element = child_elements[0]
+    default_profile_element.find_element(
+        By.CSS_SELECTOR, "h3[data-l10n-id='profiles-in-use-profile']"
+    )
