368f1d6d6332b10434b4c08f2e6a96c5e24a8424
Sarina Li <sarinajin.li@gmail.com>
2024-06-18 16:07:05 -0400

    fix random iteration

--- tests/menus/test_copy_paste_actions.py
+++ tests/menus/test_copy_paste_actions.py
@@ -1,8 +1,9 @@
-from time import sleep
+import logging
 
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.keys import Keys
 
-from modules.browser_object import ContextMenu, Navigation
+from modules.browser_object import Navigation, SearchBarContextMenu
 from modules.page_object import GoogleSearch
 from modules.util import Utilities
 
@@ -16,17 +17,34 @@ def test_text_area_copy_paste(driver: Firefox):
 
 
 def test_search_field_copy_paste(driver: Firefox):
-    nav = Navigation(driver).open()
-    context_menu = ContextMenu(driver)
+    Navigation(driver).open()
+    context_menu = SearchBarContextMenu(driver)
     google_search = GoogleSearch(driver).open()
     util = Utilities()
 
     # send the text
     random_text = util.generate_random_text("sentence")
     search_bar = google_search.get_search_bar_element()
-    search_bar.send_keys()
+    search_bar.send_keys(random_text)
+    logging.info(f"Sent the text {random_text} to the search bar.")
 
-    # google_search.type_in_search_bar(random_text)
-    # google_search.triple_click("search-bar-textarea")
+    # triple click the text to select all
+    google_search.triple_click("search-bar-textarea")
 
-    # google_search.context_click_element()
+    # context click
+    context_menu.context_click_element(search_bar)
+    with driver.context(driver.CONTEXT_CHROME):
+        context_menu.get_context_item("context-menu-copy").click()
+        google_search.hide_popup("contentAreaContextMenu")
+
+    # delete the current text
+    search_bar.send_keys(Keys.BACK_SPACE)
+
+    # context click and paste the text back
+    context_menu.context_click_element(search_bar)
+    with driver.context(driver.CONTEXT_CHROME):
+        context_menu.get_context_item("context-menu-paste").click()
+        google_search.hide_popup("contentAreaContextMenu")
+
+    # assert the value is correct
+    assert search_bar.get_attribute("value") == random_text
