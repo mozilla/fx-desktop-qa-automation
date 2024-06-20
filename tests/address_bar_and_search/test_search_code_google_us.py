0f2bb156faa40eec5f64d1218bcf2adb8905d8c4
Tracy <twalker@mozilla.com>
2024-06-20 14:23:53 -0500

    Started test and updated similar Incident test

--- tests/address_bar_and_search/test_search_code_google_us.py
+++ tests/address_bar_and_search/test_search_code_google_us.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium.webdriver import Firefox
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,19 +5,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 from modules.browser_object import Navigation
 
 
-@pytest.mark.incident
-def test_google_search_code(driver: Firefox):
+def test_search_code_google_us(driver: Firefox):
     """
     C1365268 - Default Search Code: Google - US
-    Only tests the Awesome bar portion of the test for Incident smoke
+    This tests multiple ways of sending a search; Awesome bar,
+    Search bar and selected text
     """
 
     nav = Navigation(driver).open()
 
     # Check code generated from the Awesome bar search
-    fx_code = "client=firefox-b-d"
+    fx_code = "client=firefox-b-1-d"
     nav.search("soccer")
     WebDriverWait(driver, 10).until(EC.title_contains("Google Search"))
     search_url = driver.current_url
     assert fx_code in search_url
     nav.clear_awesome_bar()
+
+    # Check code generated from the Search bar search
+
+    # Check code generated from the context click of selected text
