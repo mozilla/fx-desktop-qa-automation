267a9dd2b2e1eecc05ea8e95e80f13e099a7438e
soncuteanca <soncutean.anca@gmail.com>
2024-06-21 14:57:11 +0300

    Implement retry mechanism for locating search suggestion elements

--- tests/address_bar_and_search/test_suggestion_engine_selection.py
+++ tests/address_bar_and_search/test_suggestion_engine_selection.py
@@ -1,6 +1,10 @@
+import time
+
 import pytest
 from selenium.webdriver import Firefox, Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from modules.browser_object import Navigation
 
@@ -9,15 +13,18 @@ sites = ["Google", "Amazon.com", "Bing", "DuckDuckGo", "eBay"]
 
 @pytest.mark.parametrize("site", sites)
 def test_search_suggestion_for_engine_selection(driver: Firefox, site: str):
-    """
-    C1365228 - Test to verify that when entering "@", a list of search engines is suggested, and upon selecting an
-    engine, the search is performed using the selected engine.
-    """
     nav = Navigation(driver).open()
     nav.type_in_awesome_bar("@")
 
-    # Use base selector to find all search engine suggestions
-    suggestion_list_items = nav.get_element("search-suggestion-list", multiple=True)
+    # Try to find the element multiple times with a small delay between each attempt
+    for _ in range(30):
+        try:
+            suggestion_list_items = nav.get_elements("search-suggestion-list")
+            if suggestion_list_items:
+                break  # If the elements are found, break out of the loop
+        except Exception:
+            pass
+        time.sleep(1)  # If the elements are not found, wait for a second and try again
 
     # Filter elements to find the one that matches the desired site
     suggestion_list_item = next(
@@ -26,7 +33,7 @@ def test_search_suggestion_for_engine_selection(driver: Firefox, site: str):
 
     if suggestion_list_item:
         suggestion_list_item.click()
-        nav.type_in_awesome_bar("soccer" + Keys.ENTER)
+        nav.type_in_awesome_bar("cobra" + Keys.ENTER)
         nav.expect_in_content(EC.url_contains(site.lower()))
     else:
         pytest.fail(f"No search suggestion found for site: {site}")
