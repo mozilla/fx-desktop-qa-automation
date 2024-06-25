e3cb1067fe5df45cc38297e2f3e7a412eeea279e
Sarina Li <sarinajin.li@gmail.com>
2024-06-25 14:49:40 -0400

    added final part

--- tests/menus/test_frequently_used_context_menu.py
+++ tests/menus/test_frequently_used_context_menu.py
@@ -1,10 +1,74 @@
+import platform
+from time import sleep
+
+import pytest
 from selenium.webdriver import Firefox
 
-from modules.browser_object import ContextMenu
+from modules.browser_object import ContextMenu, Navigation
 from modules.page_object import ExamplePage
+from modules.util import BrowserActions, Utilities
 
-# def test_save_page_as(driver: Firefox):
-#     pass
+
+@pytest.mark.unstable
+def test_save_page_as(driver: Firefox):
+    """
+    C2637623.1: save page as
+    """
+    try:
+        from pynput.keyboard import Key
+    except ModuleNotFoundError:
+        pytest.skip("Could not load pynput")
+    # create objects
+    context_menu = ContextMenu(driver)
+    driver.get("https://example.com")
+    example_page = ExamplePage(driver)
+    nav = Navigation(driver)
+    util = Utilities()
+    ba = BrowserActions(driver)
+
+    # right click something that is not a hyperlink
+    title_header = example_page.get_element("title-header")
+    context_menu.context_click_element(title_header)
+
+    with driver.context(driver.CONTEXT_CHROME):
+        save_page_as = context_menu.get_context_item("context-menu-save-page-as")
+        save_page_as.click()
+
+    downloads_button = nav.get_download_button()
+
+    # short sleep to ensure menu is shown
+    sleep(0.5)
+
+    # perform key presses to save the file
+    this_platform = platform.system()
+    if this_platform == "Linux":
+        ba.controller.press(Key.alt)
+        ba.controller.press(Key.tab)
+        ba.controller.release(Key.tab)
+        ba.controller.release(Key.alt)
+
+        ba.controller.press(Key.alt)
+        ba.controller.press(Key.tab)
+        ba.controller.release(Key.tab)
+        ba.controller.release(Key.alt)
+
+        ba.key_press_release(Key.tab)
+
+        ba.key_press_release(Key.tab)
+
+    # Press and release the Enter key
+    ba.key_press_release(Key.enter)
+
+    # Wait for the animation to complete
+    with driver.context(driver.CONTEXT_CHROME):
+        nav.wait_for_download_animation_finish(downloads_button)
+
+    # verify and delete downloaded file
+    saved_image_location = util.get_saved_file_path("Example Domain.html")
+
+    util.check_file_path_validility(saved_image_location)
+
+    util.remove_file(saved_image_location)
 
 
 def test_take_screenshot(driver: Firefox):
@@ -34,23 +98,23 @@ def test_take_screenshot(driver: Firefox):
         assert screenshot_box.get_attribute("hidden") is None
 
 
-# def test_inspect(driver: Firefox):
-#     """
-#     C2637623.3: inspect works from context menu
-#     """
-#     # create objects
-#     context_menu = ContextMenu(driver)
-#     driver.get("https://example.com")
-#     example_page = ExamplePage(driver)
+def test_inspect(driver: Firefox):
+    """
+    C2637623.3: inspect works from context menu
+    """
+    # create objects
+    context_menu = ContextMenu(driver)
+    driver.get("https://example.com")
+    example_page = ExamplePage(driver)
 
-#     # right click something that is not a hyperlink
-#     title_header = example_page.get_element("title-header")
-#     context_menu.context_click_element(title_header)
+    # right click something that is not a hyperlink
+    title_header = example_page.get_element("title-header")
+    context_menu.context_click_element(title_header)
 
-#     # find an element present in the dev tools
-#     with driver.context(driver.CONTEXT_CHROME):
-#         inspect_option = context_menu.get_context_item("context-menu-inspect")
-#         inspect_option.click()
-#         context_menu.hide_popup_by_child_node(inspect_option)
+    # find an element present in the dev tools
+    with driver.context(driver.CONTEXT_CHROME):
+        inspect_option = context_menu.get_context_item("context-menu-inspect")
+        inspect_option.click()
+        context_menu.hide_popup_by_child_node(inspect_option)
 
-#         example_page.element_exists("inspect-menu-horizontal-splitter")
+        example_page.element_exists("inspect-menu-horizontal-splitter")
