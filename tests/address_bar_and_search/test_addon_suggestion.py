b59699be3ad0abecb00260a6fda33d4e41e1b097
soncuteanca <soncutean.anca@gmail.com>
2024-06-28 13:13:11 +0300

    De-parametrize the test to avoid lengthy waits

--- tests/address_bar_and_search/test_addon_suggestion.py
+++ tests/address_bar_and_search/test_addon_suggestion.py
@@ -6,35 +6,48 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from modules.browser_object import Navigation
 
-# Map input text to addon names
-input_to_addon_name = {
-    "clips": "video-downloadhelper",
-    "grammar": "languagetool",
-    "Temp mail": "private-relay",
-    "pics search": "search_by_image",
-    "darker theme": "darkreader",
-    "privacy": "privacy-badger17",
-    "read aloud": "read-aloud",
-}
+
+@pytest.fixture()
+def add_prefs():
+    return [
+        ("browser.search.region", "US"),
+    ]
 
 
-@pytest.mark.parametrize("input_text, addon_name", input_to_addon_name.items())
-def test_addon_suggestion_based_on_search_input(
-    driver: Firefox, input_text: str, addon_name: str
-):
+def test_addon_suggestion_based_on_search_input(driver: Firefox):
     """
     C2234714: Test that add-on suggestions match the URL bar input.
+
+    To avoid lengthy waits caused by network traffic during browser startup,
+    this test loops through the list of addons in a single browser session
+    instead of using parameterization. This reduces the overall test duration
+    by waiting for the network traffic only once.
     """
 
-    nav = Navigation(driver).open()
-    time.sleep(10)
-    nav.set_awesome_bar()
-    time.sleep(10)
-    nav.awesome_bar.click()
-    nav.awesome_bar.send_keys(input_text)
-    nav.element_visible("addon-suggestion")
-    nav.get_element("addon-suggestion").click()
+    # Map input text to addon names
+    input_to_addon_name = {
+        "clips": "video-downloadhelper",
+        "grammar": "languagetool",
+        "Temp mail": "private-relay",
+        "pics search": "search_by_image",
+        "darker theme": "darkreader",
+        "privacy": "privacy-badger17",
+        "read aloud": "read-aloud",
+    }
 
-    # Construct the expected URL
-    expected_url = f"https://addons.mozilla.org/en-US/firefox/addon/{addon_name}/"
-    nav.expect_in_content(EC.url_contains(expected_url))
+    nav = Navigation(driver).open()
+    time.sleep(20)
+    nav.set_awesome_bar()
+    time.sleep(20)
+    nav.awesome_bar.click()
+
+    for input_text, addon_name in input_to_addon_name.items():
+        nav.awesome_bar.send_keys(input_text)
+        nav.element_visible("addon-suggestion")
+        nav.get_element("addon-suggestion").click()
+
+        # Construct the expected URL
+        expected_url = f"https://addons.mozilla.org/en-US/firefox/addon/{addon_name}/"
+        nav.expect_in_content(EC.url_contains(expected_url))
+
+        nav.awesome_bar.clear()
