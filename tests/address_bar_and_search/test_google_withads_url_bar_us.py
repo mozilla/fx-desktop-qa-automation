49bd9fd822350b6780df59f8dc820a3209178793
vsangereanMOZ <vsangerean@mozilla.com>
2024-07-04 14:07:41 +0300

    Update test to run with latest changes from HY's commit

--- tests/address_bar_and_search/test_google_withads_url_bar_us.py
+++ tests/address_bar_and_search/test_google_withads_url_bar_us.py
@@ -1,9 +1,11 @@
 import time
+
 import pytest
 from selenium.webdriver import Firefox
 
 from modules.browser_object import Navigation
 from modules.page_object import AboutTelemetry
+from modules.page_object_about_config import AboutConfig
 from modules.util import Utilities
 
 
@@ -21,22 +23,24 @@ def test_google_withads_url_bar_us(driver: Firefox):
 
     # instantiate objects
     nav = Navigation(driver).open()
+    about_config = AboutConfig(driver)
+    util = Utilities()
+
+    # change pref value in order to not display accept cookies banner
+    about_config.change_pref_value("cookiebanners.service.mode", 1)
+
     nav.search("iphone")
     time.sleep(5)
     about_telemetry = AboutTelemetry(driver).open()
-    util = Utilities()
+    time.sleep(2)
 
     # Click on Raw JSON, switch tab and click on Raw Data
     about_telemetry.get_element("category-raw").click()
     about_telemetry.switch_tab()
     about_telemetry.get_element("rawdata-tab").click()
-    time.sleep(5)
 
-    #Verify the following ping is recorded: ""browser.search.withads.urlbar": { "google:tagged": 1}".
-
+    # Verify the following ping is recorded: ""browser.search.withads.urlbar": { "google:tagged": 1}".
     json_data = util.decode_url(driver)
     assert util.assert_json_value(
-        json_data,
-        '$..["browser.search.withads.urlbar"].["google:tagged"]',
-        1,
+        json_data, '$..["browser.search.withads.urlbar"].["google:tagged"]', 1
     )
