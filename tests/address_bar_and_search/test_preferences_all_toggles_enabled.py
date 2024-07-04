c4c598876f36afd9863a047f9b65a7cbcd9a99cc
Hani Yacoub <hani.yacoub@svadmins-MacBook-Pro.local>
2024-07-04 19:10:10 +0300

    Modifications

--- tests/address_bar_and_search/test_preferences_all_toggles_enabled.py
+++ tests/address_bar_and_search/test_preferences_all_toggles_enabled.py
@@ -1,13 +1,9 @@
-
 import time
 
 import pytest
-from selenium.common import NoSuchElementException
 from selenium.webdriver import Firefox
 
 from modules.browser_object_navigation import Navigation
-from modules.browser_object_tabbar import TabBar
-from modules.page_object_about_newtab import AboutNewtab
 from modules.page_object_about_prefs import AboutPrefs
 from modules.util import BrowserActions
 
@@ -27,7 +23,6 @@ def test_preferences_all_toggles_enabled(driver: Firefox):
     nav = Navigation(driver).open()
     about_prefs = AboutPrefs(driver, category="search").open()
     u = BrowserActions(driver)
-    tabs = TabBar(driver)
 
     # Check if toggles are enabled
     nonsponsored_checkbox = about_prefs.get_element("firefox-suggest-nonsponsored")
@@ -36,30 +31,14 @@ def test_preferences_all_toggles_enabled(driver: Firefox):
     assert sponsors_checkbox.is_selected(), f"Checkbox with selector '{sponsors_checkbox}' is not checked"
 
     # Check if sponsored suggestion is displayed
-    tabs.new_tab_by_button()
-    tabs.switch_tab()
     time.sleep(20)
     u.search("iphone", with_enter=False)
-    time.sleep(5)
+    time.sleep(20)
     with (driver.context(driver.CONTEXT_CHROME)):
-        firefox_suggest = nav.get_element("firefox-suggestion")
-        # .get_attribute("Sponsored")
-        # sponsored = firefox_suggest.get_attribute("Sponsored")
-        # expected_attribute = "Sponsored"
-        assert firefox_suggest.is_displayed()
-    # sponsored = firefox_suggest.get_attribute("Sponsored")
-    # assert firefox_suggest.is_displayed()
+        assert nav.get_elements("firefox-suggestion")
 
     # Check if a non-sponsored suggestion is displayed
-    nav.clear_awesome_bar()
     time.sleep(20)
-    u.search("wikipedia", with_enter=False)
-    # nav.type_in_awesome_bar("wikipedia")
-    time.sleep(20)
-    try:
-        with (driver.context(driver.CONTEXT_CHROME)):
-            firefox_suggest = nav.get_element("firefox-suggestion")
-            is_displayed = firefox_suggest.is_displayed()
-    except NoSuchElementException:
-        is_displayed = False
-    assert not is_displayed, f"Element with XPath {firefox_suggest} is displayed, but it should not be."
+    u.search("wiki", with_enter=False)
+    with (driver.context(driver.CONTEXT_CHROME)):
+        assert not nav.get_elements("firefox-suggestion")
