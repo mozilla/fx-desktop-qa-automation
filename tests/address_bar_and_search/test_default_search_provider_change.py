b48762f251c67afaa0365c44004776828e581b4a
vsangereanMOZ <vsangerean@mozilla.com>
2024-07-08 15:24:27 +0300

    VS/test change default search provider

--- tests/address_bar_and_search/test_default_search_provider_change.py
+++ tests/address_bar_and_search/test_default_search_provider_change.py
@@ -1,14 +1,15 @@
+from time import sleep
+
 import pytest
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import Firefox
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from modules.page_object import AboutPrefs, AboutConfig
 
 from modules.browser_object import Navigation
 from modules.browser_object_context_menu import ContextMenu
-from time import sleep
+from modules.page_object import AboutConfig, AboutPrefs
 
 
 @pytest.fixture()
@@ -16,6 +17,7 @@ def add_prefs():
     return [
         ("browser.search.region", "US"),
     ]
+
 
 def test_default_search_provider_change(driver: Firefox):
     """
@@ -29,7 +31,6 @@ def test_default_search_provider_change(driver: Firefox):
     search_term = "what is life?"
 
     # enable search bar via about:config
-    #about_config.change_pref_value("cookiebanners.service.mode", 1)
     pref = "browser.search.widget.inNavBar"
     about_config.toggle_true_false_config(pref)
     nav.clear_awesome_bar()
@@ -44,7 +45,6 @@ def test_default_search_provider_change(driver: Firefox):
     # switch to the second tab
     driver.switch_to.window(driver.window_handles[1])
     sleep(1)
-
 
     # check that the current URL is about:preferences#search
     assert driver.current_url == "about:preferences#search"
@@ -64,7 +64,9 @@ def test_default_search_provider_change(driver: Firefox):
 
     # Open the search bar and type in a keyword and check if it's with the right provider
     nav.type_in_search_bar(search_term)
-    search_engine_name_element = driver.find_element(By.CSS_SELECTOR, '.searchbar-engine-name')
+    with driver.context(driver.CONTEXT_CHROME):
+        search_engine_name_element = driver.find_element(
+            By.CSS_SELECTOR, ".searchbar-engine-name"
+        )
 
-    assert search_engine_name_element.text == "DuckDuckGo"
-    sleep(4)
+        assert search_engine_name_element.get_attribute("value") == "DuckDuckGo Search"
