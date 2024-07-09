d7c277676c7558d1e762ba244d9796ec6a1cddd2
Hani Yacoub <hani.yacoub@svadmins-MacBook-Pro.local>
2024-07-09 18:10:45 +0300

    Removing logging

--- tests/address_bar_and_search/test_sap_google_adclick.py
+++ tests/address_bar_and_search/test_sap_google_adclick.py
@@ -44,7 +44,6 @@ def test_sap_google_adclick(driver: Firefox):
 
     # Verify pings are recorded
     json_data = u.decode_url(driver)
-    logging.warn(json_data)
     assert u.assert_json_value(
         json_data, '$..keyedScalars.["browser.search.adclicks.urlbar"].["google:tagged"]', 1
     )
