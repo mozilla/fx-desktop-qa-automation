a2f2572d6f577e6c71ed7dd727d5e3327aa88a96
Sarina Li <sarinajin.li@gmail.com>
2024-07-10 15:16:43 -0400

    formatting

--- tests/language_packs/test_language_pack_install_preferences.py
+++ tests/language_packs/test_language_pack_install_preferences.py
@@ -5,6 +5,7 @@ from selenium.webdriver import Firefox
 from modules.page_object import AboutPrefs
 from modules.util import BrowserActions
 
+
 def test_language_pack_install_about_preferences(driver: Firefox):
     """
     C1549409: language packs can be installed from about:preferences and firefox is correctly localized
@@ -16,9 +17,13 @@ def test_language_pack_install_about_preferences(driver: Firefox):
     iframe = about_prefs.get_element("browser-popup")
     ba.switch_to_iframe_context(iframe)
     about_prefs.get_element("language-set-alternatives-popup-select-lanuages").click()
-    about_prefs.element_clickable("language-set-alternatives-popup-select-lanuages-search-more")
+    about_prefs.element_clickable(
+        "language-set-alternatives-popup-select-lanuages-search-more"
+    )
 
     with driver.context(driver.CONTEXT_CHROME):
-        select_more = about_prefs.get_element("language-set-alternatives-popup-select-lanuages-search-more")
+        select_more = about_prefs.get_element(
+            "language-set-alternatives-popup-select-lanuages-search-more"
+        )
         select_more.click()
     sleep(10)
