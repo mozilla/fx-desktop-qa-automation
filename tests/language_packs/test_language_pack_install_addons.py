2bb50b713ed89b37fefcf1b326bb48c6747483a5
Sarina Li <sarinajin.li@gmail.com>
2024-07-10 16:16:02 -0400

    formatting

--- tests/language_packs/test_language_pack_install_addons.py
+++ tests/language_packs/test_language_pack_install_addons.py
@@ -2,10 +2,11 @@ from time import sleep
 
 from selenium.webdriver import Firefox
 
-from modules.page_object import AboutPrefs, AboutAddons, Navigation
+from modules.page_object import AboutAddons, AboutPrefs, Navigation
+
 
 def test_language_pack_install_from_addons(driver: Firefox):
-    """"
+    """ "
     C1549408: verify that installing a language pack from about:addons will correctly change the locale
     """
     about_prefs = AboutPrefs(driver, category="general")
