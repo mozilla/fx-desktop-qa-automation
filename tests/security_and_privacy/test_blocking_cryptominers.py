ab9475370fdc2a53e5dbfef5edc13ab5ad6c845f
Hani Yacoub <hani.yacoub@svadmins-MacBook-Pro.local>
2024-07-12 12:47:01 +0300

    Test blocking cryptominers

---
+++ tests/security_and_privacy/test_blocking_cryptominers.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium.webdriver import Firefox
+
+from modules.browser_object_navigation import Navigation
+from modules.page_object_about_prefs import AboutPrefs
+
+url = "https://senglehardt.com/test/trackingprotection/test_pages/fingerprinting_and_cryptomining.html"
+
+
+def test_blocking_cryptominers(driver: Firefox):
+    # instantiate objects
+    nav = Navigation(driver).open()
+    about_prefs = AboutPrefs(driver, category="privacy").open()
+
+    # Select custom option and keep just cryptominers checked
+    about_prefs.get_element("custom-radio").click()
+    about_prefs.get_element("cookies-checkbox").click()
+    about_prefs.get_element("tracking-checkbox").click()
+    about_prefs.get_element("known-fingerprints-checkbox").click()
+    about_prefs.get_element("suspected-fingerprints-checkbox").click()
+
+    # Access url and click on the shield icon and verify that cryptominers are blocked
+    driver.get(url)
+    with driver.context(driver.CONTEXT_CHROME):
+        nav.get_element("shield-icon").click()
+        assert nav.get_element("cryptominers").is_displayed()
