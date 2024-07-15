9fff934b8f313a4e050805e41ae51804c20867d2
soncuteanca <soncutean.anca@gmail.com>
2024-07-15 15:15:07 +0300

    Revert to my previos code

--- tests/security_and_privacy/test_https_enabled_private_browsing.py
+++ tests/security_and_privacy/test_https_enabled_private_browsing.py
@@ -1,7 +1,5 @@
 import pytest
-from selenium.common import TimeoutException
 from selenium.webdriver import Firefox
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 HTTP_URL = "http://example.com"
@@ -23,15 +21,8 @@ def test_https_first_mode_in_private_browsing(driver: Firefox):
     # Navigate to the HTTP URL
     driver.get(HTTP_URL)
 
-    try:
-        # Wait for the URL to be redirected to HTTPS
-        WebDriverWait(driver, 10).until(EC.url_contains("https://example.com/"))
-    except TimeoutException:
-        pytest.fail(
-            f"Timed out waiting for URL to switch to HTTPS: {driver.current_url}"
-        )
-
-        # Assertion to ensure URL starts with HTTPS
-    assert driver.current_url.startswith(
-        "https://"
-    ), f"Final URL should be 'https://example.com/', but was: {driver.current_url}"
+    # Wait for the URL to be redirected to HTTPS
+    assert WebDriverWait(driver, 10).until(
+        lambda d: d.current_url.startswith("https://"),
+        message=f"Final URL should use HTTPS, but was: {driver.current_url}",
+    )
