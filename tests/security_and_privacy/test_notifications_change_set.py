1786136b7d1b1bdbd521fc9fbbe6bf73bce54922
Sarina Li <sarinajin.li@gmail.com>
2024-07-15 15:10:11 -0400

    add permission checking

--- tests/security_and_privacy/test_notifications_change_set.py
+++ tests/security_and_privacy/test_notifications_change_set.py
@@ -8,8 +8,8 @@ from modules.util import BrowserActions, Utilities
 
 NOTIFICATIONS_SITE = "https://www.bennish.net/web-notifications.html"
 associated_labels = [
-    ("notifications-allow-button", "Allow"),
-    ("notifications-block-button", "Block"),
+    ("notifications-allow-button", "Allow", "granted"),
+    ("notifications-block-button", "Block", "denied"),
 ]
 
 
@@ -18,8 +18,8 @@ def add_prefs():
     return []
 
 
-@pytest.mark.parametrize("button_data, button_text", associated_labels)
-def test_notifications_allow(driver: Firefox, button_data: str, button_text: str):
+@pytest.mark.parametrize("button_data, button_text, permission", associated_labels)
+def test_notifications_allow(driver: Firefox, button_data: str, button_text: str, permission: str):
     """
     C159150: verifies that changing different settings allows for notifcations to be blocked
     """
@@ -33,11 +33,13 @@ def test_notifications_allow(driver: Firefox, button_data: str, button_text: str
     with driver.context(driver.CONTEXT_CHROME):
         about_prefs.get_element(button_data).click()
 
-        check_notification_script = """
+    check_notification_script = """
     return Notification.permission;
 """
     permission_status = driver.execute_script(check_notification_script)
+
     logging.info(f"Notification permission status: {permission_status}")
+    assert permission_status == permission
 
     about_prefs.open()
     about_prefs.get_element("permissions-notifications-button").click()
