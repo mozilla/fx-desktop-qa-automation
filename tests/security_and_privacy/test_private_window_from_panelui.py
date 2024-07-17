4fd9fb8b3871f72c23c76a016b20689ada002ef3
Ben Chatterton <bchatterton@mozilla.com>
2024-07-17 07:50:06 -0700

    use new method

--- tests/security_and_privacy/test_private_window_from_panelui.py
+++ tests/security_and_privacy/test_private_window_from_panelui.py
@@ -9,5 +9,5 @@ def test_private_window_from_panelui(driver: Firefox):
     panelui = PanelUi(driver).open_panel_menu()
     panelui.select_panel_setting("new-private-window-option")
     panelui.wait_for_num_windows(2)
-    panelui.switch_to_new_tab()
+    panelui.switch_to_new_window()
     AboutPrivatebrowsing(driver).wait_for_page_to_load()
