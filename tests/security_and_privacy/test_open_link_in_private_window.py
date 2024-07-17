bbf673c8fb1a86045b7753240f43c58c0e909ce0
Ben Chatterton <bchatterton@mozilla.com>
2024-07-17 13:56:35 -0700

    write test

---
+++ tests/security_and_privacy/test_open_link_in_private_window.py
@@ -0,0 +1,18 @@
+from selenium.webdriver import Firefox
+
+from modules.browser_object import ContextMenu, Navigation
+from modules.page_object import ExamplePage
+
+
+def test_open_link_in_private_window(driver: Firefox):
+    """C101662 - Links can be successfully opened in a Private Browsing session"""
+    example = ExamplePage(driver).open()
+    context_menu = ContextMenu(driver)
+    nav = Navigation(driver)
+
+    example.context_click("more-information")
+    context_menu.click_context_item("context-menu-open-link-in-new-private-window")
+    nav.wait_for_num_windows(2)
+    nav.switch_to_new_window()
+    with driver.context(driver.CONTEXT_CHROME):
+        pass
