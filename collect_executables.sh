ad4dc129e125800e7fdfec3850e6c6882df750f0
Ben Chatterton <bchatterton@mozilla.com>
2024-07-22 09:37:44 -0700

    remove debug, try to find broken arch

--- collect_executables.sh
+++ collect_executables.sh
@@ -44,13 +44,10 @@ fi
 
 # Find the version of Geckodriver that matches arch
 FILENAME="-${SYSTEM_NAME}${BITS}${ARCH}.${EXT}"
-echo "$FILENAME"
-
 # 20 is arbitrary and may break if future releases of Geckodriver have more than 20 channels
 for i in {0..20}
 do
     GECKO_LINK=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq ".[\"assets\"][${i}][\"browser_download_url\"]" | tr -d '"')
-    echo "$GECKO_LINK"
     if [[ $GECKO_LINK == *"${FILENAME}"* ]] && [[ $GECKO_LINK != *".asc" ]]
     then
         curl -OL "$GECKO_LINK"
