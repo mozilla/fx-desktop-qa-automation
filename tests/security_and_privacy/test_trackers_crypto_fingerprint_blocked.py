a58efde0e0e8bbad490df5ceae448c3bbdeb034c
Sarina Li <sarinajin.li@gmail.com>
2024-07-24 10:16:08 -0400

    format

--- tests/security_and_privacy/test_trackers_crypto_fingerprint_blocked.py
+++ tests/security_and_privacy/test_trackers_crypto_fingerprint_blocked.py
@@ -1,7 +1,3 @@
-import logging
-from time import sleep
-
-import pytest
 from selenium.webdriver import Firefox
 
 from modules.browser_object import Navigation, TrackerPanel
@@ -14,44 +10,55 @@ def test_cross_site_trackrs_crypto_fingerprinter_blocked(driver: Firefox):
     """
     C446393: Ensures that some trackers are blocked on certain website
     """
+    # instantiate objs
     tracker_page = GenericPage(driver, url=TRACKER_URL)
     tracker_panel = TrackerPanel(driver)
     nav = Navigation(driver).open()
 
-    tracker_page.open()
+    # wait for the shield icon
     tracker_page.open()
     tracker_panel.wait_for_blocked_tracking_icon(nav, tracker_page)
     nav.open_tracker_panel()
 
+    # get children
     driver.set_context(driver.CONTEXT_CHROME)
     tracker_item_container = tracker_panel.get_element("tracking-item-container")
     all_tracking_items = tracker_panel.get_all_children(tracker_item_container)
 
-    blocked = set()
-    add_blocked = False
-    allowed = set()
-    add_allowed = False
+    # instantiate test specific objs
+    blocked = set(["Cross-Site Tracking Cookies", "Fingerprinters", "Cryptominers"])
+    allowed = set(["Tracking Content"])
+    rm_blocked = False
+    rm_allowed = False
 
     for item in all_tracking_items:
-        # encounter a blocked header (everything after it is blocked in the list)
-        if item.get_attribute("id") == "protections-popup-blocking-section-header":
-            add_allowed = False
-            add_blocked = True
-        elif (
-            item.get_attribute("id") == "protections-popup-not-blocking-section-header"
-        ):
-            add_allowed = True
-            add_blocked = False
-        else:
-            child_labels = tracker_panel.get_element(
-                "tracking-item-container-label", multiple=True, parent_element=item
-            )
-            for child in child_labels:
-                label = child.get_attribute("innerHTML")
-                if add_blocked:
-                    blocked.add(label)
-                elif add_allowed:
-                    allowed.add(label)
+        # encounter a header (everything after it is blocked/allowed in the list)
+        inner_html = item.get_attribute("innerHTML")
+        if "Blocked" in inner_html:
+            rm_blocked = True
+            continue
+        elif "Allowed" in inner_html:
+            rm_allowed = True
+            rm_blocked = False
+            continue
 
-    logging.info(blocked)
-    logging.info(allowed)
+        # assign a temp memory location, assign the set to it depending on the header seen earlier
+        rm_set = set()
+        to_rm_item = ""
+        if rm_blocked:
+            rm_set = blocked
+        elif rm_allowed:
+            rm_set = allowed
+
+        # go through the possible strings to look for, remove it if appropriate
+        for item in rm_set:
+            if item in inner_html:
+                to_rm_item = item
+                break
+        if to_rm_item != "":
+            rm_set.remove(to_rm_item)
+
+    # ensure that we have seen all of the expected items in each section
+    assert (
+        len(blocked) == 0 and len(allowed) == 0
+    ), "Not all of the expected items were found in each of the Blocked/Allowed sections."
