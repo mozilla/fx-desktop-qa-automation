ed2f32cae1b72478b63e5ed4513ee416746dc02f
Ben Chatterton <bchatterton@mozilla.com>
2024-07-26 15:53:41 -0700

    update gha flow and tests

--- tests/security_and_privacy/test_cookies_not_saved_private_browsing.py
+++ tests/security_and_privacy/test_cookies_not_saved_private_browsing.py
@@ -1,12 +1,10 @@
-import pytest
 from selenium.webdriver import Firefox
 
 from modules.browser_object import Navigation, PanelUi
-from modules.page_object import AboutPrefs, GoogleSearch
+from modules.page_object import AboutPrefs, GenericPage
 from modules.util import BrowserActions
 
 
-@pytest.mark.unstable
 def test_cookies_not_saved_private_browsing(driver: Firefox):
     """
     C101677: ensure that cookies are not saved after using private browsing
@@ -15,18 +13,21 @@ def test_cookies_not_saved_private_browsing(driver: Firefox):
     about_prefs = AboutPrefs(driver, category="privacy")
     panel_ui = PanelUi(driver).open()
     nav = Navigation(driver)
-    google_search = GoogleSearch(driver)
+    wiki_page = GenericPage(
+        driver, url="https://ro.wikipedia.org/wiki/Pagina_principal%C4%83"
+    )
     ba = BrowserActions(driver)
 
     # open new private window
     panel_ui.open_private_window()
     nav.switch_to_new_window()
 
-    # open the google page and perform a search
-    google_search.open()
-    google_search.type_in_search_bar("hello")
-    google_search.press_enter_search_bar()
-    google_search.wait_for_page_to_load()
+    # open the wiki page and perform a search
+    wiki_page.open()
+    wiki_search_bar = wiki_page.get_element("wiki-search-bar")
+    wiki_search_bar.send_keys("hello")
+    wiki_page.get_element("wiki-search-button").click()
+    wiki_page.wait_for_page_to_load()
 
     # close the page and switch to first tab
     driver.close()
