1839a7d2e908be798fe0e8badad7d4e10d2b00c6
Ben Chatterton <bchatterton@mozilla.com>
2024-07-29 10:28:00 -0700

    fix bad window switch

--- tests/security_and_privacy/test_downloads_from_private_not_leaked.py
+++ tests/security_and_privacy/test_downloads_from_private_not_leaked.py
@@ -45,12 +45,16 @@ def delete_files(sys_platform):
 def test_downloads_from_private_not_leaked(driver: Firefox, delete_files):
     """C101674 - Downloads initiated from a private window are not leaked to the non-private window"""
 
-    # We're going to assume no downloads as every test is run in a new instance
+    # We've deleted relevant downloads just to be safe
     non_private_window = driver.current_window_handle
     panelui = PanelUi(driver).open_panel_menu()
     panelui.select_panel_setting("new-private-window-option")
     panelui.wait_for_num_windows(2)
-    panelui.switch_to_new_window()
+
+    # Using this instead of switch_to_new_window, suspect it may be broken
+    original_window_idx = driver.window_handles.index(non_private_window)
+    private_window = driver.window_handles[1 - original_window_idx]
+    driver.switch_to.window(private_window)
 
     about_downloads = AboutDownloads(driver)
     about_downloads.open()
