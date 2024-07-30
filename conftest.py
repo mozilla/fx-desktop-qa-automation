8c415b6242509f60efc6e6bda80af95540e87b70
Sarina Li <sarinajin.li@gmail.com>
2024-07-30 09:58:17 -0400

    add stderr

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import sys
 import datetime
 import logging
 import os
@@ -64,7 +65,7 @@ def pytest_exception_interact(node, call, report):
         try:
             test_name = node.name
             logging.info(f"Handling exception for test: {test_name}")
-            print(f"NODE LOGS HERE {node.funcargs}")
+            print(f"NODE LOGS HERE {node.funcargs}", file=sys.stderr)
             driver = node.funcargs["driver"]
             opt_ci = node.funcargs["opt_ci"]
             if driver:
