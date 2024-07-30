87da6c5ae55ba9e00a7eaee6a9f1d0ac02b309a3
Sarina Li <sarinajin.li@gmail.com>
2024-07-30 15:42:37 -0400

    POM/BOM for now

---
+++ modules/browser_object_pdf_viewer.py
@@ -0,0 +1,8 @@
+
+from modules.page_base import BasePage
+
+class PdfViewer(BasePage):
+    """
+    BOM for PDF viewer
+    """
+    URL_TEMPLATE = ""
---
+++ modules/page_object_about_crash.py
@@ -0,0 +1,8 @@
+
+from modules.page_base import BasePage
+
+class AboutCrashes(BasePage):
+    """
+    POM for about:crashes page
+    """
+    URL_TEMPLATE = "about:crashes"
