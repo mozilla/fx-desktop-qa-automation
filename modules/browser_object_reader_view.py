9cee0adede8a876434085f28df643efb08460625
Sarina Li <sarinajin.li@gmail.com>
2024-07-31 12:09:47 -0400

    format

--- modules/browser_object_reader_view.py
+++ modules/browser_object_reader_view.py
@@ -1,3 +1,5 @@
+from selenium.webdriver import Keys
+
 from modules.page_base import BasePage
 
 
@@ -7,3 +9,77 @@ class ReaderView(BasePage):
     """
 
     URL_TEMPLATE = ""
+
+    def open_reader_view_searchbar(self) -> BasePage:
+        """
+        Opens the reader view using the search bar
+        """
+        with self.driver.context(self.driver.CONTEXT_CHROME):
+            self.get_element("reader-view-button").click()
+        self.wait_for_reader_view_open()
+        return self
+
+    def open_reader_view_keys(self) -> BasePage:
+        """
+        Opens the reader view using keys
+        """
+        if self.sys_platform() == "Darwin":
+            self.actions.key_down(Keys.COMMAND).key_down(Keys.ALT).send_keys(
+                "r"
+            ).key_up(Keys.ALT).key_up(Keys.COMMAND).perform()
+        elif self.sys_platform() == "Linux":
+            self.actions.key_down(Keys.CONTROL).key_down(Keys.ALT).send_keys(
+                "r"
+            ).key_up(Keys.ALT).key_up(Keys.CONTROL).perform()
+        else:
+            self.actions.send_keys(Keys.F9).perform()
+        self.wait_for_reader_view_open()
+        return self
+
+    def close_reader_view_searchbar(self) -> BasePage:
+        """
+        Closes the reader view using the search bar
+        """
+        with self.driver.context(self.driver.CONTEXT_CHROME):
+            self.get_element("reader-view-button").click()
+        self.wait_for_reader_view_closed()
+        return self
+
+    def close_reader_view_toolbar(self) -> BasePage:
+        """
+        Closes the reader view using the toolbar
+        """
+        self.get_element("reader-toolbar-close").click()
+        self.wait_for_reader_view_closed()
+        return self
+
+    def close_reader_view_keys(self, sys_platform: str) -> BasePage:
+        """
+        Closes the reader view using keys
+        """
+        if sys_platform == "Darwin":
+            self.actions.key_down(Keys.COMMAND).key_down(Keys.ALT).send_keys(
+                "r"
+            ).key_up(Keys.ALT).key_up(Keys.COMMAND).perform()
+        elif sys_platform == "Linux":
+            self.actions.key_down(Keys.CONTROL).key_down(Keys.ALT).send_keys(
+                "r"
+            ).key_up(Keys.ALT).key_up(Keys.CONTROL).perform()
+        else:
+            self.actions.send_keys(Keys.F9).perform()
+        self.wait_for_reader_view_closed()
+        return self
+
+    def wait_for_reader_view_open(self) -> BasePage:
+        """
+        Checks to see if the reader view toolbar is present demonstrating that the reader view is open.
+        """
+        self.wait.until(lambda _: self.element_exists("reader-toolbar"))
+        return self
+
+    def wait_for_reader_view_closed(self) -> BasePage:
+        """
+        Checks to see if the reader view toolbar is not present, demonstrating that reader view is not open.
+        """
+        self.wait.until(lambda _: self.element_does_not_exist("reader-toolbar"))
+        return self
