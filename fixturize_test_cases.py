8cb2874e0d0426a13c395ca4a84ca5fab9a66c34
Ben Chatterton <bchatterton@mozilla.com>
2024-08-06 12:28:29 -0700

    finish script

--- fixturize_test_cases.py
+++ fixturize_test_cases.py
@@ -8,7 +8,8 @@ for root, _, files in os.walk("tests"):
         if file.startswith("test") and file.endswith(".py"):
             case_num = None
             filepath = os.path.join(root, file)
-            for line in open(filepath).readlines():
+            script = open(filepath).readlines()
+            for line in script:
                 m = TEST_CASE_RE.match(line.strip())
                 if not m:
                     continue
@@ -17,16 +18,38 @@ for root, _, files in os.walk("tests"):
                     print(f"Found multiple case_nums: {case_num} and {m[2]}")
                 case_num = m[2]
             if case_num:
-                with open(filepath, "a") as fh:
+                with open("filepath", "w") as fh:
                     lines = "\n".join(
                         [
-                            "import pytest",
+                            "",
                             "@pytest.fixture()",
                             "def test_case():",
-                            f'    return "{case_num}"',
+                            f'    return "{case_num}"\n',
+                            "",
                         ]
                     )
-                    # fh.write(lines)
+                    # fh.write("import pytest\n")
+                    print("import pytest")
+                    imports = True
+                    for i in range(len(script)):
+                        line = script[i]
+                        if (
+                            not (
+                                len(line.strip()) == 0
+                                or line.strip().startswith("'")
+                                or line.strip().startswith('"')
+                                or line.strip().startswith("#")
+                                or line.strip().startswith("import")
+                                or line.strip().startswith("from")
+                            )
+                            and imports
+                        ):
+                            # fh.write(lines)
+                            print(lines)
+                            imports = False
+                        else:
+                            # fh.write(f"{line}")
+                            print(line)
                     # print(filepath)
                     # print(lines)
                     # print("-=----=-")
