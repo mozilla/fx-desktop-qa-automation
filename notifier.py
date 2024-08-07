8970bb562d9825bedf79f642cc10b5b132edcbe3
Sarina Li <sarinajin.li@gmail.com>
2024-08-07 16:07:20 -0400

    remove a space

--- notifier.py
+++ notifier.py
@@ -1,47 +1,44 @@
-from google.cloud import storage
-
-
 # def write_read():
 #     """Write and read a blob from GCS using file-like IO"""
 #     # The ID of your GCS bucket
 #     bucket_name = "notifier-artifact-bucket"
-
 #     # The ID of your new GCS object
-#     blob_name = "new_file"
-
-#     storage_client = storage.Client()
+#     blob_name = "new_folder/new_file.txt"
+#     # Path to your service account key file
+#     key_path = "credentials.json"
+#     # Load credentials from the service account key file
+#     credentials = service_account.Credentials.from_service_account_file(key_path)
+#     # Initialize the client with explicit credentials
+#     storage_client = storage.Client(credentials=credentials)
 #     bucket = storage_client.bucket(bucket_name)
 #     blob = bucket.blob(blob_name)
-
-#     # Mode can be specified as wb/rb for bytes mode.
-#     # See: https://docs.python.org/3/library/io.html
-#     with blob.open("w") as f:
+#     # Set the Content-Type before writing
+#     blob.content_type = 'text/plain'
+#     # Write data to the blob
+#     with blob.open("w", content_type='text/plain') as f:
 #         f.write("Hello world")
-
-#     with blob.open("r") as f:
-#         print(f.read())
-
 # write_read()
+import os
 
-# import os
+from google.cloud import storage
+from google.oauth2 import service_account
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-# from slack_sdk import WebClient
-# from slack_sdk.errors import SlackApiError
+# Your OAuth access token
+token = os.getenv("SLACK_KEY")
 
-# # Your OAuth access token
-# token = os.getenv("SLACK_KEY")
+# Initialize a Web API client
+client = WebClient(token=token)
 
-# # Initialize a Web API client
-# client = WebClient(token=token)
-
-# try:
-#     response = client.chat_postMessage(
-#         channel="#desktop-qa-monitoring-notifier",  # Channel ID or name (e.g., #general)
-#         text="from actions... 1",
-#     )
-#     print("i have sent the message.")
-# except SlackApiError as e:
-#     print(f"Error sending message: {e.response['error']}")
+try:
+    response = client.chat_postMessage(
+        channel="#desktop-qa-monitoring-notifier",  # Channel ID or name (e.g., #general)
+        text="from ACTIONS... 1",
+    )
+    print("i have sent the message.")
+except SlackApiError as e:
+    print(f"Error sending message: {e.response['error']}")
 
 
 # {
