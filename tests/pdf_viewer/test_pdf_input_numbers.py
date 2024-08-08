96473ba316d85669f55a1ce0028b516c78a6686b
vsangereanMOZ <vsangerean@mozilla.com>
2024-08-08 17:05:46 +0300

    vs/test-pdf-input-numbers

--- tests/pdf_viewer/test_pdf_input_numbers.py
+++ tests/pdf_viewer/test_pdf_input_numbers.py
@@ -1,10 +1,7 @@
 import time
-
 import pytest
-
 from selenium.webdriver import Firefox
-
-from modules.page_object import GenericPdf
+from modules.page_object_generics import GenericPdf
 
 
 @pytest.mark.headed
@@ -15,25 +12,27 @@ def test_pdf_input_numbers(driver: Firefox, fillable_pdf_url: str):
     from pynput.keyboard import Controller, Key
 
     pdf = GenericPdf(driver, pdf_url=fillable_pdf_url).open()
+    keyboard = Controller()
 
     numeric_field = pdf.get_element("zipcode-field")
 
     # Test value to input in the field
-    test_value = '12345'
+    test_value = "12345"
 
     # Clear the field and enter the test value
     numeric_field.clear()
     numeric_field.send_keys(test_value)
 
     # Press Tab to move to the next field (if applicable)
-    numeric_field.send_keys(Key.tab)
+    keyboard.release(Key.tab)
 
     # Allow time for the field to update
     time.sleep(2)
 
     # Verify the value is still present
-    value = numeric_field.get_attribute('value')
+    value = numeric_field.get_attribute("value")
     assert value == test_value, f"Expected value {test_value}, but got {value}."
 
     print(
-        "Test passed: Numeric field with id 'pdfjs_internal_id_666R' has been filled correctly and the value persists.")
+        "Test passed: Numeric field has been filled correctly and the value persists."
+    )
