94627f185e7fa56c56802c3c49276c371158af9b
soncuteanca <soncutean.anca@gmail.com>
2024-08-08 19:50:36 +0300

    conftest

--- tests/pdf_viewer/test_pdf_download.py
+++ tests/pdf_viewer/test_pdf_download.py
@@ -2,8 +2,8 @@ import os
 import time
 
 import pytest
-
 from selenium.webdriver import Firefox
+
 from modules.page_object import GenericPdf
 
 
@@ -13,7 +13,9 @@ def add_prefs():
 
 
 @pytest.mark.headed
-def test_pdf_download(driver: Firefox, fillable_pdf_url: str, downloads_folder: str, sys_platform):
+def test_pdf_download(
+    driver: Firefox, fillable_pdf_url: str, downloads_folder: str, sys_platform
+):
     """
     C3932: PDF files can be successfully downloaded via pdf.js
     """
@@ -57,6 +59,10 @@ def test_pdf_download(driver: Firefox, fillable_pdf_url: str, downloads_folder: 
     saved_pdf_location = os.path.join(downloads_folder, file_name)
 
     # Verify if the file exists
-    assert os.path.exists(saved_pdf_location), f"The file was not downloaded to {saved_pdf_location}."
+    assert os.path.exists(
+        saved_pdf_location
+    ), f"The file was not downloaded to {saved_pdf_location}."
 
-    print(f"Test passed: The file {file_name} has been downloaded and is present at {saved_pdf_location}.")
+    print(
+        f"Test passed: The file {file_name} has been downloaded and is present at {saved_pdf_location}."
+    )
