514217dd457063eac73a13d0517251f7d4b78e5f
Ben Chatterton <bchatterton@mozilla.com>
2024-08-08 13:23:27 -0700

    no more pynput

--- tests/pdf_viewer/test_download_triggered_on_content_disposition_attachment.py
+++ tests/pdf_viewer/test_download_triggered_on_content_disposition_attachment.py
@@ -1,8 +1,8 @@
 from time import sleep
 
-import pytest
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from modules.browser_object_navigation import Navigation
 from modules.browser_object_tabbar import TabBar
@@ -13,33 +13,31 @@ CONTENT_DISPOSITION_ATTACHMENT_URL = (
 )
 
 
-@pytest.mark.headed
 def test_download_panel_triggered_on_content_disposition_attachment(driver: Firefox):
     """
     C936502: Ensure that the Always ask option in Firefox Applications settings
     triggers the download panel for PDFs with Content-Disposition: attachment.
     """
 
-    from pynput.keyboard import Controller, Key
-
-    keyboard = Controller()
     tabs = TabBar(driver)
     nav = Navigation(driver)
     about_prefs = AboutPrefs(driver, category="general").open()
 
-    about_prefs.get_element("pdf-content-type").click()
-    about_prefs.get_element("pdf-open-in-firefox").click()
-    sleep(3)
-    keyboard.press(Key.down)
-    keyboard.press(Key.enter)
+    about_prefs.find_in_settings("appl")
+    about_prefs.click_on("pdf-content-type")
+    about_prefs.click_on("pdf-actions-menu")
+    menu = about_prefs.get_element("pdf-actions-menu")
+    menu.send_keys(Keys.DOWN)
+    menu.send_keys(Keys.ENTER)
+    about_prefs.wait.until(lambda _: menu.get_attribute("label") == "Always ask")
 
     nav.search(CONTENT_DISPOSITION_ATTACHMENT_URL)
     sleep(3)
     with driver.context(driver.CONTEXT_CHROME):
         driver.switch_to.window(driver.window_handles[-1])
-
         driver.find_element(By.ID, "handleInternally").click()
-        keyboard.press(Key.enter)
+        sleep(2)
+        driver.find_element(By.ID, "unknownContentTypeWindow").send_keys(Keys.ENTER)
 
     tabs.wait_for_num_tabs(2)
     assert (
