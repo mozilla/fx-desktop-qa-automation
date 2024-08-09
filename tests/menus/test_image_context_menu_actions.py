e4bc05b2ee445011ff4d8a48d6fa6149a80b10b3
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-08-09 12:45:13 -0700

    CI and QoL Improvements (#174)
    * factor out delete files fixture
    
    * factor pynput out of utils
    
    * lock pipenv
    
    * new collect_ex; test in windows first
    
    * test unstable; retry win
    
    * try win again
    
    * get non-AMD bins for win GHA
    
    * set new collect for Mac and Linux
    
    * set new collect for Mac and Linux
    
    * retry ping for geckodriver
    
    * refactor collect py
    
    * lint
    
    * backstop for failed mac gecko pull

--- tests/menus/test_image_context_menu_actions.py
+++ tests/menus/test_image_context_menu_actions.py
@@ -47,14 +47,19 @@ def test_save_image_as(driver: Firefox):
     C2637622.2: save image as
     """
     try:
-        from pynput.keyboard import Key
+        from pynput.keyboard import Controller, Key
     except ModuleNotFoundError:
         pytest.skip("Could not load pynput")
+
+    controller = Controller()
+
+    def key_press_release(key: Key):
+        controller.press(key)
+        controller.release(key)
 
     wiki_image_page = GenericPage(driver, url=LINK_IMAGE_URL).open()
     image_context_menu = ImageContextMenu(driver)
     nav = Navigation(driver)
-    ba = BrowserActions(driver)
     util = Utilities()
 
     # wait for page to load
@@ -75,22 +80,22 @@ def test_save_image_as(driver: Firefox):
 
     this_platform = platform.system()
     if this_platform == "Linux":
-        ba.controller.press(Key.alt)
-        ba.controller.press(Key.tab)
-        ba.controller.release(Key.tab)
-        ba.controller.release(Key.alt)
+        controller.press(Key.alt)
+        controller.press(Key.tab)
+        controller.release(Key.tab)
+        controller.release(Key.alt)
 
-        ba.controller.press(Key.alt)
-        ba.controller.press(Key.tab)
-        ba.controller.release(Key.tab)
-        ba.controller.release(Key.alt)
+        controller.press(Key.alt)
+        controller.press(Key.tab)
+        controller.release(Key.tab)
+        controller.release(Key.alt)
 
-        ba.key_press_release(Key.tab)
+        key_press_release(Key.tab)
 
-        ba.key_press_release(Key.tab)
+        key_press_release(Key.tab)
 
     # Press and release the Enter key
-    ba.key_press_release(Key.enter)
+    key_press_release(Key.enter)
 
     # Wait for the animation to complete
     nav.wait_for_download_animation_finish(downloads_button)
