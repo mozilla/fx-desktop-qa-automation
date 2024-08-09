e881048beec9182acd37fffec2cfc4d3299c759e
Ben Chatterton <bchatterton@mozilla.com>
2024-08-09 08:57:42 -0700

    set new collect for Mac and Linux

--- collect_executables.sh
+++ collect_executables.sh
@@ -4,10 +4,11 @@ UNAME_A=$(uname -a)
 
 if [[ "$UNAME_A" == *"Darwin"* ]]
 then
-    curl -o Firefox.dmg -L "$(python collect_executables.py)"
+    curl -o Firefox.dmg -L "$(pipenv run python collect_executables.py)"
+    hdiutil attach Firefox.dmg
 else
-    curl -o firefox.tar.bz2 -L "$(python collect_executables.py)"
+    curl -o firefox.tar.bz2 -L "$(pipenv run python collect_executables.py)"
     tar -xvjf firefox.tar.bz2
 fi
-curl -o geckodriver.tar.gz -L "$(python collect_executables.py -g)"
+curl -o geckodriver.tar.gz -L "$(pipenv run python collect_executables.py -g)"
 tar -xvzf geckodriver.tar.gz
