21bc07a3bfa355081b0840cca80fa399a572e391
Sarina Li <sarinajin.li@gmail.com>
2024-08-12 11:45:01 -0400

    remove a file

--- filename_generator.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def generate_filename():
-    """Generates a filename with the current date and time in the specified format."""
-    current_time = datetime.datetime.now()
-    # Format the date and time as YYYY-mm-dd_HHmm_SS
-    filename = current_time.strftime("%Y-%m-%d_%H%M_%S")
-    return f"report_{filename}.html"
