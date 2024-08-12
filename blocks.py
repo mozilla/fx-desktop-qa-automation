ac7a82bc2e931e568091521d05425c4b015e511d
Sarina Li <sarinajin.li@gmail.com>
2024-08-12 11:11:16 -0400

    use single quotes

--- blocks.py
+++ blocks.py
@@ -1,9 +1,10 @@
 import os
 
+
 def return_slack_blocks(report_file: str):
-    task_text = f"*Task*: {os.getenv("GITHUB_REF_NAME")}"
-    owner_text = f"*Owner*: {os.getenv("GITHUB_ACTOR")}"
-    commit_text = f"*Commit*: https://github.com/mozilla/fx-desktop-qa-automation/commit/{os.getenv("GITHUB_SHA")}"
+    task_text = f"*Task*: {os.getenv('GITHUB_REF_NAME')}"
+    owner_text = f"*Owner*: {os.getenv('GITHUB_ACTOR')}"
+    commit_text = f"*Commit*: https://github.com/mozilla/fx-desktop-qa-automation/commit/{os.getenv('GITHUB_SHA')}"
     test_summary_text = f"*Test Summary*: {report_file} :debug:"
 
     return [
