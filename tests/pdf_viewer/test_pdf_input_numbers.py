1025f5e08245782274f9ed874b56893f300bcb65
vsangereanMOZ <vsangerean@mozilla.com>
2024-08-13 17:30:37 +0300

    vs/test-input-fields-pdf

--- tests/pdf_viewer/test_pdf_input_numbers.py
+++ tests/pdf_viewer/test_pdf_input_numbers.py
@@ -1,20 +1,34 @@
-import logging
-from time import sleep
+import time
+import os
 
 import pytest
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 
-from modules.browser_object import TabBar
 from modules.page_object import GenericPdf
 
 
-def test_pdf_input_numbers(driver: Firefox, fillable_pdf_url: str):
+@pytest.fixture()
+def delete_files_regex_string():
+    return r"i-9.*\.pdf"
+
+
+@pytest.mark.headed
+def test_pdf_input_numbers(driver: Firefox,
+                           fillable_pdf_url: str,
+                           downloads_folder: str,
+                           sys_platform,
+                           delete_files,
+                           ):
     """
     C1017528: Input data in numeric fields
     """
+
+    from pynput.keyboard import Controller, Key
+
     pdf = GenericPdf(driver, pdf_url=fillable_pdf_url)
     pdf.open()
+    keyboard = Controller()
     numeric_field = pdf.get_element("zipcode-field")
 
     # Test value to input in the field
@@ -25,3 +39,28 @@ def test_pdf_input_numbers(driver: Firefox, fillable_pdf_url: str):
 
     # Verify the value is still present
     pdf.element_attribute_contains("zipcode-field", "value", test_value)
+
+    download_button = pdf.get_element("download-button")
+    download_button.click()
+
+    time.sleep(2)
+
+    if sys_platform == "Linux":
+        keyboard.press(Key.alt)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        keyboard.release(Key.alt)
+        time.sleep(1)
+        keyboard.press(Key.alt)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        keyboard.release(Key.alt)
+        time.sleep(1)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        time.sleep(1)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
