8b6af24b4eb9e00ae373b4c9b0216c951308767e
Sarina Li <sarinajin.li@gmail.com>
2024-08-20 15:51:00 -0400

    intial commit

---
+++ tests/bookmarks_and_history/test_user_can_forget_history.py
@@ -0,0 +1,7 @@
+from selenium.webdriver import Firefox
+
+def test_user_can_forget_history(driver: Firefox):
+    """
+    C174072: Verify that the user can Forget all the history from the last 5 minutes
+    """
+    pass
