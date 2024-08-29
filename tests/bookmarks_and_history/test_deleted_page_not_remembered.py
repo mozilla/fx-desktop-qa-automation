1a4db5baa5d6ee89fdecdec1a5f77e6f716a9517
Ben Chatterton <bchatterton@mozilla.com>
2024-08-29 14:10:17 -0700

    behead deleted page test

--- tests/bookmarks_and_history/test_deleted_page_not_remembered.py
+++ tests/bookmarks_and_history/test_deleted_page_not_remembered.py
@@ -1,7 +1,11 @@
+import logging
+
 import pytest
 from selenium.webdriver import Firefox
 
 from modules.browser_object import ContextMenu, Navigation, PanelUi
+
+X_PADDING = 50
 
 
 @pytest.fixture()
@@ -14,12 +18,12 @@ def use_profile():
     return "theme_change"
 
 
-@pytest.mark.headed
 def test_deleted_page_not_remembered(driver: Firefox):
     """
     C216273: Verify that the deleted page from the Hamburger History submenu is not remembered or autofilled in the URL bar
     """
-    panel_ui = PanelUi(driver).open()
+    panel_ui = PanelUi(driver)
+    panel_ui.open()
     nav = Navigation(driver)
     context_menu = ContextMenu(driver)
 
@@ -27,7 +31,12 @@ def test_deleted_page_not_remembered(driver: Firefox):
     history_items = panel_ui.get_all_history()
 
     firefox_privacy_notice = history_items[-1]
-    panel_ui.context_click(firefox_privacy_notice)
+    with driver.context(driver.CONTEXT_CHROME):
+        x_offset = firefox_privacy_notice.size.get("width") + X_PADDING
+        logging.info(x_offset)
+        panel_ui.actions.move_to_element_with_offset(
+            firefox_privacy_notice, x_offset, 0
+        ).click().perform()
 
     context_menu.click_and_hide_menu("context-menu-delete-page")
     nav.type_in_awesome_bar("Firefox Privacy Notice")
