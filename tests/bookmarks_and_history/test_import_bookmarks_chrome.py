ba0ad16ca4e26460d236b1b7d66e26b5690ea269
Ben Chatterton <bchatterton@mozilla.com>
2024-09-12 09:02:45 -0700

    rework directory builder

--- tests/bookmarks_and_history/test_import_bookmarks_chrome.py
+++ tests/bookmarks_and_history/test_import_bookmarks_chrome.py
@@ -21,113 +21,75 @@ def test_case():
 
 @pytest.fixture()
 def add_prefs():
-    return [("browser.migrate.chrome.get_permissions.enabled", True)]
+    return [
+        ("browser.migrate.chrome.get_permissions.enabled", True),
+        ("devtools.chrome.enabled", True),
+        ("devtools.debugger.remote-enabled", True),
+    ]
 
 
 @pytest.fixture()
-def chrome_bookmarks(sys_platform, home_folder, tmp_path):
+def chrome_bookmarks(driver: Firefox, sys_platform, home_folder, tmp_path):
     """Move test Bookmarks file to correct location, fake Chrome instead of installing"""
-    source = os.path.join("data", "Chrome_Bookmarks")
+    bookmarks_source = os.path.join("data", "Chrome_Bookmarks")
     local_state_source = os.path.join("data", "Chrome_Local_State")
     if sys_platform.lower().startswith("win"):
-        target = os.path.join(
-            home_folder,
-            "AppData",
-            "Local",
-            "Google",
-            "Chrome",
-            "User Data",
-            "Default",
-            "Bookmarks",
-        )
-        app = os.path.join(
-            os.path.splitdrive(home_folder)[0],
-            "\\Program Files",
-            "Google",
-            "Chrome",
-            "Application",
-            "chrome.exe",
-        )
+        user_data_root = os.path.join(home_folder, "AppData", "Local")
+        chrome_root = os.path.join(user_data_root, "Google", "Chrome")
     elif sys_platform == "Darwin":
-        target = os.path.join(
-            home_folder,
-            "Library",
-            "Application Support",
-            "Google",
-            "Chrome",
-            "Default",
-            "Bookmarks",
+        user_data_root = os.path.join(home_folder, "Library", "Application Support")
+        chrome_root = os.path.join(user_data_root, "Google", "Chrome")
+    else:
+        user_data_root = os.path.join(home_folder, ".config")
+        chrome_root = os.path.join(user_data_root, "google-chrome")
+
+    if not os.path.exists(user_data_root):
+        logging.error(
+            f"User data not stored where we expect it, {user_data_root} does not exist"
         )
-        app = os.path.join(
-            "/Applications", "Google Chrome.app", "Contents", "MacOS", "Google Chrome"
-        )
-    elif sys_platform == "Linux":
-        target = os.path.join(
-            home_folder, ".config", "google-chrome", "Default", "Bookmarks"
-        )
-        app = os.path.join("/opt", "google", "chrome", "google-chrome")
+
+    defaults_folder = os.path.join(chrome_root, "Default")
+    bookmarks_target = os.path.join(defaults_folder, "Bookmarks")
+    local_state_target = os.path.join(chrome_root, "Local State")
 
     try:
-        fake_bookmarks = False
-        fake_app = False
-        if not os.path.exists(target):
-            logging.warning("Faking bookmarks...")
-            folder = os.path.dirname(target)
-            os.makedirs(folder, exist_ok=True)
+        fake_install = False
+        if not os.path.exists(bookmarks_target):
+            logging.warning("Faking install...")
+            os.makedirs(defaults_folder, exist_ok=True)
             logging.warning("Directory made!")
             for fakefile in ["History", "Cookies"]:
-                with open(os.path.join(folder, fakefile), "w") as fh:
+                with open(os.path.join(defaults_folder, fakefile), "w") as fh:
                     fh.write("")
+            logging.warning("History and Cookies made!")
             logging.warning("Faking local state...")
-            ls_folder = os.path.dirname(os.path.dirname(folder))
-            local_state_target = os.path.join(ls_folder, "Local State")
             copyfile(local_state_source, local_state_target)
 
-            fake_bookmarks = True
+            fake_install = True
         else:
-            logging.warning("Bookmarks folder exists...")
-            os.rename(target, tmp_path / "Bookmarks")
-        copyfile(source, target)
+            logging.warning("Install folder exists...")
+            os.rename(bookmarks_target, tmp_path / "Bookmarks")
+        copyfile(bookmarks_source, bookmarks_target)
         logging.warning("Bookmarks copied!")
 
-        # if not os.path.exists(app):
-        #     # Fake the Chrome app
-        #     logging.warning("Faking Chrome...")
-        #     os.makedirs(os.path.dirname(app), exist_ok=True)
-        #     logging.warning("Directory structure made!")
-        #     fake_app = True
-        #     with open(app, "w") as fh:
-        #         fh.write("")
-        #     logging.warning("Fake Chrome built.")
-        #     if "win" not in sys_platform.lower():
-        #         # Linux and maybe Mac need the file to be executable
-        #         os.chmod(app, stat.IRWXU)
-        #         logging.warning("Fake Chrome chmodded.")
+        yield bookmarks_target
 
-        yield target
-
-    except (FileNotFoundError, NotADirectoryError):
-        yield None
-    except PermissionError:
-        logging.warning("Permission Denied")
+    except (FileNotFoundError, NotADirectoryError, PermissionError) as e:
+        logging.warning(e)
         yield None
 
     # Teardown: We don't actually want to destroy the Chrome setup of local users
-    if os.path.exists(target):
-        os.remove(target)
-    if fake_bookmarks:
+    if os.path.exists(bookmarks_target):
+        os.remove(bookmarks_target)
+    if fake_install:
         for fakefile in ["History", "Cookies"]:
-            fake_fullpath = os.path.join(folder, fakefile)
+            fake_fullpath = os.path.join(defaults_folder, fakefile)
             if os.path.exists(fake_fullpath):
                 os.remove(fake_fullpath)
         os.remove(local_state_target)
-        os.removedirs(os.path.dirname(target))
+        os.removedirs(defaults_folder)
     elif os.path.exists(tmp_path / "Bookmarks"):
-        os.rename(tmp_path / "Bookmarks", target)
-    # if fake_app:
-    #     if os.path.exists(app):
-    #         os.remove(app)
-    #         os.removedirs(os.dirname(app))
+        os.rename(tmp_path / "Bookmarks", bookmarks_target)
 
 
 def test_chrome_bookmarks_imported(chrome_bookmarks, driver: Firefox):
