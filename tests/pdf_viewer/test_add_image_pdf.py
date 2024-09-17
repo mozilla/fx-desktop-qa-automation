00542148e49e144189b12f9b80e4754a7851d90e
Ben Chatterton <bchatterton@mozilla.com>
2024-09-17 09:43:07 -0700

    merge main; redo some fixtures

--- tests/pdf_viewer/test_add_image_pdf.py
+++ tests/pdf_viewer/test_add_image_pdf.py
@@ -1,4 +1,5 @@
 import os
+from shutil import copyfile
 
 import pytest
 from selenium.webdriver import Firefox
@@ -6,17 +7,22 @@ from selenium.webdriver import Firefox
 from modules.browser_object import ContextMenu
 from modules.page_object import GenericPdf
 
-PDF_URL = "https://web.archive.org/web/20060818161558/http://archive.dovebid.com/brochure/bro1514.pdf"
+
+@pytest.fixture()
+def temp_pdf(tmp_path):
+    loc = tmp_path / "boeing_brochure.pdf"
+    copyfile("data/boeing_brochure.pdf", loc)
+    return loc
 
 
 @pytest.mark.headed
-def test_add_image_pdf(driver: Firefox, sys_platform, absolute_path: str):
+def test_add_image_pdf(driver: Firefox, sys_platform, temp_pdf):
     """
     C2228202: Verify that the user is able to add an image to a PDF file.
     """
-    pdf_viewer = GenericPdf(driver, pdf_url=PDF_URL).open()
+    pdf_viewer = GenericPdf(driver, pdf_url=f"file://{temp_pdf}").open()
     context_menu = ContextMenu(driver)
-    pdf_viewer.add_image(os.path.join(absolute_path, "images/goomy.png"), sys_platform)
+    pdf_viewer.add_image(os.path.abspath("data/goomy.png"), sys_platform)
 
     pdf_viewer.element_exists("added-goomy-image")
     pdf_viewer.element_visible("added-goomy-image")
