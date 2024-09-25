214d0398086067df7d4900134802c9feabb7590f
soncuteanca <soncutean.anca@gmail.com>
2024-09-25 11:02:33 +0300

    Use login demo page instead of a random site

--- tests/password_manager/test_about_logins_navigation_from_context_menu.py
+++ tests/password_manager/test_about_logins_navigation_from_context_menu.py
@@ -1,17 +1,13 @@
 import pytest
 from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
 
 from modules.browser_object import ContextMenu, TabBar
-from modules.page_object_generics import GenericPage
+from modules.page_object import LoginAutofill
 
 
 @pytest.fixture()
 def test_case():
     return "2241087"
-
-
-URL_TEST_PAGE = "https://bsky.app/"
 
 
 def test_about_logins_navigation_from_login_form_context_menu(driver: Firefox):
@@ -22,13 +18,10 @@ def test_about_logins_navigation_from_login_form_context_menu(driver: Firefox):
 
     context_menu = ContextMenu(driver)
     tabs = TabBar(driver)
-    test_page = GenericPage(driver, url=URL_TEST_PAGE).open()
+    test_login = LoginAutofill(driver).open()
 
-    test_page.find_element(By.CSS_SELECTOR, 'button[aria-label="Sign in"]').click()
-    username_field = test_page.find_element(
-        By.CSS_SELECTOR, 'input[aria-label="Username or email address"]'
-    )
-    test_page.context_click(username_field)
+    username_field = test_login.get_element("username-field")
+    test_login.context_click(username_field)
     context_menu.click_context_item("context-manage-passwords")
     tabs.wait_for_num_tabs(2)
     tabs.title_contains("Passwords")
