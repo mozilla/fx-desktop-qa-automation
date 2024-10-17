8aca2b865c8c44d101fe371d60d37d00199798e3
Ben Chatterton <bchatterton@mozilla.com>
2024-10-17 09:29:33 -0700

    match mozilla-mobile reference-browser setup

--- taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
+++ taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
@@ -2,7 +2,7 @@ from taskgraph.target_tasks import register_target_task
 from taskgraph.util.taskcluster import find_task_id
 
 @register_target_task("new_beta_qa")
-def target_tasks_nightly(full_task_graph, parameters, graph_config):
+def target_tasks_beta_qa(full_task_graph, parameters, graph_config):
     """Select the set of tasks required for a Beta Smoke + Reporting session."""
 
     def filter(task, parameters):
