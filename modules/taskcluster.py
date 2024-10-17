d32afb921496ba76f4d00c4afc88df21eed76f54
Ben Chatterton <bchatterton@mozilla.com>
2024-10-17 16:55:34 -0700

    get tc secrets

---
+++ modules/taskcluster.py
@@ -0,0 +1,26 @@
+import functools
+import os
+
+import requests
+from requests.packages.urllib3.util.retry import Retry
+
+
+@functools.lru_cache()
+def get_tc_secret():
+    """Returns the Taskcluster secret.
+
+    Returns False when not running on tc
+    """
+    tc_proxy = os.environ.get("TASKCLUSTER_PROXY_URL")
+    if not tc_proxy:
+        return False
+    session = requests.Session()
+    retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    http_adapter = requests.adapters.HTTPAdapter(max_retries=retry)
+    session.mount("https://", http_adapter)
+    session.mount("http://", http_adapter)
+    secrets_url = f"{tc_proxy}/secrets/v1/secret/project%2Fmobile%2Fci%2Ftestrail"
+
+    res = session.get(secrets_url, timeout=30)
+    res.raise_for_status()
+    return res.json()["secret"]
