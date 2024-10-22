d6b87bed580f4386813c7338c1f88690378eeeed
soncuteanca <soncutean.anca@gmail.com>
2024-10-22 19:21:49 +0300

    Handle cookies

--- tests/geolocation/test_geolocation_shared_via_w3c_api.py
+++ tests/geolocation/test_geolocation_shared_via_w3c_api.py
@@ -1,4 +1,7 @@
+import time
+
 import pytest
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 
@@ -15,13 +18,28 @@ def test_case():
 def set_prefs():
     """Set prefs"""
     return [
-        ("geo.provider.network.url", "https://www.googleapis.com/geolocation/v1/geolocate?key"
-                                     "=%GOOGLE_LOCATION_SERVICE_API_KEY%"),
-        ("cookiebanners.service.mode", 1)
+        (
+            "geo.provider.network.url",
+            "https://www.googleapis.com/geolocation/v1/geolocate?key"
+            "=%GOOGLE_LOCATION_SERVICE_API_KEY%",
+        )
     ]
 
 
 TEST_URL = "https://www.w3schools.com/html/html5_geolocation.asp"
+
+
+def handle_cookie_banner(driver, web_page):
+    """
+    Address the cookie banner manually if appears, as the cookie banner dismissal preference is not effective in this
+    context
+    """
+    try:
+        driver.switch_to.window(driver.window_handles[-1])
+        web_page.find_element(By.ID, "accept-choices").click()
+    except NoSuchElementException:
+        # If the cookie banner is not found, continue with the test
+        pass
 
 
 def handle_geolocation_prompt(nav, button_type="primary"):
@@ -37,7 +55,9 @@ def click_geolocation_button_trigger(web_page):
     """
     Clicks the 'Try It' button inside the webpage to trigger the geolocation prompt
     """
-    geolocation_button_selector = "button.w3-btn.w3-blue.w3-round[onclick='getLocation()']"
+    geolocation_button_selector = (
+        "button.w3-btn.w3-blue.w3-round[onclick='getLocation()']"
+    )
     web_page.find_element(By.CSS_SELECTOR, geolocation_button_selector).click()
 
 
@@ -45,7 +65,9 @@ def is_location_marker_displayed(web_page):
     """
     Checks if the location marker ('You are here!') is displayed on the web page inside the map.
     """
-    location_marker = web_page.find_elements(By.CSS_SELECTOR, "div[aria-label='You are here!'][role='img']")
+    location_marker = web_page.find_elements(
+        By.CSS_SELECTOR, "div[aria-label='You are here!'][role='img']"
+    )
 
     if location_marker:  # Check if the marker exists
         return location_marker[0].is_displayed()
@@ -73,8 +95,11 @@ def test_allow_permission_on_geolocation_via_w3c_api(driver: Firefox):
     web_page = GenericPage(driver, url=TEST_URL).open()
     tabs = TabBar(driver)
 
-    # Click the 'Try It' button and Allow the location sharing
+    # Wait for the page to fully load and manually address the cookie banner if appears
     nav.wait_for_page_to_load()
+    handle_cookie_banner(driver, web_page)
+
+    # Click the 'Try It' button and Allow the location sharing
     click_geolocation_button_trigger(web_page)
     handle_geolocation_prompt(nav, button_type="primary")
 
@@ -110,8 +135,11 @@ def test_block_permission_on_geolocation_via_w3c_api(driver: Firefox):
     web_page = GenericPage(driver, url=TEST_URL).open()
     tabs = TabBar(driver)
 
-    # Click the 'Try It' button and Block the location sharing
+    # Wait for the page to fully load and manually address the cookie banner if appears
     nav.wait_for_page_to_load()
+    handle_cookie_banner(driver, web_page)
+
+    # Click the 'Try It' button and Block the location sharing
     click_geolocation_button_trigger(web_page)
     handle_geolocation_prompt(nav, button_type="secondary")
 
