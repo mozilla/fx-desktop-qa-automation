02b202191cd25bdd65a1d3fd6cc830504a06ba8c
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-10-29 15:38:56 -0700

    Sort out Taskcluster secrets (#306)
    * check on tc proxy
    
    * printenv
    
    * use root url; try sending a report
    
    * what am i getting from secrets api?
    
    * show full response of secrets api (changed passw to fake)
    
    * use http://taskcluster
    
    * try changing features in kinds
    
    * try changing features in kinds
    
    * try changing features in kinds
    
    * limited test; hardcoded uri
    
    * add scope, revert TC uri
    
    * test on level 1
    
    * revert testing harnesses; ready for merge
    
    * fix failing test
    
    * remove junk
    
    * xfail one test in linux

--- modules/taskcluster.py
+++ modules/taskcluster.py
@@ -7,24 +7,22 @@ from requests.packages.urllib3.util.retry import Retry
 
 
 @functools.lru_cache()
-def get_tc_secret():
+def get_tc_secret(secret_name="testrail"):
     """Returns the Taskcluster secret.
 
     Returns False when not running on tc
     """
-    tc_proxy = os.environ.get("TASKCLUSTER_PROXY_URL")
-    if not tc_proxy:
-        return False
-    logging.warning("tc_prox is not in env")
+    tc_home = os.environ.get("TASKCLUSTER_PROXY_URL", "http://taskcluster")
     session = requests.Session()
     retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
     http_adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("https://", http_adapter)
     session.mount("http://", http_adapter)
-    secrets_url = f"{tc_proxy}/secrets/v1/secret/project%2Fmozilla%2Ffx-desktop-qa-automation%2Flevel-3%2Ftestrail"
+    secrets_url = f"{tc_home}/secrets/v1/secret/project/mozilla/fx-desktop-qa-automation/level-3/{secret_name}"
 
     res = session.get(secrets_url, timeout=30)
     res.raise_for_status()
     logging.warning("secret is got")
 
+    logging.warning(res.text)
     return res.json()["secret"]
