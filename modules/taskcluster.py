aebb72423edd2096925095824f969b831a9f50ac
Ben Chatterton <bchatterton@mozilla.com>
2024-11-01 11:00:23 -0700

    don't run TC in GHA

--- modules/taskcluster.py
+++ modules/taskcluster.py
@@ -12,6 +12,8 @@ def get_tc_secret(secret_name="testrail"):
 
     Returns False when not running on tc
     """
+    if not os.environ.get("TASKCLUSTER_ROOT_URL"):
+        return False
     tc_home = os.environ.get("TASKCLUSTER_PROXY_URL", "http://taskcluster")
     session = requests.Session()
     retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
