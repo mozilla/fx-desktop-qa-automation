13d9981b8e523048a1e203aa92dfadceed17fd23
soncuteanca <soncutean.anca@gmail.com>
2024-11-04 16:30:50 +0200

    Create test for print to pdf

---
+++ tests/printing_ui/test_print_to_pdf.py
@@ -0,0 +1,74 @@
+import os
+from time import sleep
+
+import pytest
+from pynput.keyboard import Controller, Key
+from selenium.webdriver import Firefox
+
+from modules.browser_object_panel_ui import PanelUi
+
+
+@pytest.fixture()
+def test_case():
+    return "965142"
+
+
+@pytest.fixture()
+def delete_files_regex_string():
+    return r".*wikipedia.pdf"
+
+
+@pytest.fixture()
+def set_prefs():
+    return [("print.always_print_silent", True)]
+
+
+TEST_PAGE = "https://en.wikipedia.org"
+
+
+@pytest.mark.headed
+def test_print_to_pdf(
+    driver: Firefox,
+    downloads_folder: str,
+    sys_platform,
+    delete_files,
+):
+    """
+    C965142 - Verify that the user can print a webpage to PDF
+    """
+
+    keyboard = Controller()
+    panel_ui = PanelUi(driver)
+
+    driver.get(TEST_PAGE)
+
+    # Select Print option from Hamburger Menu in order to trigger the silent printing
+    with driver.context(driver.CONTEXT_CHROME):
+        panel_ui.open_panel_menu()
+        panel_ui.select_panel_setting("print-option")
+
+    # Allow time for the Save As dialog to appear
+    sleep(2)
+
+    # Type a file name in the native dialog and save the PDF
+    keyboard.type("wikipedia.pdf")
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+    # Set the expected download path and the expected PDF name
+    expected_file_name = "wikipedia.pdf"
+    saved_pdf_location = os.path.join(downloads_folder, expected_file_name)
+
+    # Allow time for the file to be saved
+    sleep(2)
+
+    # Verify if the PDF file was saved in the downloads folder
+    assert os.path.exists(
+        saved_pdf_location
+    ), f"The file was not downloaded to {saved_pdf_location}."
+
+    print(
+        f"Test passed: The file {expected_file_name} has been downloaded and is present at {saved_pdf_location}."
+    )
+
+    driver.quit()
