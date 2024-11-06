ad0d24952e4db965801da4573999e7561a749cc5
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-11-06 10:02:57 -0800

    Ben/tc confirm report (#316)
    * clone reportable outside of conftest
    
    * factor reportable out of conftest
    
    * factor reportable out of conftest
    
    * technically complete
    
    * cleanup
    
    * fix test

--- conftest.py
+++ conftest.py
@@ -286,57 +286,6 @@ def version(fx_executable: str):
     return version
 
 
-@pytest.fixture(scope="session")
-def reportable(version, sys_platform):
-    """Return true if we should report to TestRail"""
-
-    if not os.environ.get("TESTRAIL_REPORT"):
-        return False
-
-    # Find the correct test plan
-    tr_session = tri.testrail_init()
-    first_half, second_half = version.split(".")
-    channel = "Beta" if "b" in second_half else "Release"
-    if "Nightly" in first_half:
-        channel = "Nightly"
-
-    major_version = " ".join(first_half.split(" ")[1:])
-    major_number = major_version.split(" ")[-1]
-    major_milestone = tr_session.matching_milestone(TESTRAIL_FX_DESK_PRJ, major_version)
-    if not major_milestone:
-        logging.warning("Reporting: Could not find matching milestone.")
-        return False
-
-    channel_milestone = tr_session.matching_submilestone(
-        major_milestone, f"{channel} {major_number}"
-    )
-    if not channel_milestone:
-        logging.warning(
-            f"Reporting: Could not find matching submilestone for {channel} {major_number}"
-        )
-        return False
-
-    this_plan = tr_session.matching_plan_in_milestone(
-        TESTRAIL_FX_DESK_PRJ,
-        channel_milestone.get("id"),
-    )
-    if not this_plan:
-        return True
-
-    platform = "MacOS" if sys_platform == "Darwin" else sys_platform
-
-    plan_entries = this_plan.get("entries")
-    covered_suites = 0
-    for entry in plan_entries:
-        for run_ in entry.get("runs"):
-            if platform in run_.get("config"):
-                covered_suites += 1
-
-    num_suites = len([d for d in os.listdir("tests") if os.path.isdir(d)])
-
-    return covered_suites > (num_suites - SUITE_COVERAGE_TOLERANCE)
-
-
 @pytest.fixture()
 def machine_config():
     """Return the os type, version, and architecture for the machine"""
@@ -354,15 +303,9 @@ def test_case():
     return None
 
 
-@pytest.fixture(scope="session")
-def setup_reportable(reportable, request):
-    # Set the result of the `reportable` fixture on the config object
-    request.config.is_reportable = reportable
-
-
 def pytest_configure(config):
     # Check the `is_reportable` attribute on the config object
-    if not getattr(config, "is_reportable", True):
+    if os.environ.get("TESTRAIL_REPORT") and not tri.reportable():
         pytest.exit("Test run is not reportable. Exiting.")
 
 
