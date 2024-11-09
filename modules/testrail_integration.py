405dd14c6fde4f9d787791ce6974cbd89bd831ab
Ben Chatterton <bchatterton@mozilla.com>
2024-11-08 21:58:22 -0800

    fix suite count

--- modules/testrail_integration.py
+++ modules/testrail_integration.py
@@ -13,7 +13,9 @@ TESTRAIL_RUN_FMT = "[{channel} {major}] Automated testing {major}.{minor}b{build
 PLAN_NAME_RE = re.compile(r"\[(\w+) (\d+)\]")
 CONFIG_GROUP_ID = 95
 TESTRAIL_FX_DESK_PRJ = 17
-SUITE_COVERAGE_TOLERANCE = 2
+
+# Number of suites that exist in the repo but are not reported in TR for reasons.
+SUITE_COVERAGE_TOLERANCE = 5
 
 
 def get_plan_title(version_str: str, channel: str) -> str:
@@ -103,7 +105,7 @@ def reportable():
     logging.warning(
         f"Partial report exists ({covered_suites}/{num_suites} suites), session may be reportable."
     )
-    return covered_suites > (num_suites - SUITE_COVERAGE_TOLERANCE)
+    return covered_suites < (num_suites - SUITE_COVERAGE_TOLERANCE)
 
 
 def testrail_init() -> TestRail:
