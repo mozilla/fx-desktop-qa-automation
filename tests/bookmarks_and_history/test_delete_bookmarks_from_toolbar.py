a124abbfbdc5be008e370ee3e3757b2857257a24
whlpatricia <113727646+whlpatricia@users.noreply.github.com>
2024-11-12 11:33:53 -0500

    minor fix

--- tests/bookmarks_and_history/test_delete_bookmarks_from_toolbar.py
+++ tests/bookmarks_and_history/test_delete_bookmarks_from_toolbar.py
@@ -1,17 +1,14 @@
 import pytest
 from pynput.keyboard import Controller, Key
-from time import sleep
 from selenium.webdriver import Firefox
 
 from modules.browser_object_navigation import Navigation
 from modules.browser_object_panel_ui import PanelUi
-from modules.browser_object_tabbar import TabBar
-from modules.page_object_generics import GenericPage
 
 
 @pytest.fixture()
 def test_case():
-    return "2084550"
+    return "2084559"
 
 
 URL_TO_BOOKMARK = "https://www.mozilla.org/"
@@ -19,15 +16,12 @@ URL_TO_BOOKMARK = "https://www.mozilla.org/"
 
 def test_open_bookmarks_from_toolbar(driver: Firefox):
     """
-    C2084550: Verify that the user can open Bookmarks from the Toolbar with a mouse click
+    C2084559: Verify that the user can delete items from "Bookmarks Toolbar"
     """
     # Instantiate objects
     nav = Navigation(driver)
     panel = PanelUi(driver)
     keyboard = Controller()
-    
-    page = GenericPage(driver, url=URL_TO_BOOKMARK)
-    sleep(5)
 
     # Bookmark the given website via star button
     driver.get(URL_TO_BOOKMARK)
@@ -39,11 +33,9 @@ def test_open_bookmarks_from_toolbar(driver: Firefox):
 
         # Delete the bookmark
         panel.context_click("bookmark-by-title", labels=["Internet for people"])
-        sleep(10)
         for _ in range(5):
             keyboard.tap(Key.down)
         keyboard.tap(Key.enter)
 
         # Verify bookmark is deleted
         panel.element_does_not_exist("bookmark-by-title", labels=["Internet for people"])
-
