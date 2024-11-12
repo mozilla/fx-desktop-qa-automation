21fd0916a8d9f236c7b021abb53cf589e9e5607e
whlpatricia <113727646+whlpatricia@users.noreply.github.com>
2024-11-12 14:18:09 -0500

    headed

--- tests/bookmarks_and_history/test_delete_bookmarks_from_toolbar.py
+++ tests/bookmarks_and_history/test_delete_bookmarks_from_toolbar.py
@@ -14,6 +14,7 @@ def test_case():
 URL_TO_BOOKMARK = "https://www.mozilla.org/"
 
 
+@pytest.mark.headed
 def test_delete_bookmarks_from_toolbar(driver: Firefox):
     """
     C2084559: Verify that the user can delete items from "Bookmarks Toolbar"
