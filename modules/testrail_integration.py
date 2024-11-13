da7998251ba2d90459415d362ea27276a3106daa
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-11-13 07:01:14 -0800

    fix reportable problem by tracking unreported suites (#327)
    * fix reportable problem by tracking unreported suites
    
    * cleanup

--- modules/testrail_integration.py
+++ modules/testrail_integration.py
@@ -14,9 +14,6 @@ PLAN_NAME_RE = re.compile(r"\[(\w+) (\d+)\]")
 CONFIG_GROUP_ID = 95
 TESTRAIL_FX_DESK_PRJ = 17
 
-# Number of suites that exist in the repo but are not reported in TR for reasons.
-SUITE_COVERAGE_TOLERANCE = 3
-
 
 def get_plan_title(version_str: str, channel: str) -> str:
     """Given a version string, get the plan_title"""
@@ -49,18 +46,20 @@ def reportable():
     """Return true if we should report to TestRail"""
     import platform
 
-    sys_platform = platform.system()
-    version = subprocess.check_output(
-        [os.environ.get("FX_EXECUTABLE"), "--version"]
-    ).decode()
     if not os.environ.get("TESTRAIL_REPORT"):
+        logging.warning("TESTRAIL_REPORT not set, session not reportable.")
         return False
 
     # If we ask for reporting, we can force a report
     if os.environ.get("REPORTABLE"):
+        logging.warning("REPORTABLE=true; we will report this session.")
         return True
 
     # Find the correct test plan
+    sys_platform = platform.system()
+    version = subprocess.check_output(
+        [os.environ.get("FX_EXECUTABLE"), "--version"]
+    ).decode()
     tr_session = testrail_init()
     first_half, second_half = version.split(".")
     channel = "Beta" if "b" in second_half else "Release"
@@ -102,14 +101,16 @@ def reportable():
             if platform in run_.get("config"):
                 covered_suites += 1
 
-    num_suites = len(
-        [d for d in os.listdir("tests") if os.path.isdir(os.path.join("tests", d))]
-    )
+    num_suites = 0
+    for test_dir_name in os.listdir("tests"):
+        test_dir = os.path.join("tests", test_dir_name)
+        if os.path.isdir(test_dir) and not os.path.exists(
+            os.path.join(test_dir, "skip_reporting")
+        ):
+            num_suites += 1
 
-    logging.warning(
-        f"Partial report exists ({covered_suites}/{num_suites} suites), session may be reportable."
-    )
-    return covered_suites < (num_suites - SUITE_COVERAGE_TOLERANCE)
+    logging.warning("Potentially matching run found, may be reportable.")
+    return covered_suites < num_suites
 
 
 def testrail_init() -> TestRail:
