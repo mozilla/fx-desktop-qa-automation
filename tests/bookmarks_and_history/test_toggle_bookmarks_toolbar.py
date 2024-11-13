020428342ffdd29b704238200b152576857d7964
Tracy <twalker@mozilla.com>
2024-11-13 08:01:58 -0600

    Remove hard waits

--- tests/bookmarks_and_history/test_toggle_bookmarks_toolbar.py
+++ tests/bookmarks_and_history/test_toggle_bookmarks_toolbar.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pytest
 from selenium.webdriver import Firefox
 
@@ -15,27 +13,18 @@ def test_toggle_bookmark_toolbar(driver: Firefox):
     """
     C2084637: Verify that the user can Hide or Show the Bookmarks Toolbar using Keyboard shortcuts
     """
-    # instantiate object
+    # Instantiate the browser object
     nav = Navigation(driver).open()
 
-    def is_bookmarks_toolbar_collapsed() -> str:
-        with driver.context(driver.CONTEXT_CHROME):
-            value = nav.get_element("bookmarks-toolbar").get_attribute("collapsed")
-        return value
-
-    # By default, the Bookmarks Toolbar is hidden.
-    # The element attribute indicates whether the bookmarks toolbar is
-    # either, collapsed = 'true' is hidden, or not collapsed 'false' is visible.
-    assert is_bookmarks_toolbar_collapsed() == "true"
+    # By default, the Bookmarks Toolbar is hidden. The element attribute indicates
+    # whether the bookmarks toolbar is either, collapsed = 'true' is hidden,
+    # or not collapsed = 'false' is visible.
+    nav.element_attribute_contains("bookmarks-toolbar", "collapsed", "true")
 
     # Un-hide the Bookmarks Toolbar with keyboard shortcut.
     nav.toggle_bookmarks_toolbar_with_key_combo()
-
-    # Wait to ensure the Bookmarks Toolbar has time to change state
-    sleep(0.5)
-    assert is_bookmarks_toolbar_collapsed() == "false"
+    nav.element_attribute_contains("bookmarks-toolbar", "collapsed", "false")
 
     # Toggle the Bookmarks Toolbar again and confirm it is hidden.
     nav.toggle_bookmarks_toolbar_with_key_combo()
-    sleep(0.5)
-    assert is_bookmarks_toolbar_collapsed() == "true"
+    nav.element_attribute_contains("bookmarks-toolbar", "collapsed", "true")
