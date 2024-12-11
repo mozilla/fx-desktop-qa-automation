a9be1f2be8dc3952bf63f87e1a3d5ce545d3a5e5
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2024-12-11 14:14:50 -0800

    remove unstable for tests where instability cannot be addressed at the moment (#376)

--- tests/pocket/test_basic_de.py
+++ tests/pocket/test_basic_de.py
@@ -18,7 +18,7 @@ def add_prefs():
 
 
 # LOW PRIORITY
-@pytest.mark.unstable
+@pytest.mark.skip(reason="Pocket tests no longer belong to DTE.")
 @pytest.mark.locale_de
 def test_localized_pocket_layout_DE(driver: Firefox):
     """
--- tests/pocket/test_basic_fr.py
+++ tests/pocket/test_basic_fr.py
@@ -18,7 +18,7 @@ def add_prefs():
 
 
 # LOW PRIORITY
-@pytest.mark.unstable
+@pytest.mark.skip(reason="Pocket tests no longer belong to DTE.")
 @pytest.mark.locale_fr
 def test_localized_pocket_layout_FR(driver: Firefox):
     """
