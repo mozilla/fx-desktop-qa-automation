fec6f488889ff5a58025c930466ebc7b9fb0f908
Ben Chatterton <bchatterton@mozilla.com>
2025-01-03 09:03:06 -0800

    drop the dot-slash to try to fix win

--- choose_ci_set.py
+++ choose_ci_set.py
@@ -151,6 +151,6 @@ if __name__ == "__main__":
     run_list.extend(changed_models)
 
     if not run_list:
-        print(" ".join(ci_paths))
+        print(" ".join(ci_paths).replace("./", ""))
     else:
-        print(" ".join(run_list))
+        print(" ".join(run_list).replace("./", ""))
