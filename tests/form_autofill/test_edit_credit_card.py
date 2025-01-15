33344d0f18cf3a9c16ca2b85e674f6a4724d70fe
soncuteanca <soncutean.anca@gmail.com>
2025-01-15 18:26:33 +0200

    Set back the linux mark

--- tests/form_autofill/test_edit_credit_card.py
+++ tests/form_autofill/test_edit_credit_card.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import platform
 
 import pytest
 from selenium.webdriver import Firefox
@@ -25,7 +26,7 @@ def hard_quit():
 tabs = [i for i in range(4)]
 
 
-# @pytest.mark.xfail(platform.system() == "Linux", reason="Autofill Linux instability")
+@pytest.mark.xfail(platform.system() == "Linux", reason="Autofill Linux instability")
 @pytest.mark.parametrize("num_tabs", tabs)
 def test_edit_credit_card_profile(driver: Firefox, num_tabs: int, hard_quit):
     """
