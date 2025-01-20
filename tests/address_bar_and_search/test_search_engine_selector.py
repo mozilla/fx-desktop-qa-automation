14adf24158d1b107ca7bb0fc63ff0886d88c484c
Philimon Ayalew <philimonayalew@cjhp602y5x.fritz.box>
2025-01-20 22:48:09 +0100

    Minor clean up.

--- tests/address_bar_and_search/test_search_engine_selector.py
+++ tests/address_bar_and_search/test_search_engine_selector.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pytest
 from selenium.webdriver import Firefox
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,8 +19,8 @@ def test_search_engine_selector_and_validator(driver: Firefox, search_mode: str)
     Select Appropriate search engine and go to site, asserting that the correct search engine was used.
 
     Parameters:
-    driver (FireFox): webdriver used for communicating with the browser.
-    search_mode (str): search parameter selected from the awesome bar.
+        driver (FireFox): webdriver used for communicating with the browser.
+        search_mode (str): search parameter selected from the awesome bar.
     """
     nav = Navigation(driver)
     url_element_search_mode = search_mode.split()[0].lower()
