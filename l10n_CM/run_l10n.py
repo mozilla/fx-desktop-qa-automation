85436a7322f52def568b7c11cb46e63477aed8f9
Philimon Derib Ayalew <philimonset@gmail.com>
2025-02-04 17:32:33 +0100

    removed headless option

--- l10n_CM/run_l10n.py
+++ l10n_CM/run_l10n.py
@@ -22,16 +22,13 @@ def get_region_tests(test_region: str) -> list[str]:
 if __name__ == "__main__":
     valid_region = {"US", "CA", "DE", "FR"}
     regions = sys.argv[1:] if len(sys.argv[1:]) > 0 else valid_region
-    headless = "--run-headless" if sys.argv[-1] == "f" else ""
     for region in regions:
-        if region == "f":
-            break
         if region not in valid_region:
             raise ValueError("Invalid Region.")
         tests = get_region_tests(region)
         try:
             os.environ["STARFOX_REGION"] = region
-            subprocess.run(["pytest", headless, *tests], check=True, text=True)
+            subprocess.run(["pytest", *tests], check=True, text=True)
         except subprocess.CalledProcessError as e:
             print(e)
             logging.warn(f"Test run failed. {e}")
