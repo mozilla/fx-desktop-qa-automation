715f1e69ddb9d4d8bf83b5fe598da9026547ed67
Ben Chatterton <bchatterton@mozilla.com>
2025-02-04 09:35:15 -0800

    scotch bonnet tests removed from ci suite

--- tests/networking/test_http_site.py
+++ tests/networking/test_http_site.py
@@ -27,8 +27,7 @@ HTTP_SITE = "http://http.badssl.com/"
 CONNECTION_NOT_SECURE = "Connection is not secure"
 
 
-# Scotch Bonnet -- uncomment when updated
-# @pytest.mark.ci
+# Scotch Bonnet -- mark for ci when updated
 def test_http_site(driver: Firefox):
     """C2300294 Check that HTTP is allowed when appropriate"""
 
