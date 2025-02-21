3c1e2bb2629f749879b2b6aaaead20b6741cf8af
virgil.sangerean <vsangerean@mozilla.com>
2025-02-21 14:23:01 +0200

    vs/CC-doorhanger-data-is-saved-in-about-prefs

--- l10n_CM/Unified/test_demo_cc_doorhanger_data_is_stored_in_about_prefs.py
+++ l10n_CM/Unified/test_demo_cc_doorhanger_data_is_stored_in_about_prefs.py
@@ -39,7 +39,7 @@ def test_demo_cc_data_captured_in_doorhanger_and_stored(driver: Firefox, region:
     doorhanger_text = autofill_popup_obj.get_cc_doorhanger_data("cc-doorhanger-data")
     assert credit_card_sample_data.card_number[-4:] in doorhanger_text, f"Expected last 4 digits '{credit_card_sample_data.card_number[-4:]}' but not found."
     assert credit_card_sample_data.name in doorhanger_text, f"Expected name '{credit_card_sample_data.name}' but not found."
-    assert credit_card_sample_data.cvv not in doorhanger_text, f"cvv '{credit_card_sample_data.cvv}' should not be saved, but found in doorhanger."
+    assert credit_card_sample_data.cvv not in doorhanger_text, f"CVV '{credit_card_sample_data.cvv}' should not be saved, but found in doorhanger."
 
     # Click the "Save" button using click_doorhanger_button
     autofill_popup_obj.click_doorhanger_button("save")
@@ -55,4 +55,4 @@ def test_demo_cc_data_captured_in_doorhanger_and_stored(driver: Firefox, region:
     # Validate stored values match expected values
     assert elements[0].endswith(credit_card_sample_data.card_number[-4:]), f"Expected last 4 digits '{credit_card_sample_data.card_number[-4:]}' but got '{elements[0]}'"
     assert elements[1] == credit_card_sample_data.name, f"Expected name '{credit_card_sample_data.name}' but got '{elements[1]}'"
-    assert credit_card_sample_data.cvv not in elements, f"cvv '{credit_card_sample_data.cvv}' should not be saved, but found in stored values."
+    assert credit_card_sample_data.cvv not in elements, f"CVV '{credit_card_sample_data.cvv}' should not be saved, but found in stored values."
