21e79b1409d59332bba8b73b6f71dd7001c75758
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-02-26 17:26:36 +0200

    fix

--- l10n_CM/Unified/test_demo_ad_autofill_address_fields.py
+++ l10n_CM/Unified/test_demo_ad_autofill_address_fields.py
@@ -11,20 +11,18 @@ def test_case():
     return "2888563"
 
 
-def test_demo_ad_autofill_address_fields(driver: Firefox, region: str, address_autofill: AddressFill, util: Utilities):
+def test_demo_ad_autofill_address_fields(driver: Firefox, region: str, address_autofill: AddressFill, util: Utilities,
+                                         autofill_popup: AutofillPopup):
     """
     C2888563 - Verify Autofill functionality when selecting an entry from the dropdown for address fields
     """
-    # Instantiate objects
-    address_autofill_popup = AutofillPopup(driver)
-
     # Create fake data and fill it in
     address_autofill.open()
     address_autofill_data = util.fake_autofill_data(region)
     address_autofill.save_information_basic(address_autofill_data)
 
     # Click the "Save" button
-    address_autofill_popup.click_doorhanger_button("save")
+    autofill_popup.click_doorhanger_button("save")
 
     # List of field labels to be autofilled and verified
     fields_to_test = [
@@ -37,6 +35,6 @@ def test_demo_ad_autofill_address_fields(driver: Firefox, region: str, address_a
 
     # Loop through each field and perform the autofill test
     for field in fields_to_test:
-        address_autofill.autofill_and_verify(address_autofill_popup, field,
+        address_autofill.autofill_and_verify(autofill_popup, field,
                                              address_autofill_data, util)
 
