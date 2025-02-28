709fca3808a027357a10243c6d021f5a4190a5c8
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-02-28 12:45:19 +0200

    add methods to pom

--- l10n_CM/Unified/test_demo_ad_clear_tel_email.py
+++ l10n_CM/Unified/test_demo_ad_clear_tel_email.py
@@ -21,3 +21,21 @@ def test_demo_ad_clear_tel_email(
     """
     C2888571 - Verify clear functionality after selecting an entry from tele/email fields
     """
+
+    # Create fake data and fill it in
+    address_autofill.open()
+    address_autofill_data = util.fake_autofill_data(region)
+    address_autofill.save_information_basic(address_autofill_data)
+
+    # Click the "Save" button
+    autofill_popup.click_doorhanger_button("save")
+
+    # List of field labels to be autofilled and verified
+    fields_to_test = [
+        "email",
+        "tel"
+    ]
+
+    # Loop through each field and perform the autofill test
+    for field in fields_to_test:
+        address_autofill.clear_and_verify(autofill_popup, field, address_autofill_data)
