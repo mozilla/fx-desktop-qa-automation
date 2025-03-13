fd891b1863f36efdc4a04b40c03bb5e49e475095
Ben Chatterton <bchatterton@mozilla.com>
2025-03-13 09:55:18 -0700

    lint

--- taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
+++ taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
@@ -9,3 +9,12 @@ def target_tasks_beta_qa(full_task_graph, parameters, graph_config):
         return task.attributes.get("beta-qa", False)
 
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
+
+@register_target_task("new_devedition_qa")
+def target_tasks_devedition_qa(full_task_graph, parameters, graph_config):
+    """Select the set of tasks required for a DevEdition Smoke + Reporting session."""
+
+    def filter(task, parameters):
+        return task.attributes.get("devedition-qa", False)
+
+    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
