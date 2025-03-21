ce4837c0e9f3b0d30ec86269609173a1830fc498
Tracy <twalker@mozilla.com>
2025-03-21 14:54:21 -0500

    Disable cookies, via pref list, for this test, instead of looking for a cookie banner.

--- tests/tabs/test_mute_tabs.py
+++ tests/tabs/test_mute_tabs.py
@@ -1,20 +1,34 @@
 from os import environ
 
 import pytest
+from selenium.common.exceptions import (
+    ElementNotInteractableException,
+    NoSuchElementException,
+)
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.support import expected_conditions as EC
 
 from modules.browser_object import TabBar
 
 PLAY_BUTTON_SELECTOR = ".ytp-play-button"
-COOKIE_CONSENT_SELECTOR = "button[aria-label^='Accept all'], button[aria-label^='Accept the use']"
+COOKIE_CONSENT_SELECTOR = (
+    "button[aria-label^='Accept all'], button[aria-label^='Accept the use']"
+)
+
 
 @pytest.fixture()
 def test_case():
     return "134719"
 
+
+@pytest.fixture()
+def add_to_prefs_list():
+    return [("network.cookie.cookieBehavior", "2")]
+
+
 GHA = environ.get("GITHUB_ACTIONS") == "true"
+
 
 @pytest.mark.skipif(GHA, reason="Test unstable in Github Actions")
 @pytest.mark.audio
@@ -22,13 +36,7 @@ def test_mute_unmute_tab(screenshot, driver: Firefox, video_url: str):
     """C134719, test that tabs can be muted and unmuted"""
     tabs = TabBar(driver)
     driver.get(video_url)
-
-    # Dismiss cookie banner if present
-    try:
-        consent_button = driver.find_element(By.CSS_SELECTOR, COOKIE_CONSENT_SELECTOR)
-        consent_button.click()
-    except (NoSuchElementException, ElementNotInteractableException):
-        pass  # Banner wasn't displayed or not interactable; proceed normally
+    tabs.expect(EC.title_contains("Top 10"))
 
     play_button = driver.find_element(By.CSS_SELECTOR, PLAY_BUTTON_SELECTOR)
     play_button.click()
