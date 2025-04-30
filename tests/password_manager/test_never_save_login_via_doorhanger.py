b844c668254de9cb2b253414f8a3ccb0bd30e25f
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-04-30 17:46:41 +0300

    Passwors manager first batch refactor

--- tests/password_manager/test_never_save_login_via_doorhanger.py
+++ tests/password_manager/test_never_save_login_via_doorhanger.py
@@ -24,13 +24,15 @@ def test_never_save_login_via_doorhanger(driver: Firefox):
     login form. Additionally, confirm that the demo page is correctly added to the 'Exceptions - Saved Passwords'
     list in the browser's preferences.
     """
-
-    login_autofill = LoginAutofill(driver).open()
+    # Instantiate objects
+    login_autofill = LoginAutofill(driver)
     autofill_popup_panel = AutofillPopup(driver)
     nav = Navigation(driver)
     ba = BrowserActions(driver)
+    about_prefs = AboutPrefs(driver, category="privacy")
 
     # Creating an instance of the LoginForm within the LoginAutofill page object
+    login_autofill.open()
     login_form = LoginAutofill.LoginForm(login_autofill)
 
     # Fill in the login form in demo page, opt for Never Save the credentials via doorhanger and see the doorhanger is
@@ -55,7 +57,7 @@ def test_never_save_login_via_doorhanger(driver: Firefox):
     login_autofill.wait.until(lambda _: password_element.get_attribute("value") == "")
 
     # Navigate to about:preferences#privacy and open Exceptions - Saved Passwords modal
-    about_prefs = AboutPrefs(driver, category="privacy").open()
+    about_prefs.open()
     iframe = about_prefs.get_password_exceptions_popup_iframe()
     ba.switch_to_iframe_context(iframe)
 
