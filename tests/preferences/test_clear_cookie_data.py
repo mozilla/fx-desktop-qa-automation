6462a659b66ab8d98093a3ae51b873c348822609
Philimon Derib Ayalew <philimonset@gmail.com>
2025-06-18 16:46:47 +0200

    change site to one that takes a cookie

--- tests/preferences/test_clear_cookie_data.py
+++ tests/preferences/test_clear_cookie_data.py
@@ -10,6 +10,13 @@ def test_case():
     return "143627"
 
 
+def open_clear_cookies_data_dialog(about_prefs: AboutPrefs, ba: BrowserActions):
+    about_prefs.open()
+    clear_data_popup = about_prefs.press_button_get_popup_dialog_iframe("Clear Data")
+    ba.switch_to_iframe_context(clear_data_popup)
+    return about_prefs.get_clear_cookie_data_value()
+
+
 def test_clear_cookie_data(driver: Firefox):
     """
     C143627: Cookies and site data can be cleared via the "Clear Data" panel
@@ -18,27 +25,18 @@ def test_clear_cookie_data(driver: Firefox):
     about_prefs = AboutPrefs(driver, category="privacy")
     ba = BrowserActions(driver)
 
-    def open_clear_cookies_data_dialog():
-        about_prefs.open()
-        clear_data_popup = about_prefs.press_button_get_popup_dialog_iframe(
-            "Clear Data"
-        )
-        ba.switch_to_iframe_context(clear_data_popup)
-
     # Visit a site to get a cookie added to saved data
-    driver.get("https://www.firefox.com")
+    driver.get("https://www.wikipedia.com")
 
     # Navigate to the clear data dialog of about:preferences#privacy
-    open_clear_cookies_data_dialog()
-
     # Check for a non-zero value of the 'Cookies and site data' option
-    cookie_value = about_prefs.get_clear_cookie_data_value()
+    cookie_value = open_clear_cookies_data_dialog(about_prefs, ba)
     assert cookie_value > 0
 
     # Then clear the cookies and site data
     about_prefs.get_element("clear-data-accept-button").click()
 
     # Finally, check the value of the dialog option, it should be 0
-    open_clear_cookies_data_dialog()
-    cookie_value2 = about_prefs.get_clear_cookie_data_value()
+
+    cookie_value2 = open_clear_cookies_data_dialog(about_prefs, ba)
     assert cookie_value2 == 0
