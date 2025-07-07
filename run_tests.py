0d3ef61819c772eadf5766dc75b34b3ad3c22f02
Ben Chatterton <bchatterton@mozilla.com>
2025-07-07 13:51:36 -0700

    add windows to tc

--- run_tests.py
+++ run_tests.py
@@ -2,24 +2,52 @@ import argparse
 import os
 import platform
 import sys
+from pathlib import Path
+from subprocess import check_output
 
+import mozinstall
 import pytest
+import requests
 
 LINUX_FX_EXEC = "./firefox/firefox"
 WIN_FX_EXEC = "C:\\Program Files\\Custom Firefox\\firefox.exe"
 MAC_FX_EXEC = ""
 
 
+def get_fx_exec():
+    fx_exec = LINUX_FX_EXEC
+    if platform.system().lower().startswith("win"):
+        fx_exec = WIN_FX_EXEC
+    elif platform.system().lower().startswith("darwin"):
+        fx_exec = MAC_FX_EXEC
+    return fx_exec
+
+
+def install():
+    command = ["python", "collect_executables.py"]
+    if not platform.system().lower().startswith("win"):
+        command = ["./collect_executables.sh"]
+    target_filename = "setup.exe"
+    if platform.system().lower().startswith("darwin"):
+        target_filename = "Firefox.dmg"
+    elif platform.system().lower().startswith("linux"):
+        target_filename = "firefox.tar.xz"
+    url = check_output(command).decode()
+    if "collect_executables.py" in command:
+        resp = requests.get(url)
+        resp.raise_for_status()
+        with open(target_filename, "wb") as fh:
+            fh.write(resp.content)
+        mozinstall.install(target_filename, Path(get_fx_exec()).parent)
+
+
 def run_suite(parsed_args):
     """Convert script / argparse args (parsed_args) to pytest_args"""
     pytest_args = parsed_args.added_args or []
+    if parsed_args.install:
+        install()
     if parsed_args.ci and "--fx-executable" not in pytest_args:
-        fx_exec = LINUX_FX_EXEC
-        if platform.system().lower().startswith("win"):
-            fx_exec = WIN_FX_EXEC
-        elif platform.system().lower().startswith("darwin"):
-            fx_exec = MAC_FX_EXEC
-        pytest_args.extend(["--fx-executable", fx_exec])
+        pytest_args.extend(["--fx-executable", get_fx_exec()])
     if parsed_args.pyproject:
         os.rename(parsed_args.pyproject, "pyproject.toml")
     if parsed_args.subset:
@@ -47,6 +75,7 @@ parser.add_argument("added_args", nargs="*")
 parser.add_argument("--pyproject", default=None)
 parser.add_argument("-w", "--workers", default=None)
 parser.add_argument("-s", "--subset", action="store_true")
+parser.add_argument("--install", default=None)
 parser.add_argument("--ci", action="store_true")
 parser.add_argument("--headed", action="store_true")
 
