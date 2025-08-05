fa7b904231c5188c2183efa010e4432acbab38ec
soncuteanca <soncutean.anca@gmail.com>
2025-08-05 17:16:37 +0300

    Add CM movieposters CA

--- l10n_CM/run_l10n.py
+++ l10n_CM/run_l10n.py
@@ -39,7 +39,8 @@ valid_sites = {
     "yellowkorner",
     "wish",
     "bijoubrigitte",
-    "whittard"
+    "whittard",
+    "movieposters",
 }
 
 loaded_valid_sites = valid_l10n_mappings().keys()
