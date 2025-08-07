c4f964fbd5d18cfbaad2bd1f011b91d826db6e6a
soncuteanca <soncutean.anca@gmail.com>
2025-08-07 13:10:08 +0300

    Add PL region

--- l10n_CM/run_l10n.py
+++ l10n_CM/run_l10n.py
@@ -15,7 +15,7 @@ from choose_l10n_ci_set import valid_l10n_mappings
 current_dir = os.path.dirname(__file__)
 valid_flags = {"--run-headless", "-n", "--reruns", "--fx-executable", "--ci"}
 flag_with_parameter = {"-n", "--reruns"}
-valid_region = {"US", "CA", "DE", "FR", "IT", "GB"}
+valid_region = {"US", "CA", "DE", "FR", "IT", "GB", "PL"}
 valid_sites = {
     "demo",
     "amazon",
