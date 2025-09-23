f31101b519cffb8d56b735f08a12e13d2e104e8d
soncuteanca <soncutean.anca@gmail.com>
2025-09-23 15:32:06 +0300

    Add refactored test 216273

--- tests/bookmarks_and_history/test_deleted_page_not_remembered.py
+++ tests/bookmarks_and_history/test_deleted_page_not_remembered.py
@@ -19,16 +19,16 @@ def test_deleted_page_not_remembered(driver: Firefox, sys_platform):
     """
     C216273: Verify that the deleted page from the Hamburger History submenu is not remembered or autofilled in the URL bar
     """
-    panel_ui = PanelUi(driver)
-    panel_ui.open()
+    # Instantiate objects
+    panel = PanelUi(driver)
     nav = Navigation(driver)
     context_menu = ContextMenu(driver)
 
-    panel_ui.open_history_menu()
-    panel_ui.context_click("bookmark-by-title", labels=["Firefox Privacy Notice"])
-
+    # Open history menu and right-click on a specific history entry and delete it
+    panel.open_history_menu()
+    panel.context_click("bookmark-by-title", labels=["Firefox Privacy Notice"])
     context_menu.click_and_hide_menu("context-menu-delete-page")
-    nav.type_in_awesome_bar("Firefox Privacy Notice")
 
-    with driver.context(driver.CONTEXT_CHROME):
-        nav.expect(lambda _: len(nav.get_elements("results-dropdown")) == 1)
+    # Type the deleted page name in the URL bar and verify the deleted page is not suggested
+    nav.type_in_awesome_bar("Firefox Privacy Notice")
+    nav.expect(lambda _: len(nav.get_elements("results-dropdown")) == 1)
