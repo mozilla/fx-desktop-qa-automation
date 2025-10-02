aa88a271616fff9469aa961ee7acf5b7419e5ba4
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-10-02 11:39:34 +0300

    Skip failing test

--- tests/downloads/test_download_pdf_from_context_menu.py
+++ tests/downloads/test_download_pdf_from_context_menu.py
@@ -18,6 +18,7 @@ def delete_files_regex_string():
     return r".*i-9.pdf"
 
 
+@pytest.mark.skip(reason="Tracked in bug 1992076")
 @pytest.mark.headed
 def test_download_pdf_from_context_menu(
     driver: Firefox,
