c675421779ff9e9dedac9b7b1626dbd6bb4adc8c
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-10-17 15:07:11 +0300

    Use lambda instead of sleep

--- tests/password_manager/test_auto_saved_generated_password_context_menu.py
+++ tests/password_manager/test_auto_saved_generated_password_context_menu.py
@@ -52,7 +52,9 @@ def test_auto_saved_generated_password_context_menu(driver: Firefox):
     )
 
     # Verify the update doorhanger is displayed
-    sleep(3)
+    nav.expect(
+        lambda _: nav.element_visible("password-notification-key")
+    )
     nav.click_on("password-notification-key")
     autofill_popup_panel.expect(
         lambda _: UPDATE_DOORHANGER_TEXT
