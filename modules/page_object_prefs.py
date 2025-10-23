3b86cf0cdfca4d9db2c5caca1e4563ad2432b3c7
vsangereanMOZ <vsangerean@mozilla.com>
2025-10-23 10:04:11 +0300

    vs/search-mode-persists (#850)
    * vs/search-mode-persists
    
    * vs/search-mode-persists
    
    * vs/search-mode-persists
    
    * vs/search-mode-persists
    
    * vs/search-mode-persists

--- modules/page_object_prefs.py
+++ modules/page_object_prefs.py
@@ -65,6 +65,14 @@ class AboutPrefs(BasePage):
         search_input.send_keys(term)
         return self
 
+    def enable_private_window_suggestions(self):
+        """Enable 'Show search suggestions in Private Windows' if not already checked."""
+
+        checkbox = self.get_element("search-suggestion-in-private-windows")
+        if checkbox.get_attribute("checked") != "true":
+            checkbox.click()
+        return self
+
     def set_alternative_language(self, lang_code: str) -> BasePage:
         """Changes the browser language"""
         self.get_element("language-set-alternative-button").click()
@@ -690,6 +698,8 @@ class AboutPrefs(BasePage):
         dialog = self.get_element("unknown-content-type-dialog")
         dialog.send_keys(Keys.ESCAPE)
         return self
+
+
 
 
 class AboutAddons(BasePage):
