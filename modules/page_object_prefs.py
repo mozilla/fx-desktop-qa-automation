6d144f3366efa2a2436d9a660e704f23bc9bd9e5
Philimon Derib Ayalew <philimonset@gmail.com>
2025-11-11 16:08:09 +0100

    check firefox and gecko version used

--- modules/page_object_prefs.py
+++ modules/page_object_prefs.py
@@ -512,7 +512,9 @@ class AboutPrefs(BasePage):
         self.find_in_settings("pay")
         payment_button = self.find_element(By.XPATH, "//*[contains(@label, 'payment methods')]")
         logging.warning("Payment button: " + payment_button.get_attribute("label"))
-        logging.warning("Firefox Version: " + self.driver.capabilities.get("browserVersion"))
+        caps = self.driver.capabilities
+        logging.warning("Firefox Version: " + caps.get("browserVersion"))
+        logging.warning("Gecko Version: " + caps.get("moz:geckodriverVersion"))
         payment_button.click()
         iframe = self.get_element("browser-popup")
         return iframe
@@ -552,7 +554,9 @@ class AboutPrefs(BasePage):
         self.find_in_settings("pay")
         addresses_button = self.find_element(By.XPATH, "//*[(name()='button' or name()='moz-box-button') and contains(@label, 'addresses')]")
         logging.warning("Address button: " + addresses_button.get_attribute("label"))
-        logging.warning("Firefox Version: " + self.driver.capabilities.get("browserVersion"))
+        caps = self.driver.capabilities
+        logging.warning("Firefox Version: " + caps.get("browserVersion"))
+        logging.warning("Gecko Version: " + caps.get("moz:geckodriverVersion"))
         addresses_button.click()
         iframe = self.get_element("browser-popup")
         return iframe
