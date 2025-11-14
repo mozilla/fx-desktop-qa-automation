36e4cdb004f4fcd8becc9ae7628391a32c638a24
soncuteanca <soncutean.anca@gmail.com>
2025-11-14 17:36:49 +0200

    Reduce the wait time

--- tests/address_bar_and_search/test_addon_suggestion.py
+++ tests/address_bar_and_search/test_addon_suggestion.py
@@ -32,7 +32,7 @@ def test_addon_suggestion_based_on_search_input(driver: Firefox):
     nav = Navigation(driver)
     nav.set_awesome_bar()
 
-    sleep(10)  # Wait for Firefox to do backend addons initiation
+    sleep(3)  # Wait for Firefox to do backend addons initiation
     for input_text, addon_slug in INPUT_TO_ADDON_NAME.items():
         nav.type_in_awesome_bar(input_text)
 
