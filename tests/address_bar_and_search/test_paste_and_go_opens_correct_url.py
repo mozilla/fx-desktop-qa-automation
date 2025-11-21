4a3706e00bd7159de94aa95d9e23fa8b2ae95d23
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-11-21 13:40:44 +0200

    Test paste and go opens correct url

--- tests/address_bar_and_search/test_paste_and_go_opens_correct_url.py
+++ tests/address_bar_and_search/test_paste_and_go_opens_correct_url.py
@@ -1,13 +1,10 @@
-from time import sleep
-
 import pytest
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, Keys
 
-from modules.browser_object_context_menu import ContextMenu
 from modules.browser_object_navigation import Navigation
 from modules.page_object_generics import GenericPage
 
-URL_TO_COPY = "https://bug1320502.bmoattachments.org/attachment.cgi?id=8814655"
+CLIPBOARD_PAGE = "https://bug1320502.bmoattachments.org/attachment.cgi?id=8814655"
 URL = "https://developer.mozilla.org"
 
 
@@ -16,25 +13,25 @@ def test_case():
     return "3028883"
 
 
-def test_paste_and_go_opens_correct_url(driver: Firefox, pyperclip=None):
+def test_paste_and_go_opens_correct_url(driver: Firefox):
     """
     C3028883 - "Paste and Go" opens the right URL
     """
 
     # Instantiate objects
     nav = Navigation(driver)
-    page = GenericPage(driver, url=URL_TO_COPY)
-    context_menu = ContextMenu(driver)
+    page = GenericPage(driver, url=CLIPBOARD_PAGE)
 
-    # Copy the link
+    # Copy the link from the clipboard
     page.open()
-    test = page.get_element("url-to-copy").get_attribute("value")
-    print(test)
+    page.click_on("url-to-copy")
+    page.perform_key_combo(Keys.COMMAND, "a")
+    page.copy()
 
     # Open a new tab and right click in the Address bar and choose Paste and Go
     nav.open_and_switch_to_new_window("tab")
     nav.get_awesome_bar()
     nav.click_and_hide_menu("context-menu-paste-and-go")
-    sleep(5)
 
     # Check that the page is displayed
+    nav.url_contains(URL)
