e00bab7efdd56c7a46d73c617fe72ffbf87575fb
Hani Yacoub <hani.yacoub@svadmins-MBP.lan>
2025-11-24 16:51:58 +0200

    Test searchmode update on alias prefix

--- tests/address_bar_and_search/test_searchmode_update_on_alias_prefix.py
+++ tests/address_bar_and_search/test_searchmode_update_on_alias_prefix.py
@@ -1,7 +1,12 @@
 import pytest
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, Keys
 
 from modules.browser_object_navigation import Navigation
+
+
+ENGINE_KEYWORD = "@bing"
+TEXT = "QA"
+ENGINE = "Bing"
 
 
 @pytest.fixture()
@@ -16,3 +21,21 @@ def test_searchmode_update_on_alias_prefix(driver: Firefox):
 
     # Instantiate objects
     nav = Navigation(driver)
+
+    # TODO: Go to about:preferences#search and set a keyword for bing, for example: bang
+
+    # Open a new tab, focus the urlbar and enter any string
+    nav.type_in_awesome_bar(TEXT)
+
+    # Start pressing LEFT KEY on the keyboard so the text insertion cursor is at the beginning
+    for _ in range(2):
+        nav.perform_key_combo_chrome(Keys.LEFT)
+
+    # Type @bing and press SPACE
+    nav.type_in_awesome_bar(ENGINE_KEYWORD, reset=False)
+    nav.perform_key_combo_chrome(Keys.SPACE)
+
+    # Search shortcut should be identified and translated to search mode for bing with the original text
+    # remaining as search query
+    nav.verify_engine_returned(ENGINE)
+    nav.verify_plain_text_in_input_awesome_bar(TEXT)
