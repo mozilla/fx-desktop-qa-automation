33c4141c3a3c57067b2b2b7f1999dfa18627ff35
soncuteanca <soncutean.anca@gmail.com>
2025-11-28 00:15:21 +0200

    Replace the dirty profile

--- tests/address_bar_and_search/test_delete_history_entry_via_firefox_suggest_completion_list.py
+++ tests/address_bar_and_search/test_delete_history_entry_via_firefox_suggest_completion_list.py
@@ -1,14 +1,8 @@
-import time
-
 import pytest
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 
-from modules.browser_object import Navigation, PanelUi, TabBar
-
-# @pytest.fixture()
-# def use_profile():
-#     return "theme_change"
+from modules.browser_object import Navigation, PanelUi
 
 
 @pytest.fixture()
@@ -16,33 +10,31 @@ def test_case():
     return "3028901"
 
 
-YOUTUBE_URL = "https://www.youtube.com/"
-FACEBOOK_URL = "https://www.facebook.com/"
-AMAZON_URL = "https://www.amazon.com/"
+HISTORY_PAGES = [
+    "https://www.imdb.com/",
+    "https://www.youtube.com/",
+    "https://www.nasa.gov/",
+]
 
-WEBSITES = [YOUTUBE_URL, FACEBOOK_URL, AMAZON_URL]
 SEARCH_TERM = "you"
 HISTORY_ENTRY = "YouTube"
 
 
 def test_delete_history_from_url_bar_completion_list(driver: Firefox):
     """
-    C3028901 Verify that deleting an entry from the Firefox Suggest completion list also removes the same entry from
+    C3028901 - Verify that deleting an entry from the Firefox Suggest completion list also removes the same entry from
     the History menu.
     """
     # Initialize page objects
     nav = Navigation(driver)
     panel = PanelUi(driver)
-    tabs = TabBar(driver)
 
-    for url in WEBSITES:
+    # Create history by navigating to each page sequentially
+    for url in HISTORY_PAGES:
         driver.get(url)
 
     # Ensure the targeted entry is visible in History menu
     panel.verify_history_item_exists(HISTORY_ENTRY)
-
-    # tabs.new_tab_by_button()
-    # tabs.switch_to_new_tab()
 
     # Type into the URL bar to trigger suggestions
     nav.type_in_awesome_bar(SEARCH_TERM)
@@ -51,13 +43,6 @@ def test_delete_history_from_url_bar_completion_list(driver: Firefox):
     # Select the suggestion and delete using Shift+Backspace
     nav.perform_key_combo_chrome(Keys.ARROW_DOWN)
     nav.perform_key_combo_chrome(Keys.SHIFT, Keys.BACKSPACE)
-
-    # # Wait for the deleted suggestion to be removed from the list (using base page expect)
-    # with nav.driver.context(nav.driver.CONTEXT_CHROME):
-    #     nav.expect(
-    #         lambda d: HISTORY_ENTRY not in
-    #         [el.text for el in nav.get_elements("suggestion-titles")]
-    #     )
 
     # Verify the entry is removed from History menu
     panel.verify_history_item_not_exists(HISTORY_ENTRY)
