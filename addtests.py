8e1783cd2ff7539457645fd72e81ff3e62237361
Ben Chatterton <bchatterton@mozilla.com>
2025-12-04 17:04:31 -0800

    now test lint passes

--- addtests.py
+++ addtests.py
@@ -11,6 +11,6 @@ TEST_KEY = os.path.join("manifests", "key.yaml")
 if __name__ == "__main__":
     manifest = testkey.TestKey(TEST_KEY)
     if len(sys.argv) > 1 and sys.argv[1] == "-q":
-         manifest.addtests(interactive=False)
+        manifest.addtests(interactive=False)
     else:
         manifest.addtests()
