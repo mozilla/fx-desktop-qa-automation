50570ea5a9cca6aa8052ba6f5ca3c6e1d7c5e6e3
Ben Chatterton <bchatterton@mozilla.com>
2025-12-18 15:45:25 -0800

    fix frequently used

--- tests/menus/test_frequently_used_context_menu.py
+++ tests/menus/test_frequently_used_context_menu.py
@@ -17,20 +17,16 @@ def test_case():
 
 # Test is unstable in Windows GHA for now
 @pytest.mark.headed
-def test_save_page_as(driver: Firefox):
+def test_save_page_as(driver: Firefox, sys_platform):
     """
     C2637623.1: save page as
     """
     try:
-        from pynput.keyboard import Controller, Key
+        from pynput.keyboard import Controller
     except ModuleNotFoundError:
         pytest.skip("Could not load pynput")
 
     controller = Controller()
-
-    def key_press_release(key: Key):
-        controller.press(key)
-        controller.release(key)
 
     # create objects
     context_menu = ContextMenu(driver)
@@ -46,27 +42,8 @@ def test_save_page_as(driver: Firefox):
     context_menu.click_and_hide_menu("context-menu-save-page-as")
 
     # short sleep to ensure menu is shown
-    sleep(0.5)
-
-    # perform key presses to save the file
-    this_platform = platform.system()
-    if this_platform == "Linux":
-        controller.press(Key.alt)
-        controller.press(Key.tab)
-        controller.release(Key.tab)
-        controller.release(Key.alt)
-
-        controller.press(Key.alt)
-        controller.press(Key.tab)
-        controller.release(Key.tab)
-        controller.release(Key.alt)
-
-        key_press_release(Key.tab)
-
-        key_press_release(Key.tab)
-
-    # Press and release the Enter key
-    key_press_release(Key.enter)
+    sleep(2)
+    context_menu.handle_os_download_confirmation(controller, sys_platform)
 
     # Wait for the animation to complete
     nav.wait_for_download_animation_finish()
@@ -83,22 +60,18 @@ def test_take_screenshot(driver: Firefox):
     """
     # create objects
     context_menu = ContextMenu(driver)
+    nav = Navigation(driver)
     driver.get("https://example.com")
     example_page = ExamplePage(driver)
+    example_page.open()
 
     # ensure that the screenshot is not present
     example_page.element_does_not_exist("take-screenshot-box")
-
-    # right click the header
-    title_header = example_page.get_element("title-header")
-    example_page.context_click(title_header)
+    example_page.context_click("title-header")
 
     # context click the screenshot option and verify its not hidden
     context_menu.click_and_hide_menu("context-menu-take-screenshot")
-
-    with driver.context(driver.CONTEXT_CHROME):
-        screenshot_box = example_page.get_element("take-screenshot-box")
-        assert screenshot_box.get_attribute("hidden") is None
+    nav.element_exists("content-area-menu")
 
 
 def test_inspect(driver: Firefox):
@@ -112,8 +85,8 @@ def test_inspect(driver: Firefox):
     devtools = Devtools(driver)
 
     # right click something that is not a hyperlink
-    title_header = example_page.get_element("title-header")
-    example_page.context_click(title_header)
+    example_page.get_element("title-header")
+    example_page.context_click("title-header")
 
     context_menu.click_and_hide_menu("context-menu-inspect")
 
