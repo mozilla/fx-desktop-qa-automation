8206301f3358b27c95eeab29073c5c70563f3d55
Tracy <twalker@mozilla.com>
2026-01-16 14:28:46 -0600

    Use method to wait for tabs

--- tests/session_restore/test_restore_multiple_closed_tabs_at_once.py
+++ tests/session_restore/test_restore_multiple_closed_tabs_at_once.py
@@ -30,7 +30,7 @@ def test_restore_multiple_closed_tabs(driver: Firefox, tabs: TabBar, sys_platfor
         tabs.new_tab_by_button()
         driver.switch_to.window(driver.window_handles[-1])
         driver.get(URLS[i])
-    assert len(driver.window_handles) == 5
+    tabs.wait_for_num_tabs(5)
 
     # Close those 4 tabs with tab context menu option
     original_tab = tabs.get_tab(1)
@@ -38,11 +38,11 @@ def test_restore_multiple_closed_tabs(driver: Firefox, tabs: TabBar, sys_platfor
     context_menu.click_context_item("context-menu-close-multiple-tabs")
     context_menu.click_and_hide_menu("context-menu-close-multiple-tabs-to-right")
     context_menu.hide_popup_by_child_node("context-menu-close-multiple-tabs")
-    sleep(0.5)  # Give Firefox a moment to close the tabs
-    assert len(driver.window_handles) == 1
+    tabs.wait_for_num_tabs(1)
 
     # Use the method to restore the closed tabs with shortcut
     tabs.reopen_tabs_with_shortcut(sys_platform, count=1)
+    tabs.wait_for_num_tabs(5)
 
     # Verify the tabs are restored
     open_urls = set()
