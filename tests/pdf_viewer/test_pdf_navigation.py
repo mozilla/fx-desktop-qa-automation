f39f07903461411f129b334bde506a92c5a095ba
Tracy <twalker@mozilla.com>
2026-01-23 15:24:17 -0600

    Use skip mark

--- tests/pdf_viewer/test_pdf_navigation.py
+++ tests/pdf_viewer/test_pdf_navigation.py
@@ -44,77 +44,80 @@ def _move_pdf_and_get_delta(pdf_viewer: GenericPdf, key: str | int) -> int:
     return abs(page_one_item.location["y"] - initial_location)
 
 
-# def test_navigation_keys(driver: Firefox, pdf_viewer: GenericPdf):
-#     """
-#     C3927.1: ensure that you can navigate through a PDF using keys
-#
-#     Arguments:
-#         pdf_viewer: instance of generic pdf with given pdf_file_path.
-#     """
-#     # scroll down using arrow down key
-#     down_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.DOWN)
-#
-#     # scroll down using right key
-#     right_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.RIGHT)
-#     # assert right key moves more than down key
-#     assert right_key_difference > down_key_difference
-#
-#     # scroll down using end key
-#     end_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.END)
-#     # assert end key moves more than right key
-#     assert end_key_difference > right_key_difference
-#
-#     # scroll up using the Up key
-#     up_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.UP)
-#
-#     # scroll up using left key
-#     left_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.LEFT)
-#     # assert left key moves more than up key
-#     assert left_key_difference > up_key_difference
-#
-#     # scroll using home key
-#     home_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.HOME)
-#     # assert home key moves more than left key
-#     assert home_key_difference > left_key_difference
-#
-#
-# def test_navigation_next_prev(driver: Firefox, pdf_viewer: GenericPdf):
-#     """
-#     C3927.2: ensure that you can navigate through a PDF using next and previous keys
-#
-#     Arguments:
-#         pdf_viewer: instance of generic pdf with given pdf_file_path.
-#     """
-#     # go to next page.
-#     _move_pdf_and_get_delta(pdf_viewer, "next")
-#     # go back to original page.
-#     _move_pdf_and_get_delta(pdf_viewer, "prev")
-#
-#
-# def test_navigation_page_numbers(driver: Firefox, pdf_viewer: GenericPdf):
-#     """
-#     C3927.3: Ensure that you can navigate through a PDF using the numbers at the top.
-#
-#     Arguments:
-#         pdf_viewer: instance of generic pdf with given pdf_file_path.
-#     """
-#     # get an element from page one
-#     page_one_item = pdf_viewer.get_element("page-one-item")
-#     original_position = page_one_item.location["y"]
-#
-#     # jump to the second page and measure the delta
-#     one_page_delta = _move_pdf_and_get_delta(pdf_viewer, 2)
-#
-#     # jump to fourth page and measure the delta
-#     two_page_delta = _move_pdf_and_get_delta(pdf_viewer, 4)
-#
-#     # ensure that the jump for two pages was twice of the one page jump
-#     assert (2 * one_page_delta) - 10 <= two_page_delta <= (2 * one_page_delta) + 10
-#
-#     # jump back to original position and ensure we are back where we started
-#     pdf_viewer.jump_to_page(1)
-#     first_page_position = page_one_item.location["y"]
-#     assert first_page_position == original_position
+@pytest.mark.skip(reason="see bug 2010352")
+def test_navigation_keys(driver: Firefox, pdf_viewer: GenericPdf):
+    """
+    C3927.1: ensure that you can navigate through a PDF using keys
+
+    Arguments:
+        pdf_viewer: instance of generic pdf with given pdf_file_path.
+    """
+    # scroll down using arrow down key
+    down_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.DOWN)
+
+    # scroll down using right key
+    right_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.RIGHT)
+    # assert right key moves more than down key
+    assert right_key_difference > down_key_difference
+
+    # scroll down using end key
+    end_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.END)
+    # assert end key moves more than right key
+    assert end_key_difference > right_key_difference
+
+    # scroll up using the Up key
+    up_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.UP)
+
+    # scroll up using left key
+    left_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.LEFT)
+    # assert left key moves more than up key
+    assert left_key_difference > up_key_difference
+
+    # scroll using home key
+    home_key_difference = _move_pdf_and_get_delta(pdf_viewer, Keys.HOME)
+    # assert home key moves more than left key
+    assert home_key_difference > left_key_difference
+
+
+@pytest.mark.skip(reason="see bug 2010352")
+def test_navigation_next_prev(driver: Firefox, pdf_viewer: GenericPdf):
+    """
+    C3927.2: ensure that you can navigate through a PDF using next and previous keys
+
+    Arguments:
+        pdf_viewer: instance of generic pdf with given pdf_file_path.
+    """
+    # go to next page.
+    _move_pdf_and_get_delta(pdf_viewer, "next")
+    # go back to original page.
+    _move_pdf_and_get_delta(pdf_viewer, "prev")
+
+
+@pytest.mark.skip(reason="see bug 2010352")
+def test_navigation_page_numbers(driver: Firefox, pdf_viewer: GenericPdf):
+    """
+    C3927.3: Ensure that you can navigate through a PDF using the numbers at the top.
+
+    Arguments:
+        pdf_viewer: instance of generic pdf with given pdf_file_path.
+    """
+    # get an element from page one
+    page_one_item = pdf_viewer.get_element("page-one-item")
+    original_position = page_one_item.location["y"]
+
+    # jump to the second page and measure the delta
+    one_page_delta = _move_pdf_and_get_delta(pdf_viewer, 2)
+
+    # jump to fourth page and measure the delta
+    two_page_delta = _move_pdf_and_get_delta(pdf_viewer, 4)
+
+    # ensure that the jump for two pages was twice of the one page jump
+    assert (2 * one_page_delta) - 10 <= two_page_delta <= (2 * one_page_delta) + 10
+
+    # jump back to original position and ensure we are back where we started
+    pdf_viewer.jump_to_page(1)
+    first_page_position = page_one_item.location["y"]
+    assert first_page_position == original_position
 
 
 def test_toolbar_options_cursor(driver: Firefox, pdf_viewer: GenericPdf):
