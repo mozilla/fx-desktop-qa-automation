abfa3e9cc5cae834ad1bd750d643ff5345e0e4da
Tracy <twalker@mozilla.com>
2026-02-05 13:04:04 -0600

    Tracy/fix browser launch home page test (#1063)
    * Simplify browser start Home page check
    
    * Re-enable the tests

--- tests/preferences/test_firefox_home_on_launch.py
+++ tests/preferences/test_firefox_home_on_launch.py
@@ -1,9 +1,7 @@
 import pytest
 from selenium.webdriver import Firefox
 
-from modules.browser_object import PanelUi, TabBar
-from modules.components.dropdown import Dropdown
-from modules.page_object import AboutNewtab, AboutPrefs
+from modules.page_object import AboutNewtab
 
 
 @pytest.fixture()
@@ -21,39 +19,13 @@ def add_to_prefs_list():
     ]
 
 
-@pytest.fixture()
-def about_prefs_category():
-    return "home"
-
-
-@pytest.fixture()
-def drop_down_root(about_prefs: AboutPrefs):
-    about_prefs.open()
-    return about_prefs.get_element("home-new-window-dropdown")
-
-
 def test_firefox_home_on_launch(
     driver: Firefox,
-    tabs: TabBar,
     about_new_tab: AboutNewtab,
-    about_prefs: AboutPrefs,
-    panel_ui: PanelUi,
-    dropdown: Dropdown,
 ):
     """
     C143543: setting the default new window to be Firefox Home
     """
 
-    # click the dropdown
-    dropdown.select_option("Firefox Home (Default)")
-
-    # make sure that the option was selected correctly
-    about_prefs.element_attribute_is(
-        "home-new-window-dropdown", "label", "Firefox Home (Default)"
-    )
-
-    # open a new window
-    panel_ui.open_and_switch_to_new_window("window")
-
-    # make sure we are on the correct new window
+    # Make sure the Firefox Home page is opened on browser launch
     about_new_tab.element_exists("body-logo")
