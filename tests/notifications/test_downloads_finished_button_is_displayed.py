02fe539711feb3622f08a1442593f4d6cd6005cb
virgil.sangerean <vsangerean@mozilla.com>
2026-02-16 15:44:01 +0200

    vs/test-verify-downloads-button

--- tests/notifications/test_downloads_finished_button_is_displayed.py
+++ tests/notifications/test_downloads_finished_button_is_displayed.py
@@ -4,7 +4,6 @@ from selenium.webdriver import Firefox
 from modules.browser_object_navigation import Navigation
 from modules.page_object_generics import GenericPage
 
-
 TEST_URL = "https://download.samplelib.com/mp3/sample-3s.mp3"
 
 
