07c0236a188c07b7c7138dc4c0db7d7c6fd8d5c5
Ben Chatterton <bchatterton@mozilla.com>
2026-02-26 09:36:44 -0800

    cryptominers 2

--- tests/security_and_privacy/test_cryptominers_blocked_and_shown_in_info_panel.py
+++ tests/security_and_privacy/test_cryptominers_blocked_and_shown_in_info_panel.py
@@ -13,19 +13,14 @@ def test_case():
 CRYPTOMINERS_URL = "https://senglehardt.com/test/trackingprotection/test_pages/fingerprinting_and_cryptomining.html"
 
 
-def test_cryptominers_blocked_and_shown_in_info_panel(driver: Firefox):
+def test_cryptominers_blocked_and_shown_in_info_panel(
+    driver: Firefox, tracker_panel: TrackerPanel
+):
     """
     C450232: Cryptominers are blocked and shown in Standard mode in the Information pannel
     """
-    # Access URL, needed sleep otherwise cryptomining will be displayed as unblocked
-    nav = Navigation(driver)
     tracking_page = GenericPage(driver, url=CRYPTOMINERS_URL)
-    tracker_panel = TrackerPanel(driver)
-
     tracking_page.open()
-    tracker_panel.wait_for_blocked_tracking_icon(nav, tracking_page)
-
-    # Access url and click on the shield icon and verify that cryptominers are blocked
-    tracking_page.open()
-    tracker_panel.wait_for_blocked_tracking_icon(nav, tracking_page)
-    nav.assert_blocked_trackers("cryptominers")
+    tracker_panel.open_panel()
+    tracker_panel.wait_for_trackers()
+    tracker_panel.trackers_blocked("cryptominer")
