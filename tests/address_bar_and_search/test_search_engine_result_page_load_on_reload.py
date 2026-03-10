b122ed900ca50f104d129405c4225849285e791a
soncuteanca <soncutean.anca@gmail.com>
2026-03-10 17:53:05 +0200

    Remove back references

--- tests/address_bar_and_search/test_search_engine_result_page_load_on_reload.py
+++ tests/address_bar_and_search/test_search_engine_result_page_load_on_reload.py
@@ -16,7 +16,7 @@ def test_case():
     return "3028909"
 
 
-def test_search_engine_result_page_load_on_reload_or_back(driver: Firefox):
+def test_search_engine_result_page_load_on_reload(driver: Firefox):
     """
     C3028909 - Search Engine Result Page loads as a result of a reload
     """
