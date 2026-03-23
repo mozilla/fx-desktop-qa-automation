ebd637b2e9dc161ea3ccbe2f0230d350c780a588
soncuteanca <soncutean.anca@gmail.com>
2026-03-23 11:36:52 +0200

    Add changes based on the review

--- tests/sidebar/test_sidebar_vertical_tabs_move_options.py
+++ tests/sidebar/test_sidebar_vertical_tabs_move_options.py
@@ -45,17 +45,20 @@ def test_sidebar_vertical_tabs_move_options(driver: Firefox):
     tabs.context_click(tabs.get_tab(2))
     context_menu.click_and_hide_menu("context-menu-move-tab-to-start")
     tabs.hide_popup("tabContextMenu")
-    assert driver.window_handles[0] == selected_handle
+    driver.switch_to.window(driver.window_handles[0])
+    assert driver.title == middle_tab_title
 
     # Move tab to End and verify it is now the last tab
     tabs.context_click(tabs.get_tab(2))
     context_menu.click_and_hide_menu("context-menu-move-tab-to-end")
     tabs.hide_popup("tabContextMenu")
-    assert driver.window_handles[-1] == selected_handle
+    driver.switch_to.window(driver.window_handles[-1])
+    assert driver.title == middle_tab_title
 
-    # Move tab to New Window and verify the original window has one fewer tab
+    # Move tab to New Window and verify the tab is in a new window
     tabs.context_click(tabs.get_tab(2))
     context_menu.click_and_hide_menu("context-menu-move-to-new-window")
     tabs.hide_popup("tabContextMenu")
+    assert len(driver.window_handles) == NUM_TABS
     driver.switch_to.window(selected_handle)
     assert driver.title == middle_tab_title
