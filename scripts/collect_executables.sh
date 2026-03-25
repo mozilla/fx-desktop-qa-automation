d77afcf661d02802b6ad4b25989ed21c4ff86840
Ben Chatterton <bchatterton@mozilla.com>
2026-03-25 12:41:17 -0700

    fix collect ex bash

--- scripts/collect_executables.sh
+++ scripts/collect_executables.sh
@@ -16,7 +16,7 @@ else
     then
         curl -o firefox.tar.xz -L "${MANUAL_DOWNLOAD_LINK}"
     else
-        pipenv run python collect_executables.py
+        pipenv run python ./scripts/collect_executables.py
         curl -o firefox.tar.xz -L "$(pipenv run python ./scripts/collect_executables.py)"
     fi
     tar xf firefox.tar.xz
