a18581938e8913f8510a5cf96be7dd828ab35a8b
Hani Yacoub <hani.yacoub@192.168.1.129>
2026-03-30 16:03:38 +0300

    Edit method

--- modules/browser_object_autofill_popup.py
+++ modules/browser_object_autofill_popup.py
@@ -160,7 +160,7 @@ class AutofillPopup(BasePage):
         return self
 
     @BasePage.context_chrome
-    def dismiss_password_doorhanger(self):
+    def dismiss_password_doorhanger(self) -> BasePage:
         """Dismiss the Password Manager doorhanger using ESC."""
         self.get_element("password-notification-username-field").send_keys(Keys.ESCAPE)
         return self
