eb10a764097f8ee5cac1d5ad6ea85093adf1d738
Hani Yacoub <hani.yacoub@192.168.1.129>
2026-04-27 15:06:19 +0300

    Edit test and remove a method

--- tests/password_manager/test_edit_autofill_after_pp_dismissed.py
+++ tests/password_manager/test_edit_autofill_after_pp_dismissed.py
@@ -76,6 +76,7 @@ def test_edit_autofill_after_pp_dismissed(driver: Firefox):
 
     # Click on the grey key icon, choose to save the credentials and reload the form
     nav.click_on("password-notification-key")
+    autofill_popup_panel.verify_username_value(SECOND_USERNAME)
     autofill_popup_panel.click_doorhanger_button("save")
     tabs.open_and_switch_to_new_tab()
     login_autofill.open()
@@ -84,8 +85,4 @@ def test_edit_autofill_after_pp_dismissed(driver: Firefox):
     # (1 from preconditions and 1 for the newly saved credentials)
     login_autofill.click_on("username-login-field")
     autofill_popup_panel.verify_autocomplete_option(USERNAME)
-    if not autofill_popup_panel.is_autocomplete_option_present(SECOND_USERNAME):
-        login_autofill.open()
-        login_autofill.click_on("username-login-field")
-
     autofill_popup_panel.verify_autocomplete_option(SECOND_USERNAME)
