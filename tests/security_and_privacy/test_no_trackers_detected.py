017170b52213cebb3ffd365a568b8628b3c25aca
Hani Yacoub <hani.yacoub@192.168.1.129>
2026-04-30 16:16:52 +0300

    Edit test

--- tests/security_and_privacy/test_no_trackers_detected.py
+++ tests/security_and_privacy/test_no_trackers_detected.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pytest
 from selenium.webdriver import Firefox
 
@@ -18,6 +20,7 @@ def test_no_trackers_detected(driver: Firefox, trust_panel: TrustPanel):
     """
     # access url
     driver.get(NOTRACKERS_URL)
+    sleep(111111)
     # verify that no trackers are detected
     trust_panel.open_panel()
     trust_panel.assert_no_trackers()
--- tests/security_and_privacy/test_trackers_cryptominers_fingerprinters_blocked.py
+++ tests/security_and_privacy/test_trackers_cryptominers_fingerprinters_blocked.py
@@ -2,8 +2,7 @@ import pytest
 from selenium.webdriver import Firefox
 
 from modules.browser_object import Navigation, TrustPanel
-from modules.page_object import GenericPage
-from modules.page_object_prefs import AboutPrefs
+from modules.page_object import AboutPrefs, GenericPage
 
 
 @pytest.fixture()
@@ -11,13 +10,11 @@ def test_case():
     return "3054906"
 
 
-TRACKER_URL = (
-    "https://senglehardt.com/test/trackingprotection/test_pages/fingerprinting_and_cryptomining_and_cookies.html"
-)
+TRACKER_URL = "https://senglehardt.com/test/trackingprotection/test_pages/fingerprinting_and_cryptomining_and_cookies.html"
 
 
 def test_trackers_cryptominers_fingerprinters_blocked(
-    driver: Firefox, trust_panel: TrustPanel, nav: Navigation
+    driver: Firefox, trust_panel: TrustPanel
 ):
     """
     C3054906 - The ETP panel is correctly displayed when the cross-site trackers, cryptominers and fingerprinters are blocked
@@ -38,5 +35,5 @@ def test_trackers_cryptominers_fingerprinters_blocked(
     # The Tracking Content is NOT blocked
     trust_panel.trackers_detected("tracking-content")
 
-    # The Cross-Site Tracking Cookies, Fingerpinters and Cryptominers are displayed "Blocked"
+    # The Cross-Site Tracking Cookies, Fingerprinters and Cryptominers are displayed "Blocked"
     trust_panel.trackers_blocked("tracking-cookies", "cryptominer", "fingerprinter")
