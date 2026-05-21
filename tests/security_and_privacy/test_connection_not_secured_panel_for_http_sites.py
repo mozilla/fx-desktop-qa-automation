a65b9408120c4137d2493ce10a27e2a6ccf5636f
Hani Yacoub <hani.yacoub@192.168.1.129>
2026-05-21 17:09:37 +0300

    Test connection not secrued panel for http sites

---
+++ tests/security_and_privacy/test_connection_not_secured_panel_for_http_sites.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium.webdriver import Firefox
+
+from modules.browser_object_trust_panel import TrustPanel
+from modules.page_object_generics import GenericPage
+
+HTTP_SITE = "http://example.com/"
+
+
+@pytest.fixture()
+def test_case():
+    return "3054044"
+
+
+def test_connection_not_secured_panel_for_http_sites(driver: Firefox):
+    """
+    C3054044 - Connection not secure is correctly displayed for plain HTTP site
+    """
+
+    # Instantiate objects
+    test_page = GenericPage(driver, url=HTTP_SITE)
+    trust_panel = TrustPanel(driver)
+
+    # Open test page and click on the shield icon
+    test_page.open()
+    trust_panel.open_panel()
+
+    # Click the lock text row (or dedicated “Connection not secure” arrow)
+    trust_panel.click_connection_button()
+
+    # Panel header shows “Connection protections for example.com”
+    trust_panel.element_visible("trustpanel-security-information-view")
