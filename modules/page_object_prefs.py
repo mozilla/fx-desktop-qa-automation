cbd2469b7f7be49887612d5af56e37d044755af9
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2026-05-29 05:57:07 -0700

    Stabilize Autofill from Nova changes (#1375)
    * fix batch 1
    
    * fix tests
    
    * sneak a check on just autofill
    
    * selectors
    
    * reset to full test
    
    * reset pyproject & get review
    
    * mark failing test; quick fixes
    
    * key

--- modules/page_object_prefs.py
+++ modules/page_object_prefs.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 from time import sleep
 from typing import List, Literal
@@ -329,7 +328,7 @@ class AboutPrefs(BasePage):
         menulist_popup.select_by_value(option)
         return self
 
-    # Payment and Address Management
+    # ---- Payment and Address Management ---------------------------------------------------------
     def verify_cc_json(
         self, cc_info_json: dict, credit_card_fill_obj: CreditCardBase
     ) -> BasePage:
@@ -490,18 +489,26 @@ class AboutPrefs(BasePage):
             "expiration_year": "cc-exp-year",
             "name": "cc-name",
         }
+        self.switch_to_iframe_context(self.get_element("browser-popup"))
         if field_name not in fields.keys():
             raise ValueError(
                 f"{field_name} is not a valid field name for the cc dialog form."
             )
-        self.switch_to_edit_saved_payments_popup_iframe()
         value_field = self.find_element(By.ID, fields[field_name])
         if value.isdigit():
             value = int(value)
         if field_name == "expiration_year":
-            value -= (datetime.datetime.now().year % 100) + 1
+            if int(value) < 100:  # new exp years are all 4-digit
+                value = int(value) + 2000
+            value_field.click()
+            option = next(
+                el
+                for el in value_field.find_elements(By.TAG_NAME, "option")
+                if el.text == str(value)
+            )
+            option.click()
 
-        if value_field.tag_name == "select":
+        elif value_field.tag_name == "select":
             Select(value_field).select_by_index(value)
         else:
             value_field.clear()
@@ -558,19 +565,44 @@ class AboutPrefs(BasePage):
         """Get data-l10n-args from the saved payment card"""
         return self._get_tile_data("payment", idx)
 
+    def _edit_tile(self, tile_type: str, idx=0):
+        """Open the edit view of payment or address"""
+        tiles = self.get_elements(f"edit-{tile_type}")
+        tiles[idx].click()
+
+    def edit_address(self, idx=0):
+        """Click the edit button on a given address"""
+        self._edit_tile("address", idx)
+
+    def edit_payment(self, idx=0):
+        """Click the edit button on a given payment"""
+        self._edit_tile("payment", idx)
+
+    def _get_autofill_profiles(self, tile_type: str) -> List[WebElement]:
+        """Gets any type of autofill profile, do not use against n=0"""
+        self.element_visible(f"saved-{tile_type}-entry")
+        return self.get_elements(f"saved-{tile_type}-entry")
+
     def get_all_saved_cc_profiles(self) -> List[WebElement]:
         """Gets the saved credit card profiles in the cc panel"""
-        self.switch_to_saved_payments_popup_iframe()
-        element = Select(self.get_element("cc-saved-options"))
-        return element.options
+        return self._get_autofill_profiles("payment")
 
     def get_all_saved_address_profiles(self) -> List[WebElement]:
         """Gets the saved credit card profiles in the cc panel"""
-        self.switch_to_saved_addresses_popup_iframe()
-        select_el = self.get_element("address-saved-options")
-        if len(select_el.get_attribute("innerHTML")) > 1:
-            return Select(select_el).options
-        return []
+        return self._get_autofill_profiles("address")
+
+    def _confirm_n_profiles(self, tile_type: str, n: int) -> BasePage:
+        """Confirm that _n_ profiles of a type exist, where n>0"""
+        self.expect(lambda _: len(self._get_autofill_profiles(tile_type)) == n)
+        return self
+
+    def confirm_n_addresses(self, n: int) -> BasePage:
+        """Confirm that _n_ addresses exist where n>0"""
+        return self._confirm_n_profiles("address", n)
+
+    def confirm_n_payments(self, n: int) -> BasePage:
+        """Confirm that _n_ payments exist where n>0"""
+        return self._confirm_n_profiles("payment", n)
 
     def extract_address_data_from_saved_addresses_entry(
         self, util: Utilities, region: str = "US"
@@ -631,7 +663,7 @@ class AboutPrefs(BasePage):
         Switch to form iframe to edit saved payments.
         """
         self.switch_to_default_frame()
-        self.switch_to_iframe(2)
+        self.switch_to_iframe_context(self.get_element("browser-popup"))
         return self
 
     def press_button_get_popup_dialog_iframe(self, button_label: str) -> WebElement:
