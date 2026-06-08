00c3d61c7d4b1a1a2cc1604e5906e410cb8e2fc7
Ben Chatterton <bchatterton@mozilla.com>
2026-06-08 11:50:22 -0700

    fix EOF

---
+++ scripts/switch_config.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import toml
+
+if __name__ == "__main__":
+    pyproject = toml.loads(open("pyproject.toml").read())
+    config_path = os.path.join("config", f"{sys.argv[1]}_pyproject.toml")
+    if not os.path.isfile(config_path):
+        sys.exit(f"Config not found at {config_path}")
+    config = toml.loads(open(config_path).read())
+    pyproject |= config
+    toml_text = toml.dumps(pyproject)
+
+    # prettify the toml output
+    toml_out = ""
+    for line in toml_text.split("\n"):
+        if len(line) < 80:
+            toml_out = toml_out + line + "\n"
+            continue
+        line_out = line.replace('[ "', '[\n  "')
+        line_out = line_out.replace(', "', ',\n  "')
+        line_out = line_out.replace(",]", ",\n]")
+        toml_out = toml_out + line_out + "\n"
+
+    toml_out = toml_out[:-1]
+    with open("pyproject.toml", "w") as fh:
+        fh.write(toml_out)
