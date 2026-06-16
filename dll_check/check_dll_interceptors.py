c531c3dcb7a7c3ee374db3cd282369c2e1abe21a
Ben Chatterton <bchatterton@mozilla.com>
2026-06-16 11:24:44 -0600

    use thclient for all th

--- dll_check/check_dll_interceptors.py
+++ dll_check/check_dll_interceptors.py
@@ -1,18 +1,19 @@
 import os
-import re
 from subprocess import check_call, check_output
 
 import requests
 import thclient
-from bs4 import BeautifulSoup as soup
 
 # Assume beta link is in env as FX_DOWNLOAD_LINK
+PROJECT = "mozilla-beta"
 BUILDHUB = "buildhub.json"
+SEARCH_STRING = "Windows,x86,Shippable,opt,build-win32-shippable/opt,B"
+JOB_TYPE_NAME = "build-win{}-shippable/opt"
 TREEHERDER_LINK = "https://treeherder.mozilla.org/jobs?repo=mozilla-beta&searchStr=Windows%2Cshippable%2Fopt&revision={}"
 ARTIFACT_LINK = "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/{}/runs/0/artifacts/public%2Fbuild%2Ftarget.cppunittest.tests.tar.zst"
 
 
-def main():
+def main(bits: int):
     # get buildhub json
     download_dir, _ = os.getenv("FX_DOWNLOAD_URL").rsplit("/", 1)
     response = requests.get(f"{download_dir}/{BUILDHUB}")
@@ -21,15 +22,12 @@ def main():
     # use buildhub info to derive TH link
     buildinfo = response.json()
     rev = buildinfo.get("source").get("revision")
-    response = requests.get(TREEHERDER_LINK.replace("{}", rev))
-    response.raise_for_status()
-    th_source = soup(response.content)
-    task_link = th_source.find(
-        "button", {"data-testid": "job-btn", "title": re.compile("^")}
-    )
-    job_id = task_link["data-job-id"]
     treeherder = thclient.TreeherderClient()
-    jobs_list = treeherder.get_jobs(project="mozilla-beta", id=job_id)
+    push = treeherder.get_pushes(PROJECT, searchStr=SEARCH_STRING, revision=rev)[0]
+    job_type = JOB_TYPE_NAME.replace("{}", str(bits))
+    jobs_list = treeherder.get_jobs(
+        project="mozilla-beta", job_type_name=job_type, push_id=push.get("id")
+    )
     if not jobs_list:
         return None  # replace with raise
 
@@ -37,9 +35,6 @@ def main():
     task_id = jobs_list[-1].get("task_id")
     artifact_json_url = ARTIFACT_LINK.replace("{}", task_id)
     response = requests.get(artifact_json_url)
-    response.raise_for_status()
-    artifact_url = response.json().get("url")
-    response = requests.get(artifact_url)
     response.raise_for_status()
 
     # write and extract artifact with tests
@@ -60,4 +55,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    for b in [32, 64]:
+        main(b)
