305badfc7817526f4cbaebddaba29dd5be8e0cbe
Ben Chatterton <bchatterton@mozilla.com>
2026-06-22 11:22:28 -0700

    update branch with whole files from proof

--- taskcluster/fx_desktop_qa_automation_taskgraph/transforms/new_beta_qa.py
+++ taskcluster/fx_desktop_qa_automation_taskgraph/transforms/new_beta_qa.py
@@ -6,7 +6,9 @@ transforms = TransformSequence()
 @transforms.add
 def apply_config(config, tasks):
     for task in tasks:
-        task["description"] = "Runs smoke test suite against new Beta, reports to TestRail"
+        task["description"] = (
+            "Runs smoke test suite against new Beta, reports to TestRail"
+        )
 
         env = task.setdefault("worker", {}).setdefault("env", {})
 
