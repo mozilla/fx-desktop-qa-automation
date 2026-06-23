69c53a8428992e96353be809ea9e17a08ea97da0
Hani Yacoub <hani.yacoub@192.168.1.129>
2026-06-23 14:29:59 +0300

    Fix test

--- modules/page_object_addons_mozilla_org.py
+++ modules/page_object_addons_mozilla_org.py
@@ -20,11 +20,30 @@ class AmoThemes(BasePage):
         self.theme_title = self.get_element("theme-title").get_attribute("innerText")
         self.get_element("install-button").click()
         # TODO: Add toolbar browser object
-        with self.driver.context(self.driver.CONTEXT_CHROME):
-            self.driver.find_element(By.CSS_SELECTOR, "button[label='Add']").click()
-            self.driver.find_element(By.CSS_SELECTOR, "button[label='OK']").click()
-        return self
+        # with self.driver.context(self.driver.CONTEXT_CHROME):
+        #     self.driver.find_element(By.CSS_SELECTOR, "button[label='Add']").click()
+        #     self.driver.find_element(By.CSS_SELECTOR, "button[label='OK']").click()
+        # return self
 
+    @BasePage.context_chrome
+    def confirm_language_install_popup(self):
+        """Confirms the language install popup by clicking Add then OK"""
+        # Wait for the Add button to appear
+        self.custom_wait(timeout=10).until(
+            lambda _: self.get_element("addon-install-add-button-parent") is not None
+        )
+
+        # Click "Add" button
+        self.js_click_on("addon-install-add-button-field")
+
+        # Wait for the OK button to appear
+        self.custom_wait(timeout=10).until(
+            lambda _: self.get_element("addon-install-ok-button-parent") is not None
+        )
+
+        # Click "OK" button
+        self.js_click_on("addon-install-ok-button-field")
+        return self
 
 class AmoLanguages(BasePage):
     """
