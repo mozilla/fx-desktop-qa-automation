d39e9a2e1abd9b62b9b3117b39b121d505a796f9
Hani Yacoub <hani.yacoub@192.168.1.132>
2026-07-21 14:10:53 +0300

    Edit method

--- modules/browser_object_autofill_popup.py
+++ modules/browser_object_autofill_popup.py
@@ -177,10 +177,15 @@ class AutofillPopup(BasePage):
         return self
 
     @BasePage.context_chrome
-    def verify_autocomplete_option(self) -> BasePage:
-        """Wait until an autocomplete login option is displayed in the dropdown."""
+    def verify_autocomplete_option(self, value: str) -> BasePage:
+        """Wait until an autocomplete option containing `value` is displayed in the dropdown."""
         self.custom_wait(timeout=10).until(
-            lambda _: self.get_element("select-form-option-by-value") is not None
+            lambda d: any(
+                value in item.get_attribute("textContent")
+                for item in d.find_elements(
+                    "css selector", "#PopupAutoComplete richlistitem"
+                )
+            )
         )
         return self
 
