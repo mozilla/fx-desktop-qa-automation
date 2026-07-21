947d2aca307a58b6989a86c6d250263189f64118
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2026-07-21 09:29:42 -0700

    tidy up pyautogui (#1498)
    * tidy up pyautogui
    
    * manifest
    
    * key
    
    * move files
    
    * avoid age verification
    
    * add wait
    
    * don't run pdf tests - new PR will solve
    
    * use local file for multi tab mute test
    
    * win change buffer
    
    * wait for tab to show mediablocked status
    
    * buffer increase win, time win
    
    * figure out where win is clicking
    
    * remove headed for multi mute test
    
    * use shift for windows
    
    * streamline mute test, decorate special click funcs
    
    * mark test unstable in win

--- tests/tabs/test_play_mute_unmute_tabs_via_toggle.py
+++ tests/tabs/test_play_mute_unmute_tabs_via_toggle.py
@@ -1,3 +1,5 @@
+from os import listdir
+from shutil import copyfile
 from time import sleep
 
 import pytest
@@ -18,30 +20,40 @@ def test_case():
 
 
 @pytest.fixture()
+def local_doc_path(tmp_path, html_filename):
+    for file in listdir("data/pages"):
+        copyfile(f"data/pages/{file}", tmp_path / file)
+    loc = tmp_path / html_filename
+    return f"file://{loc}"
+
+
+@pytest.fixture()
+def html_filename():
+    return "web_audio_landing.html"
+
+
+@pytest.fixture()
 def add_to_prefs_list():
     return [("network.cookie.cookieBehavior", "2")]
 
 
 # This test is unstable in Windows GHA for now
 @pytest.mark.audio
-@pytest.mark.headed
-def test_play_mute_unmute_tabs_via_toggle(driver: Firefox, sys_platform: str):
+def test_play_mute_unmute_tabs_via_toggle(
+    driver: Firefox, sys_platform: str, local_doc_path
+):
     """
     C246981 - Verify that play/mute/unmute tabs via toggle audio works
     """
     tabs = TabBar(driver)
     context_menu = ContextMenu(driver)
     wait = WebDriverWait(driver, 10)
-    DELAY = 2
-    POSITION_DELAY = 0.3
 
-    # Open Mozilla's Youtube Page
-    playlist_url = "https://www.youtube.com/@Mozilla/videos"
-    playlist_page = GenericPage(driver, url=playlist_url)
+    playlist_page = GenericPage(driver, url=local_doc_path)
     playlist_page.open()
 
     # Locate and open 2 latest videos in new tabs
-    video_selector = "ytd-rich-item-renderer a#video-title-link"
+    video_selector = ".audiolink"
     video_links = wait.until(
         EC.visibility_of_all_elements_located((By.CSS_SELECTOR, video_selector))
     )
@@ -52,86 +64,27 @@ def test_play_mute_unmute_tabs_via_toggle(driver: Firefox, sys_platform: str):
 
     # Verify correct number of tabs opened
     tabs.wait_for_num_tabs(3)
-    sleep(DELAY)
+    if tabs.sys_platform == "Windows":
+        sleep(3)
 
     # Select all tabs via Control/Command click while staying on first tab
-    modifier_key = Keys.COMMAND if sys_platform == "Darwin" else Keys.CONTROL
+    for i in [2, 3]:
+        tabs.control_click(tabs.get_tab(i))
+    for i in [2, 3]:
+        tabs.element_attribute_is(tabs.get_tab(i), "multiselected", "true")
 
-    with driver.context(driver.CONTEXT_CHROME):
-        actions = tabs.actions
+    tabs.element_attribute_is(tabs.get_tab(2), "activemedia-blocked", "true")
 
-        # Hold modifier and select the video tabs
-        actions.key_down(modifier_key)
-        for i in range(2, 4):  # Select tabs 2 and 3
-            tab_to_select = tabs.get_tab(i)
-            actions.click(tab_to_select)
-        actions.key_up(modifier_key).perform()
-
-        # Verify tabs are selected (multiselected attribute)
-        for i in range(2, 4):
-            tab = tabs.get_tab(i)
-            assert tab.get_attribute("multiselected") == "true", (
-                f"Tab {i} should be multiselected"
-            )
-
-        # Helper function to click the multi-tab audio control button
-        def click_multi_tab_audio_button():
-            tab = tabs.get_tab(2)
-            element_location = tab.location
-            element_size = tab.size
-            window_position = driver.get_window_position()
-
-            inner_height = driver.execute_script("return window.innerHeight;")
-            outer_height = driver.execute_script("return window.outerHeight;")
-            chrome_height = outer_height - inner_height
-
-            element_x = (
-                window_position["x"]
-                + element_location["x"]
-                + (element_size["width"] / 2)
-            )
-            element_y = (
-                window_position["y"]
-                + element_location["y"]
-                + (element_size["height"] / 2)
-                + chrome_height
-            )
-            # Offset to click on the audio control area (left side of tab)
-            tabs.gui.moveTo(element_x - 75, element_y)
-            sleep(POSITION_DELAY)  # Small delay for mouse positioning
-            tabs.gui.click()
-            sleep(DELAY)  # Wait for action to take effect
-
+    for _ in range(2):
         # Click Play button
-        click_multi_tab_audio_button()
+        tabs.click_on("any-media-button-by-tab-index", labels=["2"])
 
-        # Verify all selected tabs are playing
         for i in [2, 3]:
             tabs.expect_tab_sound_status(i, tabs.MEDIA_STATUS.PLAYING)
-            tab = tabs.get_tab(i)
-            assert tab.get_attribute("soundplaying") is not None, (
-                f"Tab {i} should be playing audio"
-            )
 
         # Click Mute button
-        click_multi_tab_audio_button()
+        tabs.click_on("any-media-button-by-tab-index", labels=["2"])
 
-        # Verify all selected tabs are muted
         for i in [2, 3]:
             tabs.expect_tab_sound_status(i, tabs.MEDIA_STATUS.MUTED)
-            tab = tabs.get_tab(i)
-            assert tab.get_attribute("muted") is not None, f"Tab {i} should be muted"
-
-        # Click Unmute button
-        click_multi_tab_audio_button()
-
-        # Verify all selected tabs are unmuted and playing again
-        for i in [2, 3]:
-            tabs.expect_tab_sound_status(i, tabs.MEDIA_STATUS.PLAYING)
-            tab = tabs.get_tab(i)
-            assert tab.get_attribute("soundplaying") is not None, (
-                f"Tab {i} should be playing audio after unmute"
-            )
-            assert tab.get_attribute("muted") is None, (
-                f"Tab {i} should not be muted after unmute"
-            )
+        # Iterate so we know that mute -> play functionality works
