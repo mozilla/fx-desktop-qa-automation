36d603ec609c4a296a438fb4c0d2aa0c1e76e001
Ben Chatterton <bchatterton@mozilla.com>
2026-07-23 10:28:48 -0700

    mark some tests; fix others

--- tests/security_and_privacy/conftest.py
+++ tests/security_and_privacy/conftest.py
@@ -1,7 +1,8 @@
 import pytest
+from selenium.webdriver.common.by import By
 
 from modules.browser_object import MenuBar, Navigation, PanelUi, TabBar, TrustPanel
-from modules.page_object import AboutCache, AboutDownloads, AboutPrefs
+from modules.page_object import AboutCache, AboutDownloads, AboutPrefs, GenericPage
 from modules.util import BrowserActions
 
 YOUTUBE_URL = "https://www.youtube.com/"
@@ -77,3 +78,31 @@ def ba(driver):
 @pytest.fixture()
 def websites():
     return [YOUTUBE_URL, FACEBOOK_URL, AMAZON_URL]
+
+
+@pytest.fixture()
+def check_tracker_test_field(driver):
+    its_a_tracker = GenericPage(
+        driver, url="https://www.itisatrap.org/firefox/its-a-tracker.html"
+    )
+    field_order = ["third-party", "first-party", "dnt"]
+    its_a_tracker.elements |= {
+        "test-list-item": {
+            "selectorData": "#tests .trackers li",
+            "strategy": "css",
+            "groups": ["doNotCache"],
+        }
+    }
+
+    def _check_tracker_test_field(field, correct=True):
+        li_to_check = its_a_tracker.get_elements("test-list-item")[
+            field_order.index(field)
+        ]
+        span_to_check = li_to_check.find_element(
+            By.CLASS_NAME, "correct" if correct else "incorrect"
+        )
+        its_a_tracker.expect(
+            lambda _: "hidden" not in span_to_check.get_attribute("class")
+        )
+
+    return _check_tracker_test_field
