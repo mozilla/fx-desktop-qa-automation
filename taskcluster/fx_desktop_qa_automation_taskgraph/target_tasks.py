2fa3a7d86ef420f241cff4c2cd93c7f9ef2fb76c
soncuteanca <soncutean.anca@gmail.com>
2026-08-14 14:26:10 +0300

    Move glean to an independent job execution

--- taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
+++ taskcluster/fx_desktop_qa_automation_taskgraph/target_tasks.py
@@ -22,6 +22,16 @@ def target_tasks_beta_func(full_task_graph, parameters, graph_config):
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
 
 
+@register_target_task("new_beta_glean")
+def target_tasks_beta_glean(full_task_graph, parameters, graph_config):
+    """Select the set of tasks required for a Beta Glean + Reporting session."""
+
+    def filter(task, parameters):
+        return task.attributes.get("beta-glean", False)
+
+    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
+
+
 @register_target_task("new_devedition_qa")
 def target_tasks_devedition_qa(full_task_graph, parameters, graph_config):
     """Select the set of tasks required for a DevEdition Smoke + Reporting session."""
@@ -38,5 +48,15 @@ def target_tasks_rc_qa(full_task_graph, parameters, graph_config):
 
     def filter(task, parameters):
         return task.attributes.get("rc-qa", False)
+
+    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
+
+
+@register_target_task("new_rc_glean")
+def target_tasks_rc_glean(full_task_graph, parameters, graph_config):
+    """Select the set of tasks required for a RC Glean + Reporting session."""
+
+    def filter(task, parameters):
+        return task.attributes.get("rc-glean", False)
 
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
