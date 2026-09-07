427e70e9e11c92acfbc8db00abbd50da8b503f0b
Hani Yacoub <hani.yacoub@svadmins-MacBook-Pro.local>
2026-09-07 14:15:09 +0300

    Add test tp settings dp

---
+++ tests/security_and_privacy/test_tp_settings_private_browsing.py
@@ -0,0 +1,68 @@
+import pytest
+from selenium.webdriver import Firefox
+
+from modules.browser_object import Navigation, PanelUi, TrustPanel
+from modules.page_object import GenericPage
+
+TEST_URL = "https://edition.cnn.com/"
+
+
+@pytest.fixture()
+def test_case():
+    return "446434"
+
+
+def test_tp_settings_private_browsing(
+    driver: Firefox, nav: Navigation, panel_ui: PanelUi, trust_panel: TrustPanel
+):
+    """
+    C446434 - TP settings in Private Browsing
+    """
+
+    # Instantiate objects
+    test_page = GenericPage(driver, url=TEST_URL)
+    private_trust_panel = TrustPanel(driver)
+
+    # Access test website in a new tab
+    panel_ui.open_and_switch_to_new_window("tab")
+    test_page.open()
+    normal_window = driver.current_window_handle
+
+    # Open a new private window and load the same website
+    panel_ui.open_and_switch_to_new_window("private")
+    private_window = driver.current_window_handle
+    test_page.open()
+
+    # The shield icon is displayed (the "✓" one)
+    private_trust_panel.element_visible("shield-icon")
+    private_trust_panel.element_not_visible("shield-icon-disabled")
+
+    # Switch off tracking protection in normal window for the site via toggle button
+    driver.switch_to.window(normal_window)
+    trust_panel.open_panel()
+    trust_panel.trustpanel_toggle_on_off()
+
+    # Tracking protection is switched off in normal window, shield icon has an "x" in it
+    trust_panel.element_visible("shield-icon-disabled")
+
+    # Refresh the https://edition.cnn.com/ website from private window
+    driver.switch_to.window(private_window)
+    nav.click_on("refresh-button")
+
+    # TP is ON, shield icon has a "✓" in it
+    private_trust_panel.element_visible("shield-icon")
+    private_trust_panel.element_not_visible("shield-icon-disabled")
+
+    # Switch off tracking protection in private window via toggle button
+    private_trust_panel.open_panel()
+    private_trust_panel.trustpanel_toggle_on_off()
+
+    # Shield icon has a "x" inside
+    private_trust_panel.element_visible("shield-icon-disabled")
+
+    # Refresh the https://edition.cnn.com/ website from normal window
+    driver.switch_to.window(normal_window)
+    nav.click_on("refresh-button")
+
+    # TP is OFF, shield icon has a "x" inside
+    trust_panel.element_visible("shield-icon-disabled")
