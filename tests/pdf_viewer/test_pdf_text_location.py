def83dacf2adb29409b820b2ab2eaef8a4e29be9
virgil.sangerean <vsangerean@mozilla.com>
2026-09-16 13:35:23 +0300

    add functions for page object generics.

--- tests/pdf_viewer/test_pdf_text_location.py
+++ tests/pdf_viewer/test_pdf_text_location.py
@@ -1,9 +1,9 @@
 import pytest
 
-from modules.page_object import GenericPdf
-
 PDF_FILE_NAME = "i-9.pdf"
 TEXT_TO_ADD = "PDF editor text"
+TEXT_X_OFFSET = -120
+TEXT_Y_OFFSET = -180
 
 
 @pytest.fixture()
@@ -26,7 +26,7 @@ def add_to_prefs_list():
     return [("pdfjs.annotationEditorMode", 0)]
 
 
-def test_user_can_select_location_for_pdf_text(pdf_viewer: GenericPdf):
+def test_user_can_select_location_for_pdf_text(pdf_viewer):
     """
     C1938265: Verify that the user can select a location for writing text
     over the PDF document.
@@ -37,4 +37,13 @@ def test_user_can_select_location_for_pdf_text(pdf_viewer: GenericPdf):
 
     # Steps 2-3: Select Text and add text at a location on the PDF document.
     pdf_viewer.select_editor_tool("toolbar-text")
-    pdf_viewer.add_text_to_pdf_page(TEXT_TO_ADD)
+    pdf_viewer.add_text_to_pdf_page(
+        TEXT_TO_ADD,
+        x_offset=TEXT_X_OFFSET,
+        y_offset=TEXT_Y_OFFSET,
+    )
+    pdf_viewer.expect_text_at_pdf_page_location(
+        "1",
+        TEXT_X_OFFSET,
+        TEXT_Y_OFFSET,
+    )
