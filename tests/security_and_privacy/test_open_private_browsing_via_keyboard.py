69c2364a1189de37432567f953d36935c8a2f5d6
Arash Eghtesadi <aeghtesadi@mozilla.com>
2026-09-17 10:37:20 -0700

    Code cleanup

--- tests/security_and_privacy/test_open_private_browsing_via_keyboard.py
+++ tests/security_and_privacy/test_open_private_browsing_via_keyboard.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium.webdriver import Firefox
-
-from modules.browser_object import Navigation
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+
+from modules.browser_object import Navigation
 
 
 @pytest.fixture()
@@ -23,7 +23,6 @@ def test_open_private_browsing_via_keyboard(driver: Firefox, nav: Navigation):
     nav.is_private()
 
     # Verify the purple mask (private browsing indicator icon) is displayed
-    #nav.element_visible("private-browsing-icon")
     with driver.context(driver.CONTEXT_CHROME):
         WebDriverWait(driver, 10).until(
             lambda browser: any(
