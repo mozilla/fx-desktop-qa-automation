0127de6de154338a25db010809175951777323e2
Akash Kumar Shaw <ashaw@mozilla.com>
2026-09-23 11:25:05 +0200

    Add translations and spell check learn more link tests

---
+++ tests/preferences/test_spell_check_learn_more_link.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium.webdriver import Firefox
+
+from modules.page_object import AboutPrefs
+
+SUPPORT_PAGE_URL = "https://support.mozilla.org/en-US/kb/how-do-i-use-firefox-spell-checker?as=u&utm_source=inproduct"
+
+
+@pytest.fixture()
+def about_prefs_category():
+    # The Spell check card lives in Settings > Languages.
+    return "languages"
+
+
+@pytest.fixture()
+def test_case():
+    return "3399180"
+
+
+@pytest.fixture()
+def add_to_prefs_list():
+    """Add to list of prefs to set"""
+    return [("browser.settings-redesign.enabled", True)]
+
+
+def test_spell_check_learn_more_link(driver: Firefox, about_prefs: AboutPrefs):
+    """
+    C3399180 - Spell check 'Learn more' link opens the correct link.
+    """
+    about_prefs.open()
+
+    # The link opens the SUMO article in a new tab.
+    about_prefs.click_on("spell-check-learn-more")
+    about_prefs.wait_for_num_tabs(2)
+    about_prefs.switch_to_new_tab()
+    about_prefs.url_contains(SUPPORT_PAGE_URL)
